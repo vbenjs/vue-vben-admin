f05819fdf6936b2ec47fc47eff01a62e93020078
haha <admin@qq.com>
2020-08-13 16:04:23 +0800

    tableAction button 添加type color 参数 普通button 增加popconfirm

--- src/components/table/src/components/TableAction.vue
+++ src/components/table/src/components/TableAction.vue
@@ -1,73 +1,122 @@
 <script lang="tsx">
-  import { defineComponent, PropOptions } from 'compatible-vue';
-  import { Dropdown, Menu } from 'ant-design-vue';
-  import { Icon } from '@/components/icon/index';
-  import { useDesign } from '@/hooks/core/useDesign';
+import { defineComponent, PropOptions } from 'compatible-vue';
+import { Dropdown, Menu, Popconfirm } from 'ant-design-vue';
+import { Icon } from '@/components/icon/index';
+import { useDesign } from '@/hooks/core/useDesign';
 
-  import { ActionItem } from '../types/tableAction';
-  export default defineComponent({
-    name: 'TableAction',
-    props: {
-      actions: {
-        type: Array,
-        default: null,
-      } as PropOptions<ActionItem[]>,
-      dropDownActions: {
-        type: Array,
-        default: null,
-      } as PropOptions<ActionItem[]>,
-    },
-    setup(props) {
-      const { prefixCls } = useDesign('table-action');
+import { ActionItem } from '../types/tableAction';
+export default defineComponent({
+  name: 'TableAction',
+  props: {
+    actions: {
+      type: Array,
+      default: null,
+    } as PropOptions<ActionItem[]>,
+    dropDownActions: {
+      type: Array,
+      default: null,
+    } as PropOptions<ActionItem[]>,
 
-      return () => {
-        const { dropDownActions = [], actions } = props;
-        return (
-          <div class={prefixCls}>
-            {actions &&
-              actions.length &&
-              actions.map((action) => {
-                const { disabled = false, on, label, props, icon } = action;
-                return (
-                  <a-button type="link" size="small" props={props} on={on} disabled={disabled}>
-                    {label}
-                    {icon && <Icon type={icon} />}
-                  </a-button>
-                );
-              })}
-            {dropDownActions && dropDownActions.length && (
-              <Dropdown>
-                <a-button type="link" size="small">
-                  更多
-                  <Icon type="down" />
-                </a-button>
-                (
-                <Menu slot="overlay">
-                  {dropDownActions.map((action, index) => {
-                    const { disabled = false, on, label, props, icon } = action;
-                    return (
-                      <Menu.Item key={index} disabled={disabled}>
-                        <a-button type="link" size="small" {...props} on={on} disabled={disabled}>
-                          {label}
-                          {icon && <Icon type={icon} />}
-                        </a-button>
-                      </Menu.Item>
-                    );
-                  })}
-                </Menu>
-                )
-              </Dropdown>
-            )}
-          </div>
-        );
-      };
-    },
-  });
+  },
+  setup(props) {
+    const { prefixCls } = useDesign('table-action');
+    // 增加按钮的TYPE和COLOR
+    return () => {
+      const { dropDownActions = [], actions } = props;
+      return (
+        <div class={prefixCls}>
+
+          {actions &&
+          actions.length &&
+          actions.map((action) => {
+            const {
+              disabled = false,
+              on,
+              label,
+              props,
+              icon,
+              color = '',
+              type = 'link',
+              popConfirm = null,
+            } = action;
+            const button = (
+              <a-button
+                type={type}
+                size="small"
+                props={props}
+                on={on}
+                disabled={disabled}
+                color={color}
+              >
+                {label}
+                {icon && <Icon type={icon} />}
+              </a-button>
+            );
+            if (popConfirm !== null) {
+              const { title, okText = '确定', cancelText = '取消', confirm = () => {}, cancel = () => {}, icon = '' } = popConfirm;
+              return (
+                <Popconfirm
+                  title={title}
+                  onConfirm={confirm}
+                  onCancel={cancel}
+                  okText={okText}
+                  cancelText={cancelText}
+                  icon={icon}
+                >
+                  {button}
+                </Popconfirm>
+              );
+            }
+            return button;
+          })}
+          {dropDownActions && dropDownActions.length && (
+            <Dropdown>
+              <a-button type="link" size="small">
+                更多
+                <Icon type="down" />
+              </a-button>
+              (
+              <Menu slot="overlay">
+                {dropDownActions.map((action, index) => {
+                  const {
+                    disabled = false,
+                    on,
+                    label,
+                    props,
+                    icon,
+                    color = '',
+                    type = 'link',
+                  } = action;
+                  return (
+                    <Menu.Item key={index} disabled={disabled}>
+                      <a-button
+                        type={type}
+                        size="small"
+                        {...props}
+                        on={on}
+                        disabled={disabled}
+                        color={color}
+                      >
+                        {label}
+                        {icon && <Icon type={icon} />}
+                      </a-button>
+                    </Menu.Item>
+                  );
+                })}
+              </Menu>
+              )
+            </Dropdown>
+          )}
+        </div>
+      );
+    };
+  },
+});
 </script>
 <style lang="less" scoped>
-  @import (reference) '~@design';
-  @prefix-cls: ~'@{namespace}-table-action';
-  .@{prefix-cls} {
-    display: flex;
-  }
+@import (reference) '~@design';
+@prefix-cls: ~'@{namespace}-table-action';
+.@{prefix-cls} {
+  display: flex;
+}
 </style>
--- src/components/table/src/types/tableAction.ts
+++ src/components/table/src/types/tableAction.ts
@@ -2,6 +2,18 @@ export interface ActionItem {
   on?: any;
   label: string;
   disabled?: boolean;
+  color?: string;
+  type?: string;
   props?: any;
+  icon?: string;
+  popConfirm?: PopConfirm;
+}
+
+export interface PopConfirm {
+  title: string;
+  okText: string;
+  cancelText: string;
+  confirm: any;
+  cancel?: any;
   icon?: string;
 }
