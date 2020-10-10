526e6ce22bf15cd04a09faf53a08ac43da491534
nebv <vbenadmin@163.com>
2020-10-11 01:18:38 +0800

    fix: repair packaging error

--- build/config/vite/proxy.ts
+++ build/config/vite/proxy.ts
@@ -2,7 +2,7 @@ type ProxyItem = [string, string];
 
 type ProxyList = ProxyItem[];
 
-export function createProxy(list: ProxyList) {
+export function createProxy(list: ProxyList = []) {
   const ret: any = {};
   for (const [prefix, target] of list) {
     ret[prefix] = {
