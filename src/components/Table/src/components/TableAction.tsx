69bd8b7bc0832ab60043f4446b46420feece98ac
vben <anncwb@126.com>
2020-10-20 23:47:39 +0800

    perf: perf TableAction

--- src/components/Table/src/components/TableAction.tsx
+++ src/components/Table/src/components/TableAction.tsx
@@ -16,114 +16,93 @@ export default defineComponent({
       type: Array as PropType<ActionItem[]>,
       default: null,
     },
+
+    moreText: {
+      type: String as PropType<string>,
+      default: '更多',
+    },
   },
   setup(props) {
+    function renderButton(action: ActionItem, index: number) {
+      const { disabled = false, label, icon, color = '', type = 'link' } = action;
+      const button = (
+        <Button type={type} size="small" disabled={disabled} color={color} {...action} key={index}>
+          {() => (
+            <>
+              {label}
+              {icon && <Icon icon={icon} />}
+            </>
+          )}
+        </Button>
+      );
+      return button;
+    }
+
+    function renderPopConfirm(action: ActionItem, index: number) {
+      const { popConfirm = null } = action;
+      if (!popConfirm) {
+        return renderButton(action, index);
+      }
+      const {
+        title,
+        okText = '确定',
+        cancelText = '取消',
+        confirm = () => {},
+        cancel = () => {},
+        icon = '',
+      } = popConfirm;
+      return (
+        <Popconfirm
+          key={`P-${index}`}
+          title={title}
+          onConfirm={confirm}
+          onCancel={cancel}
+          okText={okText}
+          cancelText={cancelText}
+          icon={icon}
+        >
+          {() => renderButton(action, index)}
+        </Popconfirm>
+      );
+    }
+
+    const dropdownDefaultSLot = () => (
+      <Button type="link" size="small">
+        {{
+          default: () => (
+            <>
+              {props.moreText}
+              <DownOutlined />
+            </>
+          ),
+        }}
+      </Button>
+    );
+
     // 增加按钮的TYPE和COLOR
     return () => {
       const { dropDownActions = [], actions } = props;
       return (
         <div class={prefixCls}>
-          {actions &&
-            actions.length &&
-            actions.map((action, index) => {
-              const {
-                disabled = false,
-                label,
-                icon,
-                color = '',
-                type = 'link',
-                popConfirm = null,
-              } = action;
-              const button = (
-                <Button
-                  type={type}
-                  size="small"
-                  disabled={disabled}
-                  color={color}
-                  {...action}
-                  key={index}
-                >
-                  {() => (
-                    <>
-                      {label}
-                      {icon && <Icon icon={icon} />}
-                    </>
-                  )}
-                </Button>
-              );
-              if (popConfirm !== null) {
-                const {
-                  title,
-                  okText = '确定',
-                  cancelText = '取消',
-                  confirm = () => {},
-                  cancel = () => {},
-                  icon = '',
-                } = popConfirm;
-                return (
-                  <Popconfirm
-                    key={`P-${index}`}
-                    title={title}
-                    onConfirm={confirm}
-                    onCancel={cancel}
-                    okText={okText}
-                    cancelText={cancelText}
-                    icon={icon}
-                  >
-                    {() => button}
-                  </Popconfirm>
-                );
-              }
-              return button;
-            })}
-          {dropDownActions && dropDownActions.length && (
+          {actions?.map((action, index) => {
+            return renderPopConfirm(action, index);
+          })}
+          {dropDownActions?.length && (
             <Dropdown>
               {{
-                default: () => (
-                  <Button type="link" size="small">
-                    {{
-                      default: () => (
-                        <>
-                          更多
-                          <DownOutlined />
-                        </>
-                      ),
-                    }}
-                  </Button>
-                ),
+                default: dropdownDefaultSLot,
                 overlay: () => {
                   return (
                     <Menu>
                       {{
                         default: () => {
                           return dropDownActions.map((action, index) => {
-                            const {
-                              disabled = false,
-                              label,
-                              icon,
-                              color = '',
-                              type = 'link',
-                            } = action;
+                            const { disabled = false } = action;
                             return (
                               <Menu.Item key={`${index}`} disabled={disabled}>
-                                {() => (
-                                  <Button
-                                    type={type}
-                                    size="small"
-                                    {...action}
-                                    disabled={disabled}
-                                    color={color}
-                                  >
-                                    {{
-                                      default: () => (
-                                        <>
-                                          {label}
-                                          {icon && <Icon icon={icon} />}
-                                        </>
-                                      ),
-                                    }}
-                                  </Button>
-                                )}
+                                {() => {
+                                  return renderPopConfirm(action, index);
+                                }}
                               </Menu.Item>
                             );
                           });
