51723c25d28398a4e3a1d52aa6052a2b09f0a0bc
fengli <fengli@biaodianyun.com>
2020-10-26 17:45:25 +0800

    feat：add lineheight

--- src/components/Tinymce/src/toolbar.ts
+++ src/components/Tinymce/src/toolbar.ts
@@ -2,7 +2,7 @@
 // Detail list see https://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols
 
 const toolbar = [
-  'searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample',
+  'fontsizeselect lineheight searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample',
   'hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen',
 ];
 
