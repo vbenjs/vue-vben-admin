675ae32f8cb5aef55e943fe526cf7a54f1b5632a
vben <anncwb@126.com>
2020-11-01 11:30:58 +0800

    fix(mock): fix mock paging tool error

--- mock/_util.ts
+++ mock/_util.ts
@@ -38,8 +38,10 @@ export function resultError(message = 'Request failed', { code = -1, result = nu
 }
 
 export function pagination<T = any>(pageNo: number, pageSize: number, array: T[]): T[] {
-  let offset = (pageNo - 1) * pageSize;
-  return offset + pageSize >= array.length
-    ? array.slice(offset, array.length)
-    : array.slice(offset, offset + pageSize);
+  let offset = (pageNo - 1) * Number(pageSize);
+  const ret =
+    offset + Number(pageSize) >= array.length
+      ? array.slice(offset, array.length)
+      : array.slice(offset, offset + Number(pageSize));
+  return ret;
 }
