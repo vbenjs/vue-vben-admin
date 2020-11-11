c13987d04dac667cfbf4e3b66e84afa2ddddd0b4
vben <anncwb@126.com>
2020-11-11 23:49:13 +0800

    fix: fix cssVar hmr error

--- src/hooks/web/useCssVar.ts
+++ src/hooks/web/useCssVar.ts
@@ -1,34 +1,40 @@
-import { ref, Ref, isRef, watch } from '@vue/runtime-dom'
+// import { ref, Ref, isRef, watch } from '@vue/runtime-dom';
 
-export default function useCssVar(
-  prop: string,
-  refEl?: Ref<HTMLElement | null>
-) {
-  const refVar = ref('')
-  let el: HTMLElement = document.documentElement
+// TODO 打开注释会造成热更新失效,待排查
+// export default function useCssVar(prop: string, refEl?: Ref<HTMLElement | null>) {
+//   const refVar = ref('');
+//   let el: HTMLElement = document.documentElement;
 
-  if (isRef(refEl)) {
-    watch(
-      refEl,
-      () => {
-        if (refEl.value) {
-          el = refEl.value as HTMLElement
-          refVar.value = getComputedStyle(el).getPropertyValue(prop)
-        }
-      },
-      { immediate: true }
-    )
-  } else {
-    refVar.value = getComputedStyle(el).getPropertyValue(prop)
-  }
+//   if (isRef(refEl)) {
+//     watch(
+//       refEl,
+//       () => {
+//         if (refEl.value) {
+//           el = refEl.value as HTMLElement;
+//           refVar.value = getComputedStyle(el).getPropertyValue(prop);
+//         }
+//       },
+//       { immediate: true }
+//     );
+//   } else {
+//     refVar.value = getComputedStyle(el).getPropertyValue(prop);
+//   }
 
-  watch(
-    refVar,
-    val => {
-      el && el.style.setProperty(prop, val)
-    },
-    { immediate: true }
-  )
+//   watch(
+//     refVar,
+//     (val) => {
+//       el && el.style.setProperty(prop, val);
+//     },
+//     { immediate: true }
+//   );
 
-  return refVar
+//   return refVar;
+// }
+
+export function getCssVar(prop: string, dom = document.documentElement) {
+  return getComputedStyle(dom).getPropertyValue(prop);
+}
+
+export function setCssVar(prop: string, val: any, dom = document.documentElement) {
+  dom.style.setProperty(prop, val);
 }
