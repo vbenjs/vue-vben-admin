fca0bb164a0f2e03acb5090bf59634225f5c06ee
vben <anncwb@126.com>
2020-11-19 22:20:59 +0800

    fix: fix spin style

--- src/setup/ant-design-vue/spin.tsx
+++ src/setup/ant-design-vue/spin.tsx
@@ -1,16 +1,11 @@
 import { Spin } from 'ant-design-vue';
 import svgImg from '/@/assets/images/loading.svg';
 
+import './spin.less';
 Spin.setDefaultIndicator({
   indicator: () => {
     return (
-      <div
-        class="app-svg-loading"
-        style={{
-          position: 'relative',
-          width: 'auto',
-        }}
-      >
+      <div class="app-svg-loading">
         <img src={svgImg} alt="" height="32" width="32" class="g-loading" />
       </div>
     );
