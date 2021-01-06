1e5fcd2cd2981b29f06cff08e588077b2dd02f45
vben <anncwb@126.com>
2021-01-06 20:56:03 +0800

    feat: added mixSide trigger

--- src/hooks/setting/useMenuSetting.ts
+++ src/hooks/setting/useMenuSetting.ts
@@ -93,8 +93,10 @@ const getCalcContentWidth = computed(() => {
     unref(getIsTopMenu) || !unref(getShowMenu) || (unref(getSplit) && unref(getMenuHidden))
       ? 0
       : unref(getIsMixSidebar)
-      ? SIDE_BAR_SHOW_TIT_MINI_WIDTH +
-        (unref(getMixSideFixed) && unref(mixSideHasChildren) ? unref(getRealWidth) : 0)
+      ? unref(getCollapsed)
+        ? SIDE_BAR_MINI_WIDTH
+        : SIDE_BAR_SHOW_TIT_MINI_WIDTH +
+          (unref(getMixSideFixed) && unref(mixSideHasChildren) ? unref(getRealWidth) : 0)
       : unref(getRealWidth);
   return `calc(100% - ${unref(width)}px)`;
 });
