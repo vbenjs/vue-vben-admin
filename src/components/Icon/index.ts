9081dd530c73a04cb909d7c5c62230b83dfacf19
Vben <anncwb@126.com>
2021-03-05 23:12:01 +0800

    feat: add SvgIcon component

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,9 +1,10 @@
 import Icon from './src/index.vue';
+import SvgIcon from './src/SvgIcon.vue';
 // import IconPicker from './src/IconPicker.vue';
 import { createAsyncComponent } from '/@/utils/factory/createAsyncComponent';
 
 const IconPicker = createAsyncComponent(() => import('./src/IconPicker.vue'));
 
-export { Icon, IconPicker };
+export { Icon, IconPicker, SvgIcon };
 
 export default Icon;
--- src/views/demo/feat/icon/index.vue
+++ src/views/demo/feat/icon/index.vue
@@ -21,6 +21,12 @@
       </div>
     </CollapseContainer>
 
+    <CollapseContainer title="svg Sprite" class="my-5">
+      <div class="flex justify-around flex-wrap">
+        <SvgIcon name="test" size="32" />
+      </div>
+    </CollapseContainer>
+
     <CollapseContainer title="图标选择器" class="my-5">
       <div class="flex justify-around flex-wrap">
         <IconPicker />
@@ -49,7 +55,7 @@
     CodepenCircleFilled,
   } from '@ant-design/icons-vue';
 
-  import { Icon, IconPicker } from '/@/components/Icon/index';
+  import { Icon, IconPicker, SvgIcon } from '/@/components/Icon/index';
 
   import { openWindow } from '/@/utils';
   import { PageWrapper } from '/@/components/Page';
@@ -68,6 +74,7 @@
       Icon,
       Alert,
       IconPicker,
+      SvgIcon,
     },
     setup() {
       return {
