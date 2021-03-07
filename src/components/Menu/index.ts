da87095826ea3e2d879e66aadaf692813fdf5e61
Vben <anncwb@126.com>
2021-03-07 20:59:11 +0800

    fix(menu): fix menu icon missing close #328

--- src/components/Menu/index.ts
+++ src/components/Menu/index.ts
@@ -1,9 +1,9 @@
-import { createAsyncComponent } from '/@/utils/factory/createAsyncComponent';
+// import { createAsyncComponent } from '/@/utils/factory/createAsyncComponent';
 
 import BasicMenu from './src/BasicMenu.vue';
 
 // export const BasicMenu = createAsyncComponent(() => import('./src/BasicMenu.vue'));
 
-export const MenuTag = createAsyncComponent(() => import('./src/components/MenuItemTag.vue'));
+// export const MenuTag = createAsyncComponent(() => import('./src/components/MenuItemTag.vue'));
 
 export { BasicMenu };
