f7499cdb0d5a76a1ee0fbecc9cf89d784b7a494a
舒培培 <cn.shperry@gmail.com>
2021-04-05 22:00:54 +0800

    chore: optimize prod configuration to control whether to delete origin files when using compress plugin (#443)

--- types/global.d.ts
+++ types/global.d.ts
@@ -65,6 +65,7 @@ declare global {
     VITE_USE_CDN: boolean;
     VITE_DROP_CONSOLE: boolean;
     VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
+    VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
     VITE_LEGACY: boolean;
     VITE_USE_IMAGEMIN: boolean;
     VITE_GENERATE_UI: string;
