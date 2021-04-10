be202674e2046bbfeeb3751c2d430e6377270d9c
Vben <anncwb@126.com>
2021-04-10 22:16:06 +0800

    fix(theme): make sure the steps style is correct, fix #414

--- build/vite/plugin/theme.ts
+++ build/vite/plugin/theme.ts
@@ -22,7 +22,17 @@ export function configThemePlugin(isBuild: boolean): Plugin[] {
   });
   const plugin = [
     viteThemePlugin({
-      resolveSelector: (s) => `[data-theme] ${s}`,
+      resolveSelector: (s) => {
+        s = s.trim();
+        switch (s) {
+          case '.ant-steps-item-process .ant-steps-item-icon > .ant-steps-icon':
+            return '.ant-steps-item-icon > .ant-steps-icon';
+          case '.ant-steps-item-icon > .ant-steps-icon':
+            return s;
+        }
+
+        return `[data-theme] ${s}`;
+      },
       colorVariables: [...getThemeColors(), ...colors],
     }),
     antdDarkThemePlugin({
