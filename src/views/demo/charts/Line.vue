96e58812b345e293f2318209b57b31781b9c2aa8
Vben <anncwb@126.com>
2021-04-10 23:49:39 +0800

    fix: echart import path

--- src/views/demo/charts/Line.vue
+++ src/views/demo/charts/Line.vue
@@ -4,7 +4,7 @@
 <script lang="ts">
   import { defineComponent, PropType, ref, Ref, onMounted } from 'vue';
 
-  import { useECharts } from '/@/hooks/web/useECharts';
+  import { useECharts } from '/@/hooks/web/useECharts/index';
   import { getLineData } from './data';
 
   export default defineComponent({
