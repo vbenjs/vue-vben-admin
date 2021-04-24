a83ec862c7b3e8065a50185a40a74d65af1a87fe
Vben <anncwb@126.com>
2021-04-24 20:31:05 +0800

    fix: ensure that the 401 jumps to the login page correctly, fix #512

--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -117,14 +117,17 @@ export const usePermissionStore = defineStore({
 
         // !Simulate to obtain permission codes from the background,
         // this function may only need to be executed once, and the actual project can be put at the right time by itself
+        let routeList: AppRouteRecordRaw[] = [];
         try {
           this.changePermissionCode('1');
-        } catch (error) {}
+          routeList = (await getMenuListById({ id: paramId })) as AppRouteRecordRaw[];
+        } catch (error) {
+          console.error(error);
+        }
 
         if (!paramId) {
           throw new Error('paramId is undefined!');
         }
-        let routeList = (await getMenuListById({ id: paramId })) as AppRouteRecordRaw[];
 
         // Dynamically introduce components
         routeList = transformObjToRoute(routeList);
