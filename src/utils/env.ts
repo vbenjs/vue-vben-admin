9480c1c708f00f7e93f4b18bf24f763cdfd11e27
江麻妞 <50100681+jiangmaniu@users.noreply.github.com>
2021-05-11 18:56:38 +0800

    chore: typo (#584)
    * fix: 变量名错误
    
    * fix: 修复正则可以匹配纯数字问题

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -30,7 +30,7 @@ export function getAppEnvConfig() {
     VITE_GLOB_UPLOAD_URL,
   } = ENV;
 
-  if (!/[a-zA-Z\_]*/.test(VITE_GLOB_APP_SHORT_NAME)) {
+  if (!/^[a-zA-Z\_]*$/.test(VITE_GLOB_APP_SHORT_NAME)) {
     warn(
       `VITE_GLOB_APP_SHORT_NAME Variables can only be characters/underscores, please modify in the environment variables and re-running.`
     );
