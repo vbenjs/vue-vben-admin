1eab49d0ed02a911733e500d2d769369a4e26b7a
zuihou <244387066@qq.com>
2021-05-12 13:19:14 +0800

    feat(axios): Do you want to return the original response header? For example, use this property when you need to get the response header

--- src/utils/http/axios/types.ts
+++ src/utils/http/axios/types.ts
@@ -7,6 +7,8 @@ export interface RequestOptions {
   formatDate?: boolean;
   //  Whether to process the request result
   isTransformRequestResult?: boolean;
+  // 是否返回原生响应头 比如：需要获取响应头时使用该属性
+  isReturnNativeResponse?: boolean;
   // Whether to join url
   joinPrefix?: boolean;
   // Interface address, use the default apiUrl if you leave it blank
