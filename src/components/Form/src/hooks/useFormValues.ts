ec3d51d69b66500f4f604151255920460d1906ce
HUCHAOQI <hyperq1g@gmail.com>
2021-05-12 20:35:28 +0800

    feat(form): add form field nested support (#591)
    * feat(form): add form field nested support
    
    * refactor(form): use lodash set replace

--- src/components/Form/src/hooks/useFormValues.ts
+++ src/components/Form/src/hooks/useFormValues.ts
@@ -5,6 +5,8 @@ import { unref } from 'vue';
 import type { Ref, ComputedRef } from 'vue';
 import type { FormProps, FormSchema } from '../types/form';
 
+import { set } from 'lodash-es';
+
 interface UseFormValuesContext {
   defaultValueRef: Ref<any>;
   getSchema: ComputedRef<FormSchema[]>;
@@ -40,7 +42,7 @@ export function useFormValues({
       if (isString(value)) {
         value = value.trim();
       }
-      res[key] = value;
+      set(res, key, value);
     }
     return handleRangeTimeValue(res);
   }
