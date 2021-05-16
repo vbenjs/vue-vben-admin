e4c3d0918abb9e88bea1fd1d21b13b884529a027
Vben <anncwb@126.com>
2021-05-17 00:54:45 +0800

    chore: update deps

--- src/views/sys/about/index.vue
+++ src/views/sys/about/index.vue
@@ -4,7 +4,7 @@
       <div class="flex justify-between items-center">
         <span class="flex-1">
           <a :href="GITHUB_URL" target="_blank">{{ name }}</a>
-          是一个基于Vue3.01、Vite、 Ant-Design-Vue 、TypeScript
+          是一个基于Vue3.0、Vite、 Ant-Design-Vue 、TypeScript
           的后台解决方案，目标是为中大型项目开发,提供现成的开箱解决方案及丰富的示例,原则上不会限制任何代码用于商用。
         </span>
       </div>
