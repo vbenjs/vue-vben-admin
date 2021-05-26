93006c7dc7b5243b26637f444c8057c95935e622
Netfan <netfan@foxmail.com>
2021-05-26 21:47:31 +0800

    feat(table): add editable DatePicker & TimePicker (#654)
    为表格的可编辑单元格添加日期选择框和时间选择框组件

--- src/components/Table/src/componentMap.ts
+++ src/components/Table/src/componentMap.ts
@@ -1,6 +1,14 @@
 import type { Component } from 'vue';
 
-import { Input, Select, Checkbox, InputNumber, Switch } from 'ant-design-vue';
+import {
+  Input,
+  Select,
+  Checkbox,
+  InputNumber,
+  Switch,
+  DatePicker,
+  TimePicker,
+} from 'ant-design-vue';
 
 import type { ComponentType } from './types/componentType';
 import { ApiSelect } from '/@/components/Form';
@@ -14,6 +22,8 @@ componentMap.set('Select', Select);
 componentMap.set('ApiSelect', ApiSelect);
 componentMap.set('Switch', Switch);
 componentMap.set('Checkbox', Checkbox);
+componentMap.set('DatePicker', DatePicker);
+componentMap.set('TimePicker', TimePicker);
 
 export function add(compName: ComponentType, component: Component) {
   componentMap.set(compName, component);
