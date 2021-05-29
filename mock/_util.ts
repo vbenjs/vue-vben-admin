bd953de203b9fc7e3fa76a348f30e9fff2163ad8
Netfan <netfan@foxmail.com>
2021-05-29 23:11:38 +0800

    refactor(api): remove unnecessary userId param (#675)
    * refactor(api): remove unnecessary userId param
    
    移除getUserInfo、getPermCode、getMenuList接口的userId参数。
    这些接口应当始终与当前登录用户相关而无需传递userId。
    
    * fix: fix auth header key case error

--- mock/_util.ts
+++ mock/_util.ts
@@ -43,3 +43,18 @@ export function pagination<T = any>(pageNo: number, pageSize: number, array: T[]
       : array.slice(offset, offset + Number(pageSize));
   return ret;
 }
+
+export interface requestParams {
+  method: string;
+  body: any;
+  headers?: { authorization?: string };
+  query: any;
+}
+
+/**
+ * @description 本函数用于从request数据中获取token，请根据项目的实际情况修改
+ *
+ */
+export function getRequestToken({ headers }: requestParams): string | undefined {
+  return headers?.authorization;
+}
