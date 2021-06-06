fb59c0213002099dbea8e4a934e24575de2ad466
无木 <netfan@foxmail.com>
2021-06-06 16:27:19 +0800

    feat(table-img): support simple show mode and more props
    改进TableImg组件，支持简单显示模式以及更多可配置的属性。

--- src/views/demo/table/CustomerCell.vue
+++ src/views/demo/table/CustomerCell.vue
@@ -7,9 +7,31 @@
           {{ record.no }}
         </Tag>
       </template>
+      <template #avatar="{ record }">
+        <Avatar
+          :size="60"
+          :src="'http://api.btstu.cn/sjtx/api.php?lx=c1&format=images&id=' + record.id"
+        />
+      </template>
       <template #img>
         <TableImg
-          :imgList="['https://picsum.photos/id/66/346/216', 'https://picsum.photos/id/67/346/216']"
+          :size="60"
+          :simpleShow="true"
+          :imgList="[
+            'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?1622962250222',
+            'https://picsum.photos/id/66/346/216',
+            'https://picsum.photos/id/67/346/216',
+          ]"
+        />
+      </template>
+      <template #imgs>
+        <TableImg
+          :size="60"
+          :imgList="[
+            'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?1622962250222',
+            'https://picsum.photos/id/66/346/216',
+            'https://picsum.photos/id/67/346/216',
+          ]"
         />
       </template>
 
@@ -24,13 +46,19 @@
 <script lang="ts">
   import { defineComponent } from 'vue';
   import { BasicTable, useTable, BasicColumn, TableImg } from '/@/components/Table';
-  import { Tag } from 'ant-design-vue';
+  import { Tag, Avatar } from 'ant-design-vue';
   import { demoListApi } from '/@/api/demo/table';
   const columns: BasicColumn[] = [
     {
       title: 'ID',
       dataIndex: 'id',
       slots: { customRender: 'id' },
+    },
+    {
+      title: '头像',
+      dataIndex: 'avatar',
+      width: 100,
+      slots: { customRender: 'avatar' },
     },
     {
       title: '分类',
@@ -46,10 +74,17 @@
       width: 120,
     },
     {
-      title: '头像',
+      title: '图片列表1',
       dataIndex: 'img',
-      width: 120,
+      helpMessage: ['这是简单模式的图片列表', '只会显示一张在表格中', '但点击可预览多张图片'],
+      width: 140,
       slots: { customRender: 'img' },
+    },
+    {
+      title: '照片列表2',
+      dataIndex: 'imgs',
+      width: 160,
+      slots: { customRender: 'imgs' },
     },
     {
       title: '地址',
@@ -70,7 +105,7 @@
     },
   ];
   export default defineComponent({
-    components: { BasicTable, TableImg, Tag },
+    components: { BasicTable, TableImg, Tag, Avatar },
     setup() {
       const [registerTable] = useTable({
         title: '自定义列内容',
