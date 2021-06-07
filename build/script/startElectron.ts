941f0cc96c84ea7b8271faa88feaccea80e562c5
Vben <anncwb@126.com>
2021-06-08 02:18:02 +0800

    feat: support electron

--- build/script/startElectron.ts
+++ build/script/startElectron.ts
@@ -1,14 +1,22 @@
 import { createServer } from 'vite';
 import path from 'path';
 import { startCompilerElectron } from './compilerElectron';
+import minimist from 'minimist';
 
 (async () => {
-  const server = await createServer({
-    root: path.resolve(__dirname, '../../'),
-  });
+  const argv = minimist(process.argv.slice(2));
+  console.log(argv);
+  const isDev = argv.env === 'development';
+  let port: number | undefined = undefined;
+  if (isDev) {
+    const server = await createServer({
+      root: path.resolve(__dirname, '../../'),
+    });
 
-  const app = await server.listen();
-  const port = app.config.server.port;
+    const app = await server.listen();
+    port = app.config.server.port;
+    process.env.PORT = `${port}`;
+  }
+
   startCompilerElectron(port);
-  process.env.PORT = `${port}`;
 })();
--- electron-main/index.ts
+++ electron-main/index.ts
@@ -27,7 +27,7 @@ class createWin {
     });
     const URL = is_dev
       ? `http://localhost:${process.env.PORT}` // vite 启动的服务器地址
-      : `file://${join(__dirname, '../dist/render/index.html')}`; // vite 构建后的静态文件地址
+      : `file://${join(__dirname, '../index.html')}`; // vite 构建后的静态文件地址
 
     mainWindow.loadURL(URL);
 
