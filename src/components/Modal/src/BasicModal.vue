655b74323653147943cbde2352208cb765c82b8a
Vben <anncwb@126.com>
2021-06-14 23:15:30 +0800

    fix(modal): ensure that the shutdown event is not triggered multiple times

--- src/components/Modal/src/BasicModal.vue
+++ src/components/Modal/src/BasicModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <Modal @cancel="handleCancel" v-bind="getBindValue">
+  <Modal v-bind="getBindValue">
     <template #closeIcon v-if="!$slots.closeIcon">
       <ModalClose
         :canFullscreen="getProps.canFullscreen"
