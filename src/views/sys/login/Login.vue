dc27c509d21e3e5277d588cb4b2d8d67ae741815
JinMao <jinmao88@qq.com>
2021-06-17 11:54:53 +0800

    fix: fix Login Page LocalePicker showLocale condition

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -3,7 +3,7 @@
     <AppLocalePicker
       class="absolute top-4 right-4 enter-x text-white xl:text-gray-600"
       :showText="false"
-      v-if="!sessionTimeout"
+      v-if="!sessionTimeout && showLocale"
     />
     <AppDarkModeToggle class="absolute top-3 right-7 enter-x" v-if="!sessionTimeout" />
 
