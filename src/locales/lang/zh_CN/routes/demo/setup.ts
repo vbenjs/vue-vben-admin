db244defaa9fb45102be519c8a7a3863eb59d2ed
Vben <anncwb@126.com>
2021-06-17 21:43:53 +0800

    feat(demo): added guide page example

---
+++ src/locales/lang/zh_CN/routes/demo/setup.ts
@@ -0,0 +1,3 @@
+export default {
+  page: '引导页',
+};
---
+++ src/router/routes/modules/demo/setup.ts
@@ -0,0 +1,28 @@
+import type { AppRouteModule } from '/@/router/types';
+
+import { LAYOUT } from '/@/router/constant';
+import { t } from '/@/hooks/web/useI18n';
+
+const setup: AppRouteModule = {
+  path: '/setup',
+  name: 'SetupDemo',
+  component: LAYOUT,
+  redirect: '/setup/index',
+  meta: {
+    icon: 'simple-icons:about-dot-me',
+    title: t('routes.demo.setup.page'),
+  },
+  children: [
+    {
+      path: 'index',
+      name: 'SetupDemoPage',
+      component: () => import('/@/views/demo/setup/index.vue'),
+      meta: {
+        title: t('routes.demo.setup.page'),
+        icon: 'whh:paintroll',
+      },
+    },
+  ],
+};
+
+export default setup;
