07f832c398eb1641e91c37c4376927e799ccfd4e
Vben <anncwb@126.com>
2021-06-17 21:43:53 +0800

    perf(locale): reduce the number of multilingual files

--- src/locales/lang/en/routes/demo/editor.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  editor: 'Editor',
-  jsonEditor: 'Json editor',
-  markdown: 'Markdown editor',
-
-  tinymce: 'Rich text',
-  tinymceBasic: 'Basic',
-  tinymceForm: 'embedded form',
-};
--- src/locales/lang/zh_CN/routes/demo/setup.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  page: '引导页',
-};
