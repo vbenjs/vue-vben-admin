e928cfefd25fe2c5a244fdd74fa5a0187e1fb8fa
无木 <netfan@foxmail.com>
2021-06-18 17:14:31 +0800

    feat: optimize error message for api failure
    优化request并发请求失败时的错误提示（同种类型的失败只提示一个message）

--- src/utils/http/axios/checkStatus.ts
+++ src/utils/http/axios/checkStatus.ts
@@ -74,7 +74,7 @@ export function checkStatus(
     if (errorMessageMode === 'modal') {
       createErrorModal({ title: t('sys.api.errorTip'), content: errMessage });
     } else if (errorMessageMode === 'message') {
-      error(errMessage);
+      error({ content: errMessage, key: `global_error_message_status_${status}` });
     }
   }
 }
