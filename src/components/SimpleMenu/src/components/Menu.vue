1b5f26822d05440f44cf526063b6161873a11160
Vben <anncwb@126.com>
2021-06-19 22:38:29 +0800

    perf(utils): mitt default export is changed from Class to Function

--- src/components/SimpleMenu/src/components/Menu.vue
+++ src/components/SimpleMenu/src/components/Menu.vue
@@ -22,7 +22,7 @@
   import { useDesign } from '/@/hooks/web/useDesign';
   import { propTypes } from '/@/utils/propTypes';
   import { createSimpleRootMenuContext } from './useSimpleMenuContext';
-  import Mitt from '/@/utils/mitt';
+  import mitt from '/@/utils/mitt';
   export default defineComponent({
     name: 'Menu',
     props: {
@@ -44,7 +44,7 @@
     },
     emits: ['select', 'open-change'],
     setup(props, { emit }) {
-      const rootMenuEmitter = new Mitt();
+      const rootMenuEmitter = mitt();
       const instance = getCurrentInstance();
 
       const currentActiveName = ref<string | number>('');
--- src/components/SimpleMenu/src/components/SubMenuItem.vue
+++ src/components/SimpleMenu/src/components/SubMenuItem.vue
@@ -77,7 +77,7 @@
   import Icon from '/@/components/Icon';
   import { Popover } from 'ant-design-vue';
   import { isBoolean, isObject } from '/@/utils/is';
-  import Mitt from '/@/utils/mitt';
+  import mitt from '/@/utils/mitt';
 
   const DELAY = 200;
   export default defineComponent({
@@ -114,7 +114,7 @@
 
       const { prefixCls } = useDesign('menu');
 
-      const subMenuEmitter = new Mitt();
+      const subMenuEmitter = mitt();
 
       const { rootMenuEmitter } = useSimpleRootMenuContext();
 
