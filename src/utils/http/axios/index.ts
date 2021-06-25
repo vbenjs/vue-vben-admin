253d63b14699aa3d9176a279756baae0ef35c3ec
JinMao <jinmao88@qq.com>
2021-06-26 04:47:17 +0800

    fix: fix defHttp baseUrl work

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -80,6 +80,7 @@ const transform: AxiosTransform = {
 
   // 请求之前处理config
   beforeRequestHook: (config, options) => {
+    const { baseURL } = config;
     const { apiUrl, joinPrefix, joinParamsToUrl, formatDate, joinTime = true } = options;
 
     if (joinPrefix) {
@@ -88,6 +89,9 @@ const transform: AxiosTransform = {
 
     if (apiUrl && isString(apiUrl)) {
       config.url = `${apiUrl}${config.url}`;
+    }
+    if (baseURL && isString(baseURL)) {
+      config.url = `${baseURL}${config.url}`;
     }
     const params = config.params || {};
     if (config.method?.toUpperCase() === RequestEnum.GET) {
@@ -186,7 +190,7 @@ function createAxios(opt?: Partial<CreateAxiosOptions>) {
         authenticationScheme: '',
         timeout: 10 * 1000,
         // 基础接口地址
-        // baseURL: globSetting.apiUrl,
+        baseURL: globSetting.apiUrl,
         // 接口可能会有通用的地址部分，可以统一抽取出来
         urlPrefix: urlPrefix,
         headers: { 'Content-Type': ContentTypeEnum.JSON },
