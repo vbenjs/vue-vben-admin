5bfcc3f644164e499380360883750cd423fe455f
无木 <netfan@foxmail.com>
2021-06-28 11:10:58 +0800

    fix(modal): maskClosable not work
    修复BasicModal点击遮罩不能关闭的问题

--- src/components/Modal/src/BasicModal.vue
+++ src/components/Modal/src/BasicModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <Modal v-bind="getBindValue">
+  <Modal v-bind="getBindValue" @cancel="handleCancel">
     <template #closeIcon v-if="!$slots.closeIcon">
       <ModalClose
         :canFullscreen="getProps.canFullscreen"
@@ -178,6 +178,7 @@
         }
 
         visibleRef.value = false;
+        console.log(visibleRef.value);
         emit('cancel', e);
       }
 
