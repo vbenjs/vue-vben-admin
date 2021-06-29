d7b84c78744f7d0077a779b232e1358040b50383
无木 <netfan@foxmail.com>
2021-06-29 18:34:51 +0800

    fix(lock-screen): fix lock-screen can skip on new window
    修复锁屏功能可以通过刷新页面或复制 URL 打开新的浏览器标签来跳过锁定状态的问题

--- src/utils/cache/persistent.ts
+++ src/utils/cache/persistent.ts
@@ -57,12 +57,14 @@ export class Persistent {
     immediate && ls.set(APP_LOCAL_CACHE_KEY, localMemory.getCache);
   }
 
-  static removeLocal(key: LocalKeys): void {
+  static removeLocal(key: LocalKeys, immediate = false): void {
     localMemory.remove(key);
+    immediate && ls.set(APP_LOCAL_CACHE_KEY, localMemory.getCache);
   }
 
-  static clearLocal(): void {
+  static clearLocal(immediate = false): void {
     localMemory.clear();
+    immediate && ls.clear();
   }
 
   static getSession<T>(key: SessionKeys) {
@@ -74,16 +76,22 @@ export class Persistent {
     immediate && ss.set(APP_SESSION_CACHE_KEY, sessionMemory.getCache);
   }
 
-  static removeSession(key: SessionKeys): void {
+  static removeSession(key: SessionKeys, immediate = false): void {
     sessionMemory.remove(key);
+    immediate && ss.set(APP_SESSION_CACHE_KEY, sessionMemory.getCache);
   }
-  static clearSession(): void {
+  static clearSession(immediate = false): void {
     sessionMemory.clear();
+    immediate && ss.clear();
   }
 
-  static clearAll() {
+  static clearAll(immediate = false) {
     sessionMemory.clear();
     localMemory.clear();
+    if (immediate) {
+      ls.clear();
+      ss.clear();
+    }
   }
 }
 
