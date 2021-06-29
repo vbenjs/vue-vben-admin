9c56f0bde7ea9022b2d638b86a138e7da4f54733
无木 <netfan@foxmail.com>
2021-06-29 19:41:06 +0800

    fix: multi windows token  sharing
    修复同时打开多个窗口时令牌没能同步共享的问题
    
    fixed: #761

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -19,3 +19,8 @@ export function setAuthCache(key: BasicKeys, value) {
   const fn = isLocal ? Persistent.setLocal : Persistent.setSession;
   return fn(key, value, true);
 }
+
+export function clearAuthCache(immediate = true) {
+  const fn = isLocal ? Persistent.clearLocal : Persistent.clearSession;
+  return fn(immediate);
+}
