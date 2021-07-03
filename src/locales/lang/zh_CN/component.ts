6cbac4b7ece60a1a7c1fda931cfffce42dfe3e51
无木 <netfan@foxmail.com>
2021-07-03 20:14:58 +0800

    feat(avatar-cropper): add action tooltip
    为头像剪裁组件的操作按钮添加工具提示

--- src/locales/lang/zh_CN/component.ts
+++ src/locales/lang/zh_CN/component.ts
@@ -13,6 +13,13 @@ export default {
     uploadSuccess: '上传成功',
     modalTitle: '头像上传',
     okText: '确认并上传',
+    btn_reset: '重置',
+    btn_rotate_left: '逆时针旋转',
+    btn_rotate_right: '顺时针旋转',
+    btn_scale_x: '水平翻转',
+    btn_scale_y: '垂直翻转',
+    btn_zoom_in: '放大',
+    btn_zoom_out: '缩小',
   },
   drawer: {
     loadingText: '加载中...',
