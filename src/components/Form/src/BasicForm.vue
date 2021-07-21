2122743c927b17718592f32d45cce7685b571c4d
无木 <netfan@foxmail.com>
2021-07-21 11:18:10 +0800

    fix(form): fix `validate` promise catch
    修复自动校验时没有捕获promise catch的错误提示

--- src/components/Form/src/BasicForm.vue
+++ src/components/Form/src/BasicForm.vue
@@ -231,9 +231,7 @@
         formModel[key] = value;
         const { validateTrigger } = unref(getBindValue);
         if (!validateTrigger || validateTrigger === 'change') {
-          try {
-            validateFields([key]);
-          } catch (e) {}
+          validateFields([key]).catch((_) => {});
         }
       }
 
