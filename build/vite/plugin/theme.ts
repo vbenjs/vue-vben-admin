42812162c46832ce4d3e332bd579c042309115bc
无木 <netfan@foxmail.com>
2021-07-21 21:51:48 +0800

    fix(dark-theme): disabled link `button` color
    修复黑暗主题下禁用状态的link类型按钮的颜色

--- build/vite/plugin/theme.ts
+++ build/vite/plugin/theme.ts
@@ -45,8 +45,8 @@ export function configThemePlugin(isBuild: boolean): Plugin[] {
     }),
     antdDarkThemePlugin({
       preloadFiles: [
-        //path.resolve(process.cwd(), 'node_modules/ant-design-vue/dist/antd.less'),
-        path.resolve(process.cwd(), 'node_modules/ant-design-vue/dist/antd.dark.less'),
+        path.resolve(process.cwd(), 'node_modules/ant-design-vue/dist/antd.less'),
+        //path.resolve(process.cwd(), 'node_modules/ant-design-vue/dist/antd.dark.less'),
         path.resolve(process.cwd(), 'src/design/index.less'),
       ],
       filter: (id) => (isBuild ? !id.endsWith('antd.less') : true),
