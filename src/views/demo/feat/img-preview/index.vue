ff629a523528d83ad8019a869d9c3f5e4d60166f
无木 <netfan@foxmail.com>
2021-07-26 20:58:18 +0800

    feat(preview): add more features
    为Preview组件添加新的属性及事件

--- src/views/demo/feat/img-preview/index.vue
+++ src/views/demo/feat/img-preview/index.vue
@@ -8,6 +8,7 @@
   import { defineComponent } from 'vue';
   import { createImgPreview, ImagePreview } from '/@/components/Preview/index';
   import { PageWrapper } from '/@/components/Page';
+  // import { PreviewActions } from '/@/components/Preview/src/typing';
 
   const imgList: string[] = [
     'https://picsum.photos/id/66/346/216',
@@ -18,7 +19,11 @@
     components: { PageWrapper, ImagePreview },
     setup() {
       function openImg() {
-        createImgPreview({ imageList: imgList });
+        const onImgLoad = ({ index, url, dom }) => {
+          console.log(`第${index + 1}张图片已加载，URL为：${url}`, dom);
+        };
+        // 可以使用createImgPreview返回的 PreviewActions 来控制预览逻辑，实现类似幻灯片、自动旋转之类的骚操作
+        createImgPreview({ imageList: imgList, defaultWidth: 700, rememberState: true, onImgLoad });
       }
       return { imgList, openImg };
     },
