e23bd2696da945291a9b652f1af39ad1936f376b
无木 <netfan@foxmail.com>
2021-07-26 20:58:18 +0800

    feat(preview): add more features
    为Preview组件添加新的属性及事件

--- src/components/Preview/src/functional.ts
+++ src/components/Preview/src/functional.ts
@@ -6,15 +6,12 @@ import { createVNode, render } from 'vue';
 let instance: ReturnType<typeof createVNode> | null = null;
 export function createImgPreview(options: Options) {
   if (!isClient) return;
-  const { imageList, show = true, index = 0 } = options;
-
   const propsData: Partial<Props> = {};
   const container = document.createElement('div');
-  propsData.imageList = imageList;
-  propsData.show = show;
-  propsData.index = index;
+  Object.assign(propsData, { show: true, index: 0, scaleStep: 100 }, options);
 
   instance = createVNode(ImgPreview, propsData);
   render(instance, container);
   document.body.appendChild(container);
+  return instance.component?.exposed;
 }
