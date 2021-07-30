26d9476caff41cc355190604af42e0bd2ef0a353
无木 <netfan@foxmail.com>
2021-07-30 16:59:52 +0800

    feat(use-loading): add `setTip` method
    为useLoading添加setTip方法

--- src/components/Loading/src/useLoading.ts
+++ src/components/Loading/src/useLoading.ts
@@ -12,10 +12,12 @@ interface Fn {
   (): void;
 }
 
-export function useLoading(props: Partial<LoadingProps>): [Fn, Fn];
-export function useLoading(opt: Partial<UseLoadingOptions>): [Fn, Fn];
+export function useLoading(props: Partial<LoadingProps>): [Fn, Fn, (string) => void];
+export function useLoading(opt: Partial<UseLoadingOptions>): [Fn, Fn, (string) => void];
 
-export function useLoading(opt: Partial<LoadingProps> | Partial<UseLoadingOptions>): [Fn, Fn] {
+export function useLoading(
+  opt: Partial<LoadingProps> | Partial<UseLoadingOptions>
+): [Fn, Fn, (string) => void] {
   let props: Partial<LoadingProps>;
   let target: HTMLElement | Ref<ElRef> = document.body;
 
@@ -39,5 +41,9 @@ export function useLoading(opt: Partial<LoadingProps> | Partial<UseLoadingOption
     instance.close();
   };
 
-  return [open, close];
+  const setTip = (tip: string) => {
+    instance.setTip(tip);
+  };
+
+  return [open, close, setTip];
 }
