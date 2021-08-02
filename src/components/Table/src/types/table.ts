1945cb892cc8095ab9d96909ddb12fc500abd2f6
leocaan <leocaan@qq.com>
2021-08-02 22:22:07 +0800

    fix(table): recursive updateTableDataRecord
    无刷新更新表格数据时，支持递归查找，用于树状数据时。同时新增 findTableDataRecord 函数，用于支持无刷新新增数据到树状表格中

--- src/components/Table/src/types/table.ts
+++ src/components/Table/src/types/table.ts
@@ -95,6 +95,7 @@ export interface TableActionType {
   setPagination: (info: Partial<PaginationProps>) => void;
   setTableData: <T = Recordable>(values: T[]) => void;
   updateTableDataRecord: (rowKey: string | number, record: Recordable) => Recordable | void;
+  findTableDataRecord: (rowKey: string | number) => Recordable | void;
   getColumns: (opt?: GetColumnsParams) => BasicColumn[];
   setColumns: (columns: BasicColumn[] | string[]) => void;
   getDataSource: <T = Recordable>() => T[];
