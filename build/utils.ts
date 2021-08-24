9f6822991c4b2da78e0a5d0c7d6e0288f0d9d1cb
无木 <netfan@foxmail.com>
2021-08-24 22:51:46 +0800

    fix: name of vite `mode` support more characters
    修复vite模式名称不支持下划线的问题
    
    fixed: #1115

--- build/utils.ts
+++ build/utils.ts
@@ -50,7 +50,7 @@ export function wrapperEnv(envConf: Recordable): ViteEnv {
  */
 function getConfFiles() {
   const script = process.env.npm_lifecycle_script;
-  const reg = new RegExp('--mode ([a-z]+)');
+  const reg = new RegExp('--mode ([a-z_\\d]+)');
   const result = reg.exec(script as string) as any;
   if (result) {
     const mode = result[1] as string;
