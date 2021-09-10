73dc492b2a49793d945ccdae7f5c429c874f298c
无木 <netfan@foxmail.com>
2021-09-10 11:24:53 +0800

    feat(markdown-viewer): add new component
    新增MarkdownViewer组件用于显示Markdown格式的富文本
    
    close: #1181

---
+++ src/components/Markdown/src/MarkdownViewer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-html="getHtmlData" :class="$props.class" class="markdown-viewer"></div>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue';
+  import showdown from 'showdown';
+
+  const converter = new showdown.Converter();
+  converter.setOption('tables', true);
+  const props = defineProps({
+    value: { type: String },
+    class: { type: String },
+  });
+  const getHtmlData = computed(() => converter.makeHtml(props.value || ''));
+</script>
+
+<style scoped>
+  .markdown-viewer {
+    width: 100%;
+  }
+</style>
