a28d94bf1c02048b369f7c986e2c68543ef3498b
无木 <netfan@foxmail.com>
2021-09-11 20:26:27 +0800

    fix(table-action): `divider` not work as expected
    修复tableAction中的divider未按预期工作

--- src/components/Table/src/components/TableAction.vue
+++ src/components/Table/src/components/TableAction.vue
@@ -14,11 +14,7 @@
       <Divider
         type="vertical"
         class="action-divider"
-        v-if="
-          divider &&
-          index < getActions.length - (dropDownActions ? 0 : 1) &&
-          getDropdownList.length > 0
-        "
+        v-if="divider && index < getActions.length - 1"
       />
     </template>
     <Dropdown
