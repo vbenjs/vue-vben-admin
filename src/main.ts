dc756b5947bb66c13bd198be8a72288511047677
Jiang <49470702+qq34347476@users.noreply.github.com>
2021-09-16 11:22:07 +0800

    Update main.ts
    dayjs 替换moment ，dayjs 用 1.9.8版本的

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,38 @@ import { setupGlobDirectives } from '/@/directives';
 import { setupI18n } from '/@/locales/setupI18n';
 import { registerGlobComp } from '/@/components/registerGlobComp';
 
+import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import weekday from 'dayjs/plugin/weekday';
+import weekYear from 'dayjs/plugin/weekYear';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import isMoment from 'dayjs/plugin/isMoment';
+import localeData from 'dayjs/plugin/localeData';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import badMutable from 'dayjs/plugin/badMutable';
+// import 'dayjs/locale/en';
+import 'dayjs/locale/zh-cn';
+
+// ### extend
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(advancedFormat);
+dayjs.extend(customParseFormat);
+dayjs.extend(weekday);
+dayjs.extend(weekYear);
+dayjs.extend(weekOfYear);
+dayjs.extend(isMoment);
+dayjs.extend(localeData);
+dayjs.extend(localizedFormat);
+dayjs.extend(badMutable);
+
+// ### locale
+// dayjs.locale('en')
+dayjs.locale('zh-cn');
+
 // Importing on demand in local development will increase the number of browser requests by around 20%.
 // This may slow down the browser refresh speed.
 // Therefore, only enable on-demand importing in production environments .
