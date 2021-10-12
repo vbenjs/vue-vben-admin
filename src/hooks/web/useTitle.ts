a9cca62c11d3f245c810e46dd29dd2744ccf7906
zmtlwzy <49909526+zmtlwzy@users.noreply.github.com>
2021-10-12 09:59:34 +0800

    fixed: (update pageTitle when change local)
    If AppLocalePicker reload prop is false, Should be updated when changing the language

--- src/hooks/web/useTitle.ts
+++ src/hooks/web/useTitle.ts
@@ -3,6 +3,7 @@ import { useI18n } from '/@/hooks/web/useI18n';
 import { useTitle as usePageTitle } from '@vueuse/core';
 import { useGlobSetting } from '/@/hooks/setting';
 import { useRouter } from 'vue-router';
+import { useLocaleStore } from '/@/store/modules/locale';
 
 import { REDIRECT_NAME } from '/@/router/constant';
 
@@ -13,11 +14,12 @@ export function useTitle() {
   const { title } = useGlobSetting();
   const { t } = useI18n();
   const { currentRoute } = useRouter();
+  const localeStore = useLocaleStore();
 
   const pageTitle = usePageTitle();
 
   watch(
-    () => currentRoute.value.path,
+    [() => currentRoute.value.path, () => localeStore.getLocale],
     () => {
       const route = unref(currentRoute);
 
