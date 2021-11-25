2aabbc788ab05d06f4dcc2a01b25585ecf8f690b
vben <anncwb@126.com>
2021-11-26 00:09:08 +0800

    fix: Fix multi-language errors and installation errors

--- vite.config.ts
+++ vite.config.ts
@@ -99,7 +99,6 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
         'ant-design-vue/es/locale/zh_CN',
         'ant-design-vue/es/locale/en_US',
       ],
-      exclude: ['vue-demi'],
     },
   };
 };
