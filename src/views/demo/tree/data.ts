13d660bede83a991b8b613d3aeb0729cff2edfce
zuihou <244387066@qq.com>
2021-12-10 22:19:43 +0800

    feat: 添加BasicTree使用 fieldNames 后，actionList 和 插槽 会失效的示例代码

--- src/views/demo/tree/data.ts
+++ src/views/demo/tree/data.ts
@@ -33,3 +33,89 @@ export const treeData: TreeItem[] = [
     ],
   },
 ];
+
+export const treeData2: any[] = [
+  {
+    name: 'parent ',
+    id: '0-0',
+    slots: { title: 'title' },
+    children: [
+      { name: 'leaf', id: '0-0-0', slots: { title: 'title' } },
+      {
+        name: 'leaf',
+        id: '0-0-1',
+        slots: { title: 'title' },
+        children: [
+          {
+            name: 'leaf',
+            slots: { title: 'title' },
+            id: '0-0-0-0',
+            children: [{ name: 'leaf', id: '0-0-0-0-1', slots: { title: 'title' } }],
+          },
+          { name: 'leaf', slots: { title: 'title' }, id: '0-0-0-1' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'parent 2',
+    id: '1-1',
+    slots: { title: 'title' },
+    children: [
+      { name: 'leaf', slots: { title: 'title' }, id: '1-1-0' },
+      { name: 'leaf', slots: { title: 'title' }, id: '1-1-1' },
+    ],
+  },
+  {
+    name: 'parent 3',
+    id: '2-2',
+    slots: { title: 'title' },
+    children: [
+      { name: 'leaf', slots: { title: 'title' }, id: '2-2-0' },
+      { name: 'leaf', slots: { title: 'title' }, id: '2-2-1' },
+    ],
+  },
+];
+
+export const treeData3: any[] = [
+  {
+    name: 'parent ',
+    key: '0-0',
+    slots: { title: 'title' },
+    children: [
+      { name: 'leaf', key: '0-0-0', slots: { title: 'title' } },
+      {
+        name: 'leaf',
+        key: '0-0-1',
+        slots: { title: 'title' },
+        children: [
+          {
+            name: 'leaf',
+            slots: { title: 'title' },
+            key: '0-0-0-0',
+            children: [{ name: 'leaf', key: '0-0-0-0-1', slots: { title: 'title' } }],
+          },
+          { name: 'leaf', slots: { title: 'title' }, key: '0-0-0-1' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'parent 2',
+    key: '1-1',
+    slots: { title: 'title' },
+    children: [
+      { name: 'leaf', slots: { title: 'title' }, key: '1-1-0' },
+      { name: 'leaf', slots: { title: 'title' }, key: '1-1-1' },
+    ],
+  },
+  {
+    name: 'parent 3',
+    key: '2-2',
+    slots: { title: 'title' },
+    children: [
+      { name: 'leaf', slots: { title: 'title' }, key: '2-2-0' },
+      { name: 'leaf', slots: { title: 'title' }, key: '2-2-1' },
+    ],
+  },
+];
