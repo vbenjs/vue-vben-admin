874c7b6fae9800cfe4333df2eaf0a3c66a562690
饶宏瑞 <1047422165@qq.com>
2021-12-16 12:04:03 +0800

    fix: 修复编辑单元格为空时不能触发编辑的bug

--- src/components/Table/src/components/editable/EditableCell.vue
+++ src/components/Table/src/components/editable/EditableCell.vue
@@ -6,7 +6,7 @@
       @click="handleEdit"
     >
       <div class="cell-content" :title="column.ellipsis ? getValues ?? '' : ''">
-        {{ getValues ?? '&nbsp;' }}
+        {{ getValues || getValues === 0 ? getValues : '&nbsp;' }}
       </div>
       <FormOutlined :class="`${prefixCls}__normal-icon`" v-if="!column.editRow" />
     </div>
