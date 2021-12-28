de8ea591775f172eb6caa1ef72d827c903250f80
George Tan <406132614@qq.com>
2021-12-28 18:45:32 +0800

    fix(table component): reduce 'insertTableDataRecord' code logic (#1536)

--- src/components/Table/src/hooks/useDataSource.ts
+++ src/components/Table/src/hooks/useDataSource.ts
@@ -196,11 +196,10 @@ export function useDataSource(
   }
 
   function insertTableDataRecord(record: Recordable, index: number): Recordable | undefined {
-    if (!dataSourceRef.value || dataSourceRef.value.length == 0) return;
+    // if (!dataSourceRef.value || dataSourceRef.value.length == 0) return;
     index = index ?? dataSourceRef.value?.length;
     unref(dataSourceRef).splice(index, 0, record);
-    unref(propsRef).dataSource?.splice(index, 0, record);
-    return unref(propsRef).dataSource;
+    return unref(dataSourceRef);
   }
 
   function findTableDataRecord(rowKey: string | number) {
