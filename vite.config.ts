75badb67d78f57fda0fe93a73d9885bc29656738
JiaWei Lee <gsdukbh@gmail.com>
2022-02-22 10:08:22 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -53,7 +53,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       ],
     },
     server: {
-      https: true,
+      https: false,
       // Listening on all local IPs
       host: true,
       port: VITE_PORT,
