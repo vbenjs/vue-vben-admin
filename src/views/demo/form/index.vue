d7b81d0eef01865c1c4ec644218394a8741f7e7b
JinMao <jinmao88@qq.com>
2022-04-18 08:21:04 +0800

    feat: ApiTree add afterFetch props,add ApiTree demo

--- src/views/demo/form/index.vue
+++ src/views/demo/form/index.vue
@@ -461,6 +461,27 @@
       },
     },
     {
+      field: 'field36',
+      component: 'ApiTree',
+      label: '远程Tree',
+      helpMessage: ['ApiTree组件', '使用接口提供的数据生成选项'],
+      required: true,
+      componentProps: {
+        api: treeOptionsListApi,
+        params: {
+          count: 2,
+        },
+        afterFetch: (v) => {
+          //do something
+          return v;
+        },
+        resultField: 'list',
+      },
+      colProps: {
+        span: 8,
+      },
+    },
+    {
       field: 'divider-linked',
       component: 'Divider',
       label: '字段联动',
