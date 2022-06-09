a0ffc35e36c0aa1e68c8b288496bb787eccf6665
Invincible <571574085@qq.com>
2022-06-09 07:07:09 +0000

    fix: 个人中心（data.tsx）多余的html片段 (#1894)

--- src/components/FlowChart/src/FlowChart.vue
+++ src/components/FlowChart/src/FlowChart.vue
@@ -8,21 +8,21 @@
   </div>
 </template>
 <script lang="ts">
-  import type { Ref } from 'vue';
   import type { Definition } from '@logicflow/core';
-  import { defineComponent, ref, onMounted, unref, nextTick, computed, watch } from 'vue';
-  import FlowChartToolbar from './FlowChartToolbar.vue';
   import LogicFlow from '@logicflow/core';
-  import { Snapshot, BpmnElement, Menu, DndPanel, SelectionSelect } from '@logicflow/extension';
+  import '@logicflow/core/dist/style/index.css';
+  import { BpmnElement, DndPanel, Menu, SelectionSelect, Snapshot } from '@logicflow/extension';
+  import '@logicflow/extension/lib/style/index.css';
+  import type { Ref } from 'vue';
+  import { computed, defineComponent, nextTick, onMounted, ref, unref, watch } from 'vue';
+  import { toLogicFlowData } from './adpterForTurbo';
+  import { configDefaultDndPanel } from './config';
+  import FlowChartToolbar from './FlowChartToolbar.vue';
+  import { createFlowChartContext } from './useFlowContext';
+  import { JsonPreview } from '/@/components/CodeEditor';
+  import { BasicModal, useModal } from '/@/components/Modal';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { useAppStore } from '/@/store/modules/app';
-  import { createFlowChartContext } from './useFlowContext';
-  import { toLogicFlowData } from './adpterForTurbo';
-  import { useModal, BasicModal } from '/@/components/Modal';
-  import { JsonPreview } from '/@/components/CodeEditor';
-  import { configDefaultDndPanel } from './config';
-  import '@logicflow/core/dist/style/index.css';
-  import '@logicflow/extension/lib/style/index.css';
 
   export default defineComponent({
     name: 'FlowChart',
