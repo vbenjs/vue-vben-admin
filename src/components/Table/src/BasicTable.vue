d0dfd68996b65e5d78a5ded260a6839bdf8d1a93
Micah <yumingsheng@hrbeu.edu.cn>
2022-06-17 15:54:54 +0800

    style: 优化滚动条显示，只有在内容超过显示高度时才会出现

--- src/components/Table/src/BasicTable.vue
+++ src/components/Table/src/BasicTable.vue
@@ -401,6 +401,10 @@
       width: 100%;
       overflow-x: hidden;
 
+      .ant-table-body {
+        overflow: auto !important;
+      }
+
       &-title {
         display: flex;
         padding: 8px 6px;
