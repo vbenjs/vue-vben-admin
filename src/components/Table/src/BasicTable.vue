2c2adbfa33cdf94b5fd9912a9ed6019d56ff1817
gauhar <2464231008@qq.com>
2022-06-30 14:10:58 +0800

    fix: 删除无用代码，$slots中已包含

--- src/components/Table/src/BasicTable.vue
+++ src/components/Table/src/BasicTable.vue
@@ -28,10 +28,6 @@
       <template #headerCell="{ column }">
         <HeaderCell :column="column" />
       </template>
-      <!-- 增加对antdv3.x兼容 -->
-      <template #bodyCell="data">
-        <slot name="bodyCell" v-bind="data || {}"></slot>
-      </template>
       <!--      <template #[`header-${column.dataIndex}`] v-for="(column, index) in columns" :key="index">-->
       <!--        <HeaderCell :column="column" />-->
       <!--      </template>-->
