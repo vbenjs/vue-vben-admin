03e7d67c8a032a0321de264defc386c812cc2e8a
quyanan <929188726@qq.com>
2022-07-07 14:55:09 +0800

    fix(expandtable): 恢复可展开表格Demo,修复开启可展开表格后横向滚动失效问题

--- src/views/demo/table/ExpandTable.vue
+++ src/views/demo/table/ExpandTable.vue
@@ -1,14 +1,14 @@
 <template>
   <PageWrapper
     title="可展开表格"
-    content="不可与scroll共用。TableAction组件可配置stopButtonPropagation来阻止操作按钮的点击事件冒泡，以便配合Table组件的expandRowByClick"
+    content="TableAction组件可配置stopButtonPropagation来阻止操作按钮的点击事件冒泡，以便配合Table组件的expandRowByClick"
   >
     <BasicTable @register="registerTable">
+      <template #expandedRowRender="{ record }">
+        <span>No:{{ record.no }}</span>
+      </template>
       <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'no'">
-          <span>No: {{ record.no }} </span>
-        </template>
-        <template v-else-if="column.key === 'action'">
+        <template v-if="column.key === 'action'">
           <TableAction
             stopButtonPropagation
             :actions="[
@@ -56,6 +56,7 @@
           width: 160,
           title: 'Action',
           dataIndex: 'action',
+          fixed: 'right',
           // slots: { customRender: 'action' },
         },
       });
