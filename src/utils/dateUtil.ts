d8f4cdf7bdf772324d2b64d993c6ad9049577ded
LingYan <120710372@qq.com>
2022-08-11 10:49:12 +0800

    fix: 修复 date 类型定义不完整

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -7,16 +7,13 @@ const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const DATE_FORMAT = 'YYYY-MM-DD';
 
 export function formatToDateTime(
-  date: dayjs.Dayjs | undefined = undefined,
+  date: dayjs.ConfigType = undefined,
   format = DATE_TIME_FORMAT,
 ): string {
   return dayjs(date).format(format);
 }
 
-export function formatToDate(
-  date: dayjs.Dayjs | undefined = undefined,
-  format = DATE_FORMAT,
-): string {
+export function formatToDate(date: dayjs.ConfigType = undefined, format = DATE_FORMAT): string {
   return dayjs(date).format(format);
 }
 
