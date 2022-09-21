ae74852211f2deb88d1140c1987581abf5a7556a
saber <1558365539@qq.com>
2022-09-21 14:35:08 +0800

    fix: 修改外联路由打包bug

--- src/router/routes/mainOut.ts
+++ src/router/routes/mainOut.ts
@@ -3,7 +3,7 @@ The routing of this file will not show the layout.
 It is an independent new page.
 the contents of the file still need to log in to access
  */
-import type { AppRouteModule } from '/@/router/types';
+import type { AppRouteModule } from '/@/router/types'
 
 // test
 // http:ip:port/main-out
@@ -11,12 +11,12 @@ export const mainOutRoutes: AppRouteModule[] = [
   {
     path: '/main-out',
     name: 'MainOut',
-    component: () => import('/@/views/demo/main-out/index.vue'),
+    component: '/@/views/demo/main-out/index.vue',
     meta: {
       title: 'MainOut',
       ignoreAuth: true,
     },
   },
-];
+]
 
-export const mainOutRouteNames = mainOutRoutes.map((item) => item.name);
+export const mainOutRouteNames = mainOutRoutes.map((item) => item.name)
