2abc8275c6f9c3d52361b75d7e9fe73bcae85a5f
adeewu <adeewu@outlook.com>
2022-09-29 10:10:18 +0800

    修复调试位置与代码映射位置不一致问题 (#2228)

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -5,7 +5,7 @@ import legacy from '@vitejs/plugin-legacy';
 import purgeIcons from 'vite-plugin-purge-icons';
 import windiCSS from 'vite-plugin-windicss';
 import VitePluginCertificate from 'vite-plugin-mkcert';
-import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+//import vueSetupExtend from 'vite-plugin-vue-setup-extend';
 import { configHtmlPlugin } from './html';
 import { configPwaConfig } from './pwa';
 import { configMockPlugin } from './mock';
@@ -31,7 +31,7 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
     // have to
     vueJsx(),
     // support name
-    vueSetupExtend(),
+    //vueSetupExtend(),
     VitePluginCertificate({
       source: 'coding',
     }),
