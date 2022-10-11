e83a5970db10a1dff4544275d6d2ceb1976dffad
Lovell Liu <lovelliu@foxmail.com>
2022-10-11 14:15:38 +0800

    fix: prevent layout chaos because of text wrap

--- src/views/dashboard/workbench/components/QuickNav.vue
+++ src/views/dashboard/workbench/components/QuickNav.vue
@@ -3,7 +3,7 @@
     <CardGrid v-for="item in navItems" :key="item.title">
       <span class="flex flex-col items-center">
         <Icon :icon="item.icon" :color="item.color" size="20" />
-        <span class="text-md mt-2">{{ item.title }}</span>
+        <span class="text-md mt-2 truncate">{{ item.title }}</span>
       </span>
     </CardGrid>
   </Card>
