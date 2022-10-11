e6c0bc2f0a282d6b62fc5c017af75cfce477ee6c
Lovell Liu <lovelliu@foxmail.com>
2022-10-11 14:09:42 +0800

    fix: fix display issues when reloading page

--- src/views/dashboard/workbench/components/QuickNav.vue
+++ src/views/dashboard/workbench/components/QuickNav.vue
@@ -1,6 +1,6 @@
 <template>
-  <Card title="快捷导航" v-bind="$attrs">
-    <CardGrid v-for="item in navItems" :key="item">
+  <Card title="快捷导航">
+    <CardGrid v-for="item in navItems" :key="item.title">
       <span class="flex flex-col items-center">
         <Icon :icon="item.icon" :color="item.color" size="20" />
         <span class="text-md mt-2">{{ item.title }}</span>
@@ -9,9 +9,7 @@
   </Card>
 </template>
 <script lang="ts" setup>
-  import { Card } from 'ant-design-vue';
+  import { Card, CardGrid } from 'ant-design-vue';
   import { navItems } from './data';
   import { Icon } from '/@/components/Icon';
-
-  const CardGrid = Card.Grid;
 </script>
