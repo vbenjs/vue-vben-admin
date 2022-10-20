09899304d15bb294b338cc7f9de7b8bae1ff6653
CherishTao <88085841+CherishMvp@users.noreply.github.com>
2022-10-20 09:02:39 +0800

    Update layout.ts

--- src/locales/lang/en/layout.ts
+++ src/locales/lang/en/layout.ts
@@ -3,7 +3,7 @@ export default {
   header: {
     // user dropdown
     dropdownItemDoc: 'Document',
-    dropdownItemLoginOut: 'Login Out',
+    dropdownItemLoginOut: 'Log Out',
 
     tooltipErrorLog: 'Error log',
     tooltipLock: 'Lock screen',
