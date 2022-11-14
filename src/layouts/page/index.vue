89a55bf09fbc57eebf15057c6f4458c4c701260c
CyrusZhou <lsqtzj@msn.com>
2022-11-14 18:50:16 +0100

    fix: 频繁切换页面导致灰屏

--- src/layouts/page/index.vue
+++ src/layouts/page/index.vue
@@ -15,7 +15,9 @@
         appear
       >
         <keep-alive v-if="openCache" :include="getCaches">
-          <component :is="Component" :key="route.fullPath" />
+          <div :key="route.name">
+            <component :is="Component" :key="route.fullPath" />
+          </div>
         </keep-alive>
         <div v-else :key="route.name">
           <component :is="Component" :key="route.fullPath" />
