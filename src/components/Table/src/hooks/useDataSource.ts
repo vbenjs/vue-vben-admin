b07dfa2e2aedef4cef13c643b5dd148ca088321f
coolliyong <yonglongli163@163.com>
2023-01-03 10:32:58 +0800

    fix: table-datasource，如果后端返回的page total是字符串，会导致页码乱掉

--- src/components/Table/src/hooks/useDataSource.ts
+++ src/components/Table/src/hooks/useDataSource.ts
@@ -310,7 +310,7 @@ export function useDataSource(
       const resultTotal: number = isArrayResult ? res.length : get(res, totalField);
 
       // 假如数据变少，导致总页数变少并小于当前选中页码，通过getPaginationRef获取到的页码是不正确的，需获取正确的页码再次执行
-      if (resultTotal) {
+      if (Number(resultTotal)) {
         const currentTotalPage = Math.ceil(resultTotal / pageSize);
         if (current > currentTotalPage) {
           setPagination({
