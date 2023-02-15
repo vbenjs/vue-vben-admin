ef9cff328713f442e2f94474fd2babdec0cf5c7e
houtaroy <houtaroy@gmail.com>
2023-02-15 12:27:29 +0800

    ✨ feat(适配后端): 适配考拉后端
    1. 调整登录模式为OAuth2
    2. 调整接口请求, 包括状态码/返回值解析/分页参数等
    3. 去除部分检查限制

---
+++ src/apis/security/OAuth2TokenResponse.ts
@@ -0,0 +1,5 @@
+export interface OAuth2TokenResponse {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+}
---
+++ src/apis/security/index.ts
@@ -0,0 +1,31 @@
+import { OAuth2TokenResponse } from './OAuth2TokenResponse';
+import { oauth2Http } from '/@/utils/http/axios';
+import qs from 'qs';
+
+/**
+ * 客户端认证
+ */
+export function authorize() {
+  const server = import.meta.env.VITE_GLOB_OAUTH2_SERVER;
+  const clientId = import.meta.env.VITE_GLOB_OAUTH2_CLIENT_ID;
+  const redirectUri = window.location.origin + window.location.pathname;
+  window.location.href = `${server}/oauth2/authorize?response_type=code&client_id=${clientId}&scope=all&state=${new Date().getTime()}&redirect_uri=${redirectUri}`;
+}
+
+/**
+ * 授权码登录
+ * @param code 授权码
+ */
+export function loginByAuthorizationCode(code: string) {
+  return oauth2Http.post<OAuth2TokenResponse>({
+    url: '/token',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify({
+      grant_type: 'authorization_code',
+      client_id: import.meta.env.VITE_GLOB_OAUTH2_CLIENT_ID,
+      client_secret: import.meta.env.VITE_GLOB_OAUTH2_CLIENT_SECRET,
+      redirect_uri: window.location.origin + window.location.pathname,
+      code: code,
+    }),
+  });
+}
