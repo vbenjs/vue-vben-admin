058d867457d3369f0dddb7987d62040bc5d3d26f
ZhangZhiChao <54631354+Alkaidcc@users.noreply.github.com>
2023-02-19 14:25:09 +0800

    fix: 重置表单时移除URL中的参数 (#2559)

--- src/components/Form/src/hooks/useFormValues.ts
+++ src/components/Form/src/hooks/useFormValues.ts
@@ -76,7 +76,12 @@ export function useFormValues({
       }
       // Remove spaces
       if (isString(value)) {
-        value = value.trim();
+        // remove params from URL
+        if(value === '') {
+          value = undefined;
+        }else {
+          value = value.trim();
+        }
       }
       if (!tryDeconstructArray(key, value, res) && !tryDeconstructObject(key, value, res)) {
         // 没有解构成功的，按原样赋值
