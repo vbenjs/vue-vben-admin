00ad01e9856e43908b87c5679c74d357f368626c
luocong2016 <luocong2016@126.com>
2023-02-27 12:50:11 +0800

    fix: 组件属性优先默认 (#2581)

--- src/components/Form/src/components/FormItem.vue
+++ src/components/Form/src/components/FormItem.vue
@@ -80,10 +80,14 @@
           componentProps = componentProps({ schema, tableAction, formModel, formActionType }) ?? {};
         }
         if (schema.component === 'Divider') {
-          componentProps = Object.assign({ type: 'horizontal' }, componentProps, {
-            orientation: 'left',
-            plain: true,
-          });
+          componentProps = Object.assign(
+            { type: 'horizontal' },
+            {
+              orientation: 'left',
+              plain: true,
+            },
+            componentProps,
+          );
         }
         return componentProps as Recordable;
       });
