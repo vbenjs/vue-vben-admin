e1136aaf93b9e045b8a996fd72de2990fbacf052
houtaroy <houtaroy@gmail.com>
2023-03-22 11:54:53 +0800

    🦄 refactor(系统管理): 重构角色管理页面

--- src/views/system/department/DepartmentTree.vue
+++ src/views/system/department/DepartmentTree.vue
@@ -3,25 +3,14 @@
 
   import { BasicTree, TreeItem } from '/@/components/Tree';
 
-  import TreeNode from '/@/apis/TreeNode';
   import { listDepartmentTree } from '/@/apis/departments';
+
+  import { treeNode2TreeData } from '../helpers';
 
   const treeData = ref<TreeItem[]>([]);
 
   async function fetch() {
     treeData.value = treeNode2TreeData(await listDepartmentTree());
-  }
-
-  function treeNode2TreeData(treeNodes: TreeNode[]): TreeItem[] {
-    const result: TreeItem[] = [];
-    treeNodes.forEach((node) => {
-      const temp: TreeItem = { key: node.id, title: node.name };
-      if (node.children) {
-        temp.children = treeNode2TreeData(node.children);
-      }
-      result.push(temp);
-    });
-    return result;
   }
 
   onBeforeMount(() => {
--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -64,20 +64,20 @@
               {
                 icon: 'clarity:note-edit-line',
                 tooltip: '编辑',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 onClick: handleEdit.bind(null, record),
               },
               {
                 icon: 'ant-design:setting-outlined',
                 tooltip: '角色配置',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 onClick: handleSetting.bind(null, record),
               },
               {
                 icon: 'ant-design:delete-outlined',
                 tooltip: '删除',
                 color: 'error',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 popConfirm: {
                   title: '是否确认删除',
                   placement: 'left',
