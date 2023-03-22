aad7dd1c914eba69e4bf2520d6b9896952bdbb56
houtaroy <houtaroy@gmail.com>
2023-03-22 12:58:14 +0800

    🦄 refactor(系统管理): 重构用户管理页面

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -85,26 +85,26 @@
               {
                 icon: 'clarity:note-edit-line',
                 tooltip: '编辑',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 onClick: handleEdit.bind(null, record),
               },
               {
                 icon: 'mingcute:department-line',
                 tooltip: '部门配置',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 onClick: handleDepartmentSetting.bind(null, record),
               },
               {
                 icon: 'eos-icons:role-binding-outlined',
                 tooltip: '角色配置',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 onClick: handleRoleSetting.bind(null, record),
               },
               {
                 icon: 'ant-design:delete-outlined',
                 color: 'error',
                 tooltip: '删除',
-                ifShow: record.isSystem === YesNo.NO,
+                ifShow: record.isSystemic === YesNo.NO,
                 popConfirm: {
                   title: '是否确认删除',
                   placement: 'left',
