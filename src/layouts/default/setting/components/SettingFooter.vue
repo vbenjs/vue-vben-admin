302afe5860e575aefe84a92907a104074b809299
Wit〆苗大 <454690789@qq.com>
2023-03-30 09:53:44 +0800

    fix(项目配置): 修复 #2652 重置项目配置，导致左侧菜单栏整体变成白色 (#2656)
    Co-authored-by: 苗大 <caoshengmiao@hypergryph.com>

--- src/layouts/default/setting/components/SettingFooter.vue
+++ src/layouts/default/setting/components/SettingFooter.vue
@@ -35,6 +35,7 @@
   import { updateGrayMode } from '/@/logics/theme/updateGrayMode';
   import defaultSetting from '/@/settings/projectSetting';
   import { changeTheme } from '/@/logics/theme';
+  import { updateSidebarBgColor } from '/@/logics/theme/updateBackground';
 
   export default defineComponent({
     name: 'SettingFooter',
@@ -63,6 +64,7 @@
           appStore.setProjectConfig(defaultSetting);
           const { colorWeak, grayMode, themeColor } = defaultSetting;
           changeTheme(themeColor);
+          updateSidebarBgColor();
           updateColorWeak(colorWeak);
           updateGrayMode(grayMode);
           createMessage.success(t('layout.setting.resetSuccess'));
