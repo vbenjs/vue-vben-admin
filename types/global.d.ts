68ae53fff6f7b3b989101f535219794b06b7fe9e
vben <anncwb@126.com>
2023-04-01 23:48:19 +0800

    chore: remove @vitejs/plugin-legacy vite-plugin-imagemin

--- types/global.d.ts
+++ types/global.d.ts
@@ -67,9 +67,6 @@ declare global {
     VITE_DROP_CONSOLE: boolean;
     VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
     VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
-    VITE_LEGACY: boolean;
-    VITE_USE_IMAGEMIN: boolean;
-    VITE_GENERATE_UI: string;
   }
 
   declare function parseInt(s: string | number, radix?: number): number;
