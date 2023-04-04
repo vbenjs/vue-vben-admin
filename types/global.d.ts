08d4e34b778caf6f92f02f038f7230fbfbdea885
vben <anncwb@126.com>
2023-04-04 17:13:09 +0800

    chore: config ts-config

--- types/global.d.ts
+++ types/global.d.ts
@@ -63,7 +63,6 @@ declare global {
     VITE_GLOB_APP_TITLE: string;
     VITE_GLOB_APP_SHORT_NAME: string;
     VITE_USE_CDN: boolean;
-    VITE_DROP_CONSOLE: boolean;
     VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none';
     VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
   }
