02c460a902057ee703f54db6ffe1d06595c50e69
luoawai123 <luoawai@163.com>
2023-04-07 01:57:06 +0800

    feat(directives/ellipsis): add ellipsis directive

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -4,8 +4,10 @@
 import type { App } from 'vue';
 import { setupPermissionDirective } from './permission';
 import { setupLoadingDirective } from './loading';
+import { setupEllipsisDirective } from './ellipsis';
 
 export function setupGlobDirectives(app: App) {
   setupPermissionDirective(app);
   setupLoadingDirective(app);
+  setupEllipsisDirective(app);
 }
