d45325a6042125ea260102b02024ee91b52733fe
kissGoodBye <33819035+luoawai123@users.noreply.github.com>
2023-04-07 09:22:51 +0800

    feat(directives/ellipsis): add ellipsis directive (#2688)

---
+++ src/directives/ellipsis.ts
@@ -0,0 +1,42 @@
+import type { CSSProperties, DirectiveBinding, ObjectDirective, App } from 'vue';
+
+interface IValue {
+  width?: number;
+  line?: number;
+}
+
+interface IOptions {
+  [key: string]: CSSProperties;
+}
+
+const cssProperties: IOptions = {
+  single: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+  multiple: {
+    display: '-webkit-box',
+    overflow: 'hidden',
+    wordBreak: 'break-all',
+  },
+};
+
+const Ellipsis: ObjectDirective = {
+  mounted(el: HTMLElement, binding: DirectiveBinding<Array<IValue>>) {
+    const { value = [100, 1], arg = 'single' } = binding;
+    const [width, line] = value;
+    Object.entries(cssProperties[arg]).forEach(([key, value]) => {
+      el.style[key] = value;
+    });
+    el.style.width = `${width}px`;
+    if (arg === 'multiple') {
+      el.style.webkitLineClamp = `${line}`;
+      el.style.webkitBoxOrient = 'vertical';
+    }
+  },
+};
+export function setupEllipsisDirective(app: App) {
+  app.directive('ellipsis', Ellipsis);
+}
+export default Ellipsis;
