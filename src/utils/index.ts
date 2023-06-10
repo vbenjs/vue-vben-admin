1f287145f477e609b80b5a59b7787d5cd1a66213
Kirk Lin <linkirk@163.com>
2023-06-10 17:19:18 +0800

    fix(deepMerge): 去掉合并错误的代码 (#2848)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -52,14 +52,6 @@ export function deepMerge<T extends object | null | undefined, U extends object 
   target: U,
   mergeArrays: 'union' | 'intersection' | 'concat' | 'replace' = 'replace',
 ): T & U {
-
-  return mergeWith(cloneDeep(target), source, (objValue, srcValue) => {
-    if (isObject(objValue) && isObject(srcValue)) {
-      return mergeWith(cloneDeep(objValue), srcValue, (prevValue, nextValue) => {
-        // 如果是数组，合并数组(去重) If it is an array, merge the array (remove duplicates)
-        return isArray(prevValue) ? unionWith(prevValue, nextValue, isEqual) : undefined;
-      });
-
   if (!target) {
     return source as T & U;
   }
