58ce1a5ad0d7023b11687d42047f679c5da6e80c
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2023-06-17 09:42:42 +0800

    perf: 组件修改为setup

--- src/layouts/default/trigger/HeaderTrigger.vue
+++ src/layouts/default/trigger/HeaderTrigger.vue
@@ -3,23 +3,15 @@
     <MenuUnfoldOutlined v-if="getCollapsed" /> <MenuFoldOutlined v-else />
   </span>
 </template>
-<script lang="ts">
-  import { defineComponent } from 'vue';
+<script lang="ts" setup>
   import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue';
   import { useMenuSetting } from '/@/hooks/setting/useMenuSetting';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { propTypes } from '/@/utils/propTypes';
 
-  export default defineComponent({
-    name: 'HeaderTrigger',
-    components: { MenuUnfoldOutlined, MenuFoldOutlined },
-    props: {
-      theme: propTypes.oneOf(['light', 'dark']),
-    },
-    setup() {
-      const { getCollapsed, toggleCollapsed } = useMenuSetting();
-      const { prefixCls } = useDesign('layout-header-trigger');
-      return { getCollapsed, toggleCollapsed, prefixCls };
-    },
+  defineProps({
+    theme: propTypes.oneOf(['light', 'dark']),
   });
+  const { getCollapsed, toggleCollapsed } = useMenuSetting();
+  const { prefixCls } = useDesign('layout-header-trigger');
 </script>
