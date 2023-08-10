6fbc552ed1054d24f92c14a46ef3b926fe60904f
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2023-08-10 11:52:14 +0800

    fix: ci

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -26,12 +26,12 @@ export function getStorageShortName() {
 
 export function getAppEnvConfig() {
   const ENV_NAME = getVariableName(import.meta.env.VITE_GLOB_APP_TITLE);
-
+  console.log(ENV_NAME);
   const ENV = import.meta.env.DEV
     ? // Get the global configuration (the configuration will be extracted independently when packaging)
       (import.meta.env as unknown as GlobEnvConfig)
-    : (window[ENV_NAME as any] as unknown as GlobEnvConfig);
-
+    : (window[ENV_NAME] as unknown as GlobEnvConfig);
+  console.log(ENV);
   const { VITE_GLOB_APP_TITLE, VITE_GLOB_API_URL, VITE_GLOB_API_URL_PREFIX, VITE_GLOB_UPLOAD_URL } =
     ENV;
 
