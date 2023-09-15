015a7a61d54f8c9d52e7562effc753f057785502
Li Kui <90845831+likui628@users.noreply.github.com>
2023-09-15 15:56:06 +0800

    fix: type check failed for prop 'onClick' (#3028)

--- src/components/Form/src/components/FormAction.vue
+++ src/components/Form/src/components/FormAction.vue
@@ -14,13 +14,7 @@
         </Button>
         <slot name="submitBefore"></slot>
 
-        <Button
-          type="primary"
-          class="mr-2"
-          v-bind="getSubmitBtnOptions"
-          @click="submitAction"
-          v-if="showSubmitButton"
-        >
+        <Button type="primary" class="mr-2" v-bind="getSubmitBtnOptions" v-if="showSubmitButton">
           {{ getSubmitBtnOptions.text }}
         </Button>
 
@@ -41,7 +35,6 @@
 </template>
 <script lang="ts">
   import type { ColEx } from '../types/index';
-  //import type { ButtonProps } from 'ant-design-vue/es/button/buttonTypes';
   import { defineComponent, computed, PropType } from 'vue';
   import { Form, Col } from 'ant-design-vue';
   import { Button, ButtonProps } from '/@/components/Button';
