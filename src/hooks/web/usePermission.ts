99ddc3598a20e1b2261d6e86bcd53cecd957c3c3
luchanan <398778994@qq.com>
2023-09-17 12:08:17 +0800

    perf: 指令权限v-auth支持并显示按钮权限 (#3035)

--- src/hooks/web/usePermission.ts
+++ src/hooks/web/usePermission.ts
@@ -75,6 +75,14 @@ export function usePermission() {
     if (PermissionModeEnum.BACK === permMode) {
       const allCodeList = permissionStore.getPermCodeList as string[];
       if (!isArray(value)) {
+        const splits = ['||', '&&'];
+        const splitName = splits.find((item) => value.includes(item));
+        if (splitName) {
+          const splitCodes = value.split(splitName);
+          return splitName === splits[0]
+            ? intersection(splitCodes, allCodeList).length > 0
+            : intersection(splitCodes, allCodeList).length === splitCodes.length;
+        }
         return allCodeList.includes(value);
       }
       return (intersection(value, allCodeList) as string[]).length > 0;
