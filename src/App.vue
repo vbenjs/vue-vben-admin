982952e45c3bd8be2fac5482061fd56d8c0ee976
invalid w <wangjuesix@gmail.com>
2023-09-20 12:37:02 +0800

    perf(style): 使用antd4的暗黑主题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <ConfigProvider :locale="getAntdLocale">
+  <ConfigProvider :locale="getAntdLocale" :theme="isDark ? darkTheme : {}">
     <AppProvider>
       <RouterView />
     </AppProvider>
@@ -7,15 +7,25 @@
 </template>
 
 <script lang="ts" setup>
-  import { ConfigProvider } from 'ant-design-vue';
+  import { ConfigProvider, theme } from 'ant-design-vue';
   import { AppProvider } from '@/components/Application';
   import { useTitle } from '@/hooks/web/useTitle';
   import { useLocale } from '@/locales/useLocale';
+  import { useRootSetting } from '/@/hooks/setting/useRootSetting';
   import 'dayjs/locale/zh-cn';
+  import { computed } from 'vue';
+  import { ThemeEnum } from '/@/enums/appEnum';
 
+  const { darkAlgorithm } = theme;
   // support Multi-language
   const { getAntdLocale } = useLocale();
+  const { getDarkMode } = useRootSetting();
 
+  const isDark = computed(() => getDarkMode.value === ThemeEnum.DARK);
+
+  const darkTheme = {
+    algorithm: [darkAlgorithm],
+  };
   // Listening to page changes and dynamically changing site titles
   useTitle();
 </script>
