a63a10c047cda32d92f35780163772fb20a6fe7a
invalid w <wangjuesix@gmail.com>
2023-09-27 16:38:59 +0800

    fix: 修复rule validator类型默认为string，导致 radio 等组件在 setFormValues 时，如果值不是string类型，提示校验错误

--- src/components/Form/src/helper.ts
+++ src/components/Form/src/helper.ts
@@ -40,6 +40,9 @@ export function setComponentRuleType(
   component: ComponentType,
   valueFormat: string,
 ) {
+  if (Reflect.has(rule, 'type')) {
+    return;
+  }
   if (['DatePicker', 'MonthPicker', 'WeekPicker', 'TimePicker'].includes(component)) {
     rule.type = valueFormat ? 'string' : 'object';
   } else if (['RangePicker', 'Upload', 'CheckboxGroup', 'TimePicker'].includes(component)) {
