c315aa980175a5292d13bae629afb90366a540e6
黄小民 <DesignHhuang@users.noreply.github.com>
2023-09-28 21:38:36 +0800

    vite-config中将ant-design-vue升级到版本4 (#3089)
    * fix: #3077 最新代码 ApiTransfer编辑后无法正常显示数据
    
    * feat: 升级ant-design-vue至4.0.3
    
    * feat: vite-config up to antd v4
    
    * fix: 去掉console

--- internal/vite-config/src/utils/modifyVars.ts
+++ internal/vite-config/src/utils/modifyVars.ts
@@ -2,7 +2,11 @@ import { resolve } from 'node:path';
 
 import { generate } from '@ant-design/colors';
 // @ts-ignore: typo
-import { getThemeVariables } from 'ant-design-vue/dist/theme';
+/* import { getThemeVariables } from 'ant-design-vue/dist/theme'; */
+import { theme } from 'ant-design-vue/lib';
+import convertLegacyToken from 'ant-design-vue/lib/theme/convertLegacyToken';
+
+const { defaultAlgorithm, defaultSeed } = theme;
 
 const primaryColor = '#0960bd';
 
@@ -25,11 +29,14 @@ export function generateModifyVars() {
     primaryColorObj[`primary-${index + 1}`] = palettes[index];
   }
 
-  const modifyVars = getThemeVariables();
+  // const modifyVars = getThemeVariables();
+  const mapToken = defaultAlgorithm(defaultSeed);
+  const v3Token = convertLegacyToken(mapToken);
+
   return {
-    ...modifyVars,
+    ...v3Token,
     // reference:  Avoid repeated references
-    hack: `${modifyVars.hack} @import (reference) "${resolve('src/design/config.less')}";`,
+    hack: `true; @import (reference) "${resolve('src/design/config.less')}";`,
     'primary-color': primary,
     ...primaryColorObj,
     'info-color': primary,
