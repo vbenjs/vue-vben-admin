5e0d2daa11e332f94c64379fa603e4491b0395f4
Kirk Lin <linkirk@163.com>
2023-10-08 17:31:55 +0800

    refactor: use `unplugin-config` (#3106)
    * refactor: use `unplugin-config`
    
    * fix: preserve indentation in generated code

--- internal/vite-config/src/utils/hash.ts
+++ internal/vite-config/src/utils/hash.ts
@@ -4,5 +4,13 @@ function createContentHash(content: string, hashLSize = 12) {
   const hash = createHash('sha256').update(content);
   return hash.digest('hex').slice(0, hashLSize);
 }
+function strToHex(str: string) {
+  const result: string[] = [];
+  for (let i = 0; i < str.length; ++i) {
+    const hex = str.charCodeAt(i).toString(16);
+    result.push(('000' + hex).slice(-4));
+  }
+  return result.join('').toUpperCase();
+}
 
-export { createContentHash };
+export { createContentHash, strToHex };
