d7f65edcd11018b5026209775bc8db75fa4d8a5c
Kirk Lin <linkirk@163.com>
2023-10-08 16:22:46 +0800

    fix: preserve indentation in generated code

--- internal/vite-config/src/plugins/appConfig.ts
+++ internal/vite-config/src/plugins/appConfig.ts
@@ -1,13 +1,14 @@
 import GenerateConfig from 'unplugin-config/vite';
 import { type PluginOption } from 'vite';
 
+import { getEnvConfig } from '../utils/env';
 import { strToHex } from '../utils/hash';
 
 const GLOBAL_CONFIG_FILE_NAME = '_app.config.js';
 // This constant sets the output directory for the Vite package
 const OUTPUT_DIR = 'dist';
-
-const APP_NAME = strToHex('Vben Admin');
+const config: any = getEnvConfig().then();
+const APP_NAME = strToHex(config?.VITE_GLOB_APP_TITLE ?? 'Vben Admin');
 export function createConfigPluginConfig(shouldGenerateConfig: boolean): PluginOption {
   // https://github.com/kirklin/unplugin-config
   return GenerateConfig({
