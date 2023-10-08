694dead3115a535782b7dd3a3c2dc4eaa9f32d76
invalid w <wangjuesix@gmail.com>
2023-10-08 18:09:59 +0800

    Revert "refactor: use `unplugin-config` (#3106)"
    This reverts commit a712a8e5a088da59b32343eb23819ea9040830ef.

--- internal/vite-config/src/plugins/index.ts
+++ internal/vite-config/src/plugins/index.ts
@@ -3,7 +3,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import { type PluginOption } from 'vite';
 import purgeIcons from 'vite-plugin-purge-icons';
 
-import { createConfigPluginConfig } from './appConfig';
+import { createAppConfigPlugin } from './appConfig';
 import { configCompressPlugin } from './compress';
 import { configHtmlPlugin } from './html';
 import { configMockPlugin } from './mock';
@@ -18,10 +18,10 @@ interface Options {
   enableAnalyze?: boolean;
 }
 
-async function createPlugins({ isBuild, enableMock, compress, enableAnalyze }: Options) {
+async function createPlugins({ isBuild, root, enableMock, compress, enableAnalyze }: Options) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()];
 
-  const appConfigPlugin = await createConfigPluginConfig(isBuild);
+  const appConfigPlugin = await createAppConfigPlugin({ root, isBuild });
   vitePlugins.push(appConfigPlugin);
 
   // vite-plugin-html
