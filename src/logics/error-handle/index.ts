cea297ce888e1a1cef5a10736fd9b389ab3135fa
invalid w <wangjuesix@gmail.com>
2023-10-09 12:02:53 +0800

    chore(error-handle): 处理vue errorHandler绑定事件的参数类型不一致

--- src/logics/error-handle/index.ts
+++ src/logics/error-handle/index.ts
@@ -62,16 +62,15 @@ function formatComponentName(vm: any) {
 /**
  * Configure Vue error handling function
  */
-
-function vueErrorHandler(err: Error, vm: any, info: string) {
+function vueErrorHandler(err: unknown, vm: any, info: string) {
   const errorLogStore = useErrorLogStoreWithOut();
   const { name, path } = formatComponentName(vm);
   errorLogStore.addErrorLogInfo({
     type: ErrorTypeEnum.VUE,
     name,
     file: path,
-    message: err.message,
-    stack: processStackMsg(err),
+    message: (err as Error).message,
+    stack: processStackMsg(err as Error),
     detail: info,
     url: window.location.href,
   });
