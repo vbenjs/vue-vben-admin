e8e39ab9b9b844c2ccadc5d8dea0037f6be8931c
415313629@qq.com <415313629@qq.com>
2023-10-12 14:32:46 +0800

    chore: fix type:check error

--- src/views/demo/table/EditRowTable.vue
+++ src/views/demo/table/EditRowTable.vue
@@ -3,7 +3,7 @@
     <BasicTable @register="registerTable" @edit-change="onEditChange">
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
-          <TableAction :actions="createActions(record, column)" />
+          <TableAction :actions="createActions(record)" />
         </template>
       </template>
     </BasicTable>
@@ -278,7 +278,7 @@
         }
       }
 
-      function createActions(record: EditRecordRow, column: BasicColumn): ActionItem[] {
+      function createActions(record: EditRecordRow): ActionItem[] {
         if (!record.editable) {
           return [
             {
@@ -291,13 +291,13 @@
         return [
           {
             label: '保存',
-            onClick: handleSave.bind(null, record, column),
+            onClick: handleSave.bind(null, record),
           },
           {
             label: '取消',
             popConfirm: {
               title: '是否取消编辑',
-              confirm: handleCancel.bind(null, record, column),
+              confirm: handleCancel.bind(null, record),
             },
           },
         ];
