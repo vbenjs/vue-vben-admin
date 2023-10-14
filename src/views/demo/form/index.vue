d4f2641b9a4434e3da8ffd2f5bd2d6bb55216307
invalid w <wangjuesix@gmail.com>
2023-10-14 11:19:58 +0800

    chore(Demo): restore ApiTree schema show

--- src/views/demo/form/index.vue
+++ src/views/demo/form/index.vue
@@ -510,27 +510,27 @@
         span: 8,
       },
     },
-    // {
-    //   field: 'field36',
-    //   component: 'ApiTree',
-    //   label: '远程Tree',
-    //   helpMessage: ['ApiTree组件', '使用接口提供的数据生成选项'],
-    //   required: true,
-    //   componentProps: {
-    //     api: treeOptionsListApi,
-    //     params: {
-    //       count: 2,
-    //     },
-    //     afterFetch: (v) => {
-    //       //do something
-    //       return v;
-    //     },
-    //     resultField: 'list',
-    //   },
-    //   colProps: {
-    //     span: 8,
-    //   },
-    // },
+    {
+      field: 'field36',
+      component: 'ApiTree',
+      label: '远程Tree',
+      helpMessage: ['ApiTree组件', '使用接口提供的数据生成选项'],
+      required: true,
+      componentProps: {
+        api: treeOptionsListApi,
+        params: {
+          count: 2,
+        },
+        afterFetch: (v) => {
+          //do something
+          return v;
+        },
+        resultField: 'list',
+      },
+      colProps: {
+        span: 8,
+      },
+    },
     {
       field: 'divider-linked',
       component: 'Divider',
