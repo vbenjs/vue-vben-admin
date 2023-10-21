6569d84f57e1b3e7d773ad87a5eb84fc62abf206
invalid w <wangjuesix@gmail.com>
2023-10-21 11:48:38 +0800

    fix(BasicTree):  not inherit slot and not show icon slot. close #1902

--- src/components/Tree/src/TreeIcon.ts
+++ src/components/Tree/src/TreeIcon.ts
@@ -3,7 +3,7 @@ import { h } from 'vue';
 import { isString } from 'lodash-es';
 import Icon from '@/components/Icon/Icon.vue';
 
-export const TreeIcon = ({ icon }: { icon: VNode | string }) => {
+export const TreeIcon = ({ icon }: { icon: VNode | string | undefined }) => {
   if (!icon) return null;
   if (isString(icon)) {
     return h(Icon, { icon, class: 'mr-1' });
