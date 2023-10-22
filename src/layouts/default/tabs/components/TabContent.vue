6d62104c74994bae10d77a5978043aedb8bd2259
freespace8 <f8_soft@qq.com>
2023-10-21 21:40:41 -0500

    fix(tabs): 修复标签页快捷菜单缩小问题

--- src/layouts/default/tabs/components/TabContent.vue
+++ src/layouts/default/tabs/components/TabContent.vue
@@ -2,7 +2,7 @@
   <Dropdown
     :dropMenuList="getDropMenuList"
     :trigger="getTrigger"
-    placement="bottom"
+    placement="bottomRight"
     overlayClassName="multiple-tabs__dropdown"
     @menu-event="handleMenuEvent"
   >
