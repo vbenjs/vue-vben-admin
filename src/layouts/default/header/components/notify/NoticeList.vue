13b080f1333014e010ddcc6f37a032f35399890a
bowen <54492610+jiaowoxiaobala@users.noreply.github.com>
2023-10-25 11:55:14 +0800

    chore(ATypographyParagraph): fix type:check (#3187)

--- src/layouts/default/header/components/notify/NoticeList.vue
+++ src/layouts/default/header/components/notify/NoticeList.vue
@@ -60,13 +60,22 @@
   import { List, Avatar, Tag, Typography } from 'ant-design-vue';
   import { isNumber } from '/@/utils/is';
 
+  // types
+  import type { StyleValue } from '/@/utils/types';
+  import type { FunctionalComponent } from 'vue';
+  import type { ParagraphProps } from 'ant-design-vue/es/typography/Paragraph';
+
   export default defineComponent({
     components: {
       [Avatar.name]: Avatar,
       [List.name]: List,
       [List.Item.name]: List.Item,
       AListItemMeta: List.Item.Meta,
-      ATypographyParagraph: Typography.Paragraph,
+      ATypographyParagraph: Typography.Paragraph as FunctionalComponent<
+        ParagraphProps & {
+          style?: StyleValue;
+        }
+      >,
       [Tag.name]: Tag,
     },
     props: {
