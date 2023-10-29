0d8be72e0b3b6bbd2fe1f9374bdc0b1253253ecb
zwtvip <zwtvip@qq.com>
2023-10-29 16:21:48 +0800

    fix: 修复黑暗模式下一些样式问题

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -110,12 +110,18 @@
       -webkit-text-fill-color: #c9d1d9 !important;
       box-shadow: inherit !important;
     }
+
+    .ant-divider-inner-text {
+      color: @text-color-secondary;
+      font-size: 12px;
+    }
   }
 
   .@{prefix-cls} {
     min-height: 100%;
     overflow: hidden;
 
+    /* stylelint-disable-next-line media-query-no-invalid */
     @media (max-width: @screen-xl) {
       background-color: #293146;
 
@@ -136,7 +142,7 @@
       background-repeat: no-repeat;
       background-position: 100%;
       background-size: auto 100%;
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-xl) {
         display: none;
       }
@@ -188,19 +194,19 @@
 
     input:not([type='checkbox']) {
       min-width: 360px;
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-xl) {
         min-width: 320px;
       }
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-lg) {
         min-width: 260px;
       }
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-md) {
         min-width: 240px;
       }
-
+      /* stylelint-disable-next-line media-query-no-invalid */
       @media (max-width: @screen-sm) {
         min-width: 160px;
       }
@@ -208,11 +214,6 @@
 
     .@{countdown-prefix-cls} input {
       min-width: unset;
-    }
-
-    .ant-divider-inner-text {
-      color: @text-color-secondary;
-      font-size: 12px;
     }
   }
 </style>
