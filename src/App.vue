c250f65c8943644112eb54f5b11eedd7a13924f7
苗大 <caoshengmiao@hypergryph.com>
2023-10-31 17:10:43 +0800

    perf(ConfigProvider): 配置antdv主题, 使其与modifyVars配置一致

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <ConfigProvider :locale="getAntdLocale" :theme="isDark ? darkTheme : {}">
+  <ConfigProvider :locale="getAntdLocale" :theme="themeConfig">
     <AppProvider>
       <RouterView />
     </AppProvider>
@@ -7,19 +7,34 @@
 </template>
 
 <script lang="ts" setup>
-  import { ConfigProvider } from 'ant-design-vue';
   import { AppProvider } from '@/components/Application';
   import { useTitle } from '@/hooks/web/useTitle';
   import { useLocale } from '@/locales/useLocale';
+  import { ConfigProvider } from 'ant-design-vue';
 
-  import 'dayjs/locale/zh-cn';
   import { useDarkModeTheme } from '@/hooks/setting/useDarkModeTheme';
+  import 'dayjs/locale/zh-cn';
+  import { computed } from 'vue';
 
   // support Multi-language
   const { getAntdLocale } = useLocale();
 
   const { isDark, darkTheme } = useDarkModeTheme();
 
+  const themeConfig = computed(() =>
+    Object.assign(
+      {
+        token: {
+          colorPrimary: '#0960bd',
+          colorSuccess: '#55D187',
+          colorWarning: '#EFBD47',
+          colorError: '#ED6F6F',
+          colorInfo: '#0960bd',
+        },
+      },
+      isDark.value ? darkTheme : {},
+    ),
+  );
   // Listening to page changes and dynamically changing site titles
   useTitle();
 </script>
