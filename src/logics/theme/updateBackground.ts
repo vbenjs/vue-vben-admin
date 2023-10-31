68ca67f5317da9619023313c1127592b9f0a7e02
invalid w <wangjuesix@gmail.com>
2023-10-31 16:39:33 +0800

    chore(theme): remove dead code

--- src/logics/theme/updateBackground.ts
+++ src/logics/theme/updateBackground.ts
@@ -3,10 +3,6 @@ import { useAppStore } from '/@/store/modules/app';
 import { ThemeEnum } from '/@/enums/appEnum';
 import { setCssVar } from './util';
 
-const TEXT_COLOR_VAR = '--text-color';
-
-const BORDER_COLOR_VAR = '--border-color';
-
 const HEADER_BG_COLOR_VAR = '--header-bg-color';
 const HEADER_BG_HOVER_COLOR_VAR = '--header-bg-hover-color';
 const HEADER_MENU_ACTIVE_BG_COLOR_VAR = '--header-active-menu-bg-color';
@@ -14,44 +10,6 @@ const HEADER_MENU_ACTIVE_BG_COLOR_VAR = '--header-active-menu-bg-color';
 const SIDER_DARK_BG_COLOR = '--sider-dark-bg-color';
 const SIDER_DARK_DARKEN_BG_COLOR = '--sider-dark-darken-bg-color';
 const SIDER_LIGHTEN_BG_COLOR = '--sider-dark-lighten-bg-color';
-
-/**
- * Change the text color of the html
- * @param color
- */
-export function updateTextColor(color?: string) {
-  const appStore = useAppStore();
-  const darkMode = appStore.getDarkMode === ThemeEnum.DARK;
-  if (!color) {
-    if (darkMode) {
-      color = '#c9d1d9';
-    } else {
-      color = 'rgb(0, 0, 0, 0.85)';
-    }
-  }
-
-  // text color
-  setCssVar(TEXT_COLOR_VAR, color);
-}
-
-/**
- * Change the border color of the border
- * @param color
- */
-export function updateBorderColor(color?: string) {
-  const appStore = useAppStore();
-  const darkMode = appStore.getDarkMode === ThemeEnum.DARK;
-  if (!color) {
-    if (darkMode) {
-      color = '#303030';
-    } else {
-      color = '#eee';
-    }
-  }
-
-  // text color
-  setCssVar(BORDER_COLOR_VAR, color);
-}
 
 /**
  * Change the background color of the top header
