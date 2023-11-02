bb3d5b8ae8e8ae0e91acb3022f51572daf3210c6
苗大 <454690789@qq.com>
2023-11-02 09:05:58 +0800

    perf(ConfigProvider): 配置antdv主题色, 使其与modifyVars配置一致 (#3219)
    * perf(ConfigProvider): 配置antdv主题, 使其与modifyVars配置一致
    
    * fix(dark): 修正颜色写法
    
    * fix(dark): 修正app-content-background-color深色颜色
    
    * style(BasicTable): row striped style add important
    
    ---------
    
    Co-authored-by: 苗大 <caoshengmiao@hypergryph.com>

--- src/logics/theme/dark.ts
+++ src/logics/theme/dark.ts
@@ -14,7 +14,7 @@ export type CustomColorType = {
 export const customColorList: CustomColorType[] = [
   {
     name: '--text-color',
-    light: 'rgb(0,0,0,85%)',
+    light: 'rgba(0 0 0 85%)',
     dark: '#c9d1d9',
   },
   {
@@ -30,7 +30,7 @@ export const customColorList: CustomColorType[] = [
   {
     name: '--app-content-background-color',
     light: '#fafafa',
-    dark: '#151515',
+    dark: '#1e1e1e',
   },
   // custom example
   {
