038f969ca371d96bec0050f910b8175899cd43b8
xachary <179740385@qq.com>
2023-11-09 14:15:54 +0800

    feat(layout): move setting button to tabs when fold

---
+++ src/layouts/default/tabs/components/SettingButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <span :class="`${prefixCls}__extra-fold`" @click="openDrawer(true)">
+    <Icon icon="ion:settings-outline" />
+    <SettingDrawer @register="register" />
+  </span>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import SettingDrawer from '/@/layouts/default/setting/SettingDrawer';
+  import Icon from '@/components/Icon/Icon.vue';
+
+  import { useDrawer } from '/@/components/Drawer';
+
+  import { useDesign } from '/@/hooks/web/useDesign';
+
+  export default defineComponent({
+    name: 'SettingButton',
+    components: { SettingDrawer, Icon },
+    setup() {
+      const [register, { openDrawer }] = useDrawer();
+      const { prefixCls } = useDesign('multiple-tabs-content');
+
+      return {
+        register,
+        openDrawer,
+        prefixCls,
+      };
+    },
+  });
+</script>
