0faafcf98f92d0d7034b76b69e0086d95caa3119
masicheng <297876926@qq.com>
2023-11-09 11:53:28 +0800

    feat: 新增环境变量

--- src/hooks/setting/index.ts
+++ src/hooks/setting/index.ts
@@ -3,8 +3,14 @@ import type { GlobConfig } from '/#/config';
 import { getAppEnvConfig } from '/@/utils/env';
 
 export const useGlobSetting = (): Readonly<GlobConfig> => {
-  const { VITE_GLOB_APP_TITLE, VITE_GLOB_API_URL, VITE_GLOB_API_URL_PREFIX, VITE_GLOB_UPLOAD_URL } =
-    getAppEnvConfig();
+  const {
+    VITE_GLOB_APP_TITLE,
+    VITE_GLOB_API_URL,
+    VITE_GLOB_API_URL_PREFIX,
+    VITE_GLOB_UPLOAD_URL,
+    VITE_GLOB_ZXTID,
+    VITE_GLOB_DOWNLOAD_URL,
+  } = getAppEnvConfig();
 
   // Take global configuration
   const glob: Readonly<GlobConfig> = {
@@ -13,6 +19,8 @@ export const useGlobSetting = (): Readonly<GlobConfig> => {
     shortName: VITE_GLOB_APP_TITLE.replace(/\s/g, '_').replace(/-/g, '_'),
     urlPrefix: VITE_GLOB_API_URL_PREFIX,
     uploadUrl: VITE_GLOB_UPLOAD_URL,
+    downloadUrl: VITE_GLOB_DOWNLOAD_URL,
+    zxtId: VITE_GLOB_ZXTID,
   };
   return glob as Readonly<GlobConfig>;
 };
