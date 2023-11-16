3b2760ca3ae5b51989ea7e97713f629a567fe53a
黄小民 <DesignHhuang@users.noreply.github.com>
2023-11-16 10:00:13 +0800

    fix: defaultValue类型为number时的bug (#3288)

--- src/components/Form/src/helper.ts
+++ src/components/Form/src/helper.ts
@@ -61,9 +61,17 @@ export function processDateValue(attr: Recordable, component: string) {
   }
 }
 
+export const defaultValueComponents = [
+  'Input',
+  'InputPassword',
+  'InputNumber',
+  'InputSearch',
+  'InputTextArea',
+];
+
 export function handleInputNumberValue(component?: ComponentType, val?: any) {
   if (!component) return val;
-  if (['Input', 'InputPassword', 'InputSearch', 'InputTextArea'].includes(component)) {
+  if (defaultValueComponents.includes(component)) {
     return val && isNumber(val) ? `${val}` : val;
   }
   return val;
@@ -73,8 +81,6 @@ export function handleInputNumberValue(component?: ComponentType, val?: any) {
  * 时间字段
  */
 export const dateItemType = genType();
-
-export const defaultValueComponents = ['Input', 'InputPassword', 'InputSearch', 'InputTextArea'];
 
 // TODO 自定义组件封装会出现验证问题，因此这里目前改成手动触发验证
 export const NO_AUTO_LINK_COMPONENTS: ComponentType[] = [
