20f85ed3697e2b96dcf676d1a7daa9a71895fa43
415313629@qq.com <415313629@qq.com>
2023-11-17 18:50:23 +0800

    refactor: update

--- src/views/demo/feat/tabs/index.vue
+++ src/views/demo/feat/tabs/index.vue
@@ -24,46 +24,39 @@
     </CollapseContainer>
   </PageWrapper>
 </template>
-<script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import { CollapseContainer } from '/@/components/Container';
-  import { useTabs } from '/@/hooks/web/useTabs';
-  import { PageWrapper } from '/@/components/Page';
-  import { Input, Alert } from 'ant-design-vue';
-  import { useMessage } from '/@/hooks/web/useMessage';
+<script lang="ts" setup>
+  import { ref } from 'vue';
   import { useGo } from '/@/hooks/web/usePage';
+  import { useTabs } from '/@/hooks/web/useTabs';
+  import { useMessage } from '/@/hooks/web/useMessage';
 
-  export default defineComponent({
+  import { Input, Alert } from 'ant-design-vue';
+  import { PageWrapper } from '/@/components/Page';
+  import { CollapseContainer } from '/@/components/Container';
+
+  const AInput = Input;
+  const AAlert = Alert;
+
+  defineOptions({
     name: 'TabsDemo',
-    components: { CollapseContainer, PageWrapper, [Input.name]: Input, [Alert.name]: Alert },
-    setup() {
-      const go = useGo();
-      const title = ref<string>('');
-      const { closeAll, closeLeft, closeRight, closeOther, closeCurrent, refreshPage, setTitle } =
-        useTabs();
-      const { createMessage } = useMessage();
-      function setTabTitle() {
-        if (title.value) {
-          setTitle(title.value);
-        } else {
-          createMessage.error('请输入要设置的Tab标题！');
-        }
-      }
-
-      function toDetail(index: number) {
-        go(`/feat/tabs/detail/${index}`);
-      }
-      return {
-        closeAll,
-        closeLeft,
-        closeRight,
-        closeOther,
-        closeCurrent,
-        toDetail,
-        refreshPage,
-        setTabTitle,
-        title,
-      };
-    },
   });
+
+  const go = useGo();
+  const title = ref('');
+
+  const { closeAll, closeLeft, closeRight, closeOther, closeCurrent, refreshPage, setTitle } =
+    useTabs();
+  const { createMessage } = useMessage();
+
+  function setTabTitle() {
+    if (title.value) {
+      setTitle(title.value);
+    } else {
+      createMessage.error('请输入要设置的Tab标题！');
+    }
+  }
+
+  function toDetail(index: number) {
+    go(`/feat/tabs/detail/${index}`);
+  }
 </script>
