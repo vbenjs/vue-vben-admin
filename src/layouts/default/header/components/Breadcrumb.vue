051cbcc698eda540babe0b1840da1d5935a31e13
invalid w <wangjuesix@gmail.com>
2023-11-23 12:11:10 +0800

    chore(Breadcrumb): revert setup change

--- src/layouts/default/header/components/Breadcrumb.vue
+++ src/layouts/default/header/components/Breadcrumb.vue
@@ -1,152 +1,153 @@
 <template>
   <div :class="[prefixCls, `${prefixCls}--${theme}`]">
-    <Breadcrumb :routes="routes">
+    <a-breadcrumb :routes="routes">
       <template #itemRender="{ route, routes: routesMatched, paths }">
         <Icon :icon="getIcon(route)" v-if="getShowBreadCrumbIcon && getIcon(route)" />
         <span v-if="!hasRedirect(routesMatched, route)">
-          {{ t(getTitle(route)) }}
+          {{ t(route.name || route.meta.title) }}
         </span>
-        <router-link v-else to="" @click="handleClick(route, paths, $event)">
-          {{ t(getTitle(route)) }}
+        <router-link v-else to="" @click="handleClick(route, paths, $event as Event)">
+          {{ t(route.name || route.meta.title) }}
         </router-link>
       </template>
-    </Breadcrumb>
+    </a-breadcrumb>
   </div>
 </template>
-<script lang="ts" setup>
+<script lang="ts">
   import type { RouteLocationMatched } from 'vue-router';
   import { useRouter } from 'vue-router';
-  import type { Menu } from '@/router/types';
+  import type { Menu } from '/@/router/types';
 
-  import { ref, watchEffect } from 'vue';
+  import { defineComponent, ref, watchEffect } from 'vue';
 
   import { Breadcrumb } from 'ant-design-vue';
   import Icon from '@/components/Icon/Icon.vue';
 
-  import { useDesign } from '@/hooks/web/useDesign';
-  import { useRootSetting } from '@/hooks/setting/useRootSetting';
-  import { useGo } from '@/hooks/web/usePage';
-  import { useI18n } from '@/hooks/web/useI18n';
+  import { useDesign } from '/@/hooks/web/useDesign';
+  import { useRootSetting } from '/@/hooks/setting/useRootSetting';
+  import { useGo } from '/@/hooks/web/usePage';
+  import { useI18n } from '/@/hooks/web/useI18n';
 
-  import { propTypes } from '@/utils/propTypes';
-  import { isString } from '@/utils/is';
-  import { filter } from '@/utils/helper/treeHelper';
-  import { getMenus } from '@/router/menus';
+  import { propTypes } from '/@/utils/propTypes';
+  import { isString } from '/@/utils/is';
+  import { filter } from '/@/utils/helper/treeHelper';
+  import { getMenus } from '/@/router/menus';
 
-  import { REDIRECT_NAME } from '@/router/constant';
-  import { getAllParentPath } from '@/router/helper/menuHelper';
+  import { REDIRECT_NAME } from '/@/router/constant';
+  import { getAllParentPath } from '/@/router/helper/menuHelper';
 
-  defineOptions({ name: 'LayoutBreadcrumb' });
+  export default defineComponent({
+    name: 'LayoutBreadcrumb',
+    components: { Icon, [Breadcrumb.name]: Breadcrumb },
+    props: {
+      theme: propTypes.oneOf(['dark', 'light']),
+    },
+    setup() {
+      const routes = ref<RouteLocationMatched[]>([]);
+      const { currentRoute } = useRouter();
+      const { prefixCls } = useDesign('layout-breadcrumb');
+      const { getShowBreadCrumbIcon } = useRootSetting();
+      const go = useGo();
 
-  defineProps({
-    theme: propTypes.oneOf(['dark', 'light']),
-  });
+      const { t } = useI18n();
+      watchEffect(async () => {
+        if (currentRoute.value.name === REDIRECT_NAME) return;
+        const menus = await getMenus();
 
-  const routes = ref<any[]>([]);
-  const { currentRoute } = useRouter();
-  const { prefixCls } = useDesign('layout-breadcrumb');
-  const { getShowBreadCrumbIcon } = useRootSetting();
-  const go = useGo();
+        const routeMatched = currentRoute.value.matched;
+        const cur = routeMatched?.[routeMatched.length - 1];
+        let path = currentRoute.value.path;
 
-  const { t } = useI18n();
-  watchEffect(async () => {
-    if (currentRoute.value.name === REDIRECT_NAME) return;
-    const menus = await getMenus();
+        if (cur && cur?.meta?.currentActiveMenu) {
+          path = cur.meta.currentActiveMenu as string;
+        }
 
-    const routeMatched = currentRoute.value.matched;
-    const cur = routeMatched?.[routeMatched.length - 1];
-    let path = currentRoute.value.path;
+        const parent = getAllParentPath(menus, path);
+        const filterMenus = menus.filter((item) => item.path === parent[0]);
+        const matched = getMatched(filterMenus, parent) as any;
 
-    if (cur && cur?.meta?.currentActiveMenu) {
-      path = cur.meta.currentActiveMenu as string;
-    }
+        if (!matched || matched.length === 0) return;
 
-    const parent = getAllParentPath(menus, path);
-    const filterMenus = menus.filter((item) => item.path === parent[0]);
-    const matched = getMatched(filterMenus, parent) as any;
+        const breadcrumbList = filterItem(matched);
 
-    if (!matched || matched.length === 0) return;
+        if (currentRoute.value.meta?.currentActiveMenu) {
+          breadcrumbList.push({
+            ...currentRoute.value,
+            name: currentRoute.value.meta?.title || currentRoute.value.name,
+          } as unknown as RouteLocationMatched);
+        }
+        routes.value = breadcrumbList;
+      });
 
-    const breadcrumbList = filterItem(matched);
-
-    if (currentRoute.value.meta?.currentActiveMenu) {
-      breadcrumbList.push({
-        ...currentRoute.value,
-        name: currentRoute.value.meta?.title || currentRoute.value.name,
-      } as unknown as RouteLocationMatched);
-    }
-    routes.value = breadcrumbList;
-  });
-
-  function getMatched(menus: Menu[], parent: string[]) {
-    const metched: Menu[] = [];
-    menus.forEach((item) => {
-      if (parent.includes(item.path)) {
-        metched.push({
-          ...item,
-          name: item.meta?.title || item.name,
+      function getMatched(menus: Menu[], parent: string[]) {
+        const metched: Menu[] = [];
+        menus.forEach((item) => {
+          if (parent.includes(item.path)) {
+            metched.push({
+              ...item,
+              name: item.meta?.title || item.name,
+            });
+          }
+          if (item.children?.length) {
+            metched.push(...getMatched(item.children, parent));
+          }
         });
+        return metched;
       }
-      if (item.children?.length) {
-        metched.push(...getMatched(item.children, parent));
+
+      function filterItem(list: RouteLocationMatched[]) {
+        return filter(list, (item) => {
+          const { meta, name } = item;
+          if (!meta) {
+            return !!name;
+          }
+          const { title, hideBreadcrumb, hideMenu } = meta;
+          if (!title || hideBreadcrumb || hideMenu) {
+            return false;
+          }
+          return true;
+        }).filter((item) => !item.meta?.hideBreadcrumb);
       }
-    });
-    return metched;
-  }
 
-  function filterItem(list: any[]) {
-    return filter(list, (item) => {
-      const { meta, name } = item;
-      if (!meta) {
-        return !!name;
+      function handleClick(route: RouteLocationMatched, paths: string[], e: Event) {
+        e?.preventDefault();
+        const { children, redirect, meta } = route;
+
+        if (children?.length && !redirect) {
+          e?.stopPropagation();
+          return;
+        }
+        if (meta?.carryParam) {
+          return;
+        }
+
+        if (redirect && isString(redirect)) {
+          go(redirect);
+        } else {
+          let goPath = '';
+          if (paths.length === 1) {
+            goPath = paths[0];
+          } else {
+            const ps = paths.slice(1);
+            const lastPath = ps.pop() || '';
+            goPath = `${lastPath}`;
+          }
+          goPath = /^\//.test(goPath) ? goPath : `/${goPath}`;
+          go(goPath);
+        }
       }
-      const { title, hideBreadcrumb, hideMenu } = meta;
-      if (!title || hideBreadcrumb || hideMenu) {
-        return false;
+
+      function hasRedirect(routes: RouteLocationMatched[], route: RouteLocationMatched) {
+        return routes.indexOf(route) !== routes.length - 1;
       }
-      return true;
-    }).filter((item) => !item.meta?.hideBreadcrumb);
-  }
 
-  function handleClick(route: any, paths: string[], e: Event) {
-    e?.preventDefault();
-    const { children, redirect, meta } = route;
-
-    if (children?.length && !redirect) {
-      e?.stopPropagation();
-      return;
-    }
-    if (meta?.carryParam) {
-      return;
-    }
-
-    if (redirect && isString(redirect)) {
-      go(redirect);
-    } else {
-      let goPath = '';
-      if (paths.length === 1) {
-        goPath = paths[0];
-      } else {
-        const ps = paths.slice(1);
-        const lastPath = ps.pop() || '';
-        goPath = `${lastPath}`;
+      function getIcon(route) {
+        return route.icon || route.meta?.icon;
       }
-      goPath = /^\//.test(goPath) ? goPath : `/${goPath}`;
-      go(goPath);
-    }
-  }
 
-  function hasRedirect(routes: any[], route: any) {
-    return routes.indexOf(route) !== routes.length - 1;
-  }
-
-  function getIcon(route) {
-    return route.icon || route.meta.icon;
-  }
-
-  function getTitle(route) {
-    return route.name || route.meta.title;
-  }
+      return { routes, t, prefixCls, getIcon, getShowBreadCrumbIcon, handleClick, hasRedirect };
+    },
+  });
 </script>
 <style lang="less">
   @prefix-cls: ~'@{namespace}-layout-breadcrumb';
