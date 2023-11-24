7aa19b1e971b6b1fc26a2ece4bdbf782b8e6b256
Tang <11726782+sugar_gitee@user.noreply.gitee.com>
2023-11-24 15:55:13 +0800

    fix: 解决Breadcrumb面包屑导航404问题

--- src/layouts/default/header/components/Breadcrumb.vue
+++ src/layouts/default/header/components/Breadcrumb.vue
@@ -125,15 +125,18 @@
           go(redirect);
         } else {
           let goPath = '';
+          let link: boolean;
           if (paths.length === 1) {
             goPath = paths[0];
+            link = true;
           } else {
             const ps = paths.slice(1);
             const lastPath = ps.pop() || '';
             goPath = `${lastPath}`;
+            link = lastPath.includes(ps[0]);
           }
           goPath = /^\//.test(goPath) ? goPath : `/${goPath}`;
-          go(goPath);
+          link ? go(goPath) : window.open(goPath.replace(/^\//, ''), '_blank');
         }
       }
 
