203ae841b221b79114f95f323082771f302445f9
苗大 <454690789@qq.com>
2023-12-18 21:15:23 +0800

    perf(darkMode): 深色模式颜色定义以及切换方式优化 (#3436)
    * perf(darkMode): 深色模式颜色定义以及切换方式优化
    
    * perf: text-color rgb->rgba
    
    ---------
    
    Co-authored-by: 苗大 <caoshengmiao@hypergryph.com>

--- src/logics/theme/dark.ts
+++ src/logics/theme/dark.ts
@@ -1,4 +1,3 @@
-import { setCssVar } from './util';
 import { addClass, hasClass, removeClass } from '@/utils/domUtils';
 
 export type CustomColorType = {
@@ -6,46 +5,6 @@ export type CustomColorType = {
   light: string;
   dark: string;
 };
-
-/**
- * 自定义颜色列表
- * 需先在 src/design/color.less 内定义变量和 light 颜色
- */
-export const customColorList: CustomColorType[] = [
-  {
-    name: '--text-color',
-    light: 'rgba(0, 0, 0, 0.85)',
-    dark: '#c9d1d9',
-  },
-  {
-    name: '--border-color',
-    light: '#eee',
-    dark: '#303030',
-  },
-  {
-    name: '--component-background-color',
-    light: '#fff',
-    dark: '#151515',
-  },
-  {
-    name: '--app-content-background-color',
-    light: '#fafafa',
-    dark: '#1e1e1e',
-  },
-  // custom example
-  {
-    name: '--custom-example-color',
-    light: '#ff4d4f',
-    dark: '#55D187',
-  },
-];
-
-// 根据主题更新自定义颜色
-export function updateCustomColor(mode: 'light' | 'dark') {
-  customColorList.forEach((item) => {
-    setCssVar(item.name, item[mode]);
-  });
-}
 
 export async function updateDarkTheme(mode: string | null = 'light') {
   const htmlRoot = document.getElementById('htmlRoot');
@@ -64,5 +23,4 @@ export async function updateDarkTheme(mode: string | null = 'light') {
       removeClass(htmlRoot, 'dark');
     }
   }
-  updateCustomColor(mode === 'dark' ? 'dark' : 'light');
 }
