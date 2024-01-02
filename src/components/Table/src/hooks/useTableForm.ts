98e2e4c89a859e67c911134652d4b005be51e2d1
xiaoMingTongXue <40878104+cMing1997@users.noreply.github.com>
2024-01-02 09:44:50 +0800

    fix(BasicForm): 修复 useComponentRegister 方法无法添加自定义的组件，添加时爆类型错误的问题 (#3483)
    * types(component): 修复 useComponentRegister 方法无法添加自定义的组件，添加时爆类型错误的问题
    
    * types(component): 修复 table 组件的 useTableForm 入参的 fetch 和 TableActionType 的 reload 类型定义报错的问题
    
    ---------
    
    Co-authored-by: chengmingrui <chengmingrui@oneaiops.com>

--- src/components/Table/src/hooks/useTableForm.ts
+++ src/components/Table/src/hooks/useTableForm.ts
@@ -7,7 +7,7 @@ import { isFunction } from '@/utils/is';
 export function useTableForm(
   propsRef: ComputedRef<BasicTableProps>,
   slots: Slots,
-  fetch: (opt?: FetchParams | undefined) => Promise<void>,
+  fetch: (opt?: FetchParams | undefined) => Promise<Recordable<any>[] | undefined>,
   getLoading: ComputedRef<boolean | undefined>,
 ) {
   const getFormProps = computed((): Partial<FormProps> => {
