9106c8250954b866c956f946de09fc5118e7a4d9
xiaoMingTongXue <40878104+cMing1997@users.noreply.github.com>
2024-01-02 09:44:50 +0800

    fix(BasicForm): 修复 useComponentRegister 方法无法添加自定义的组件，添加时爆类型错误的问题 (#3483)
    * types(component): 修复 useComponentRegister 方法无法添加自定义的组件，添加时爆类型错误的问题
    
    * types(component): 修复 table 组件的 useTableForm 入参的 fetch 和 TableActionType 的 reload 类型定义报错的问题
    
    ---------
    
    Co-authored-by: chengmingrui <chengmingrui@oneaiops.com>

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -65,3 +65,8 @@ export function isHttpUrl(path: string): boolean {
   const reg = /^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?/;
   return reg.test(path);
 }
+
+export function isPascalCase(str: string): boolean {
+  const regex = /^[A-Z][A-Za-z]*$/;
+  return regex.test(str);
+}
