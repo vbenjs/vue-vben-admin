f13540c1aeb45c6e2134249fd288855bf653182f
doverdee <doverlee@foxmail.com>
2024-01-05 13:58:25 +0800

    feat: 添加点选地图位置和特殊字段正则校验
    1. 添加地图组件并在基本表单中新增点选位置示例
    2. 添加正则验证工具类并在验证表单中新增邮箱验证示例

---
+++ src/components/Map/src/LocationModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <BasicModal
+    v-bind="$attrs"
+    @register="registerModal"
+    width="928px"
+    title="选择位置"
+    @ok="handleSubmit"
+  >
+    <div class="mapMode">
+      <LocationBMapGL @click="HandlerClickMap" :addr="addr" :lng="lng" :lat="lat" />
+    </div>
+  </BasicModal>
+</template>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import { BasicModal, useModalInner } from '@/components/Modal';
+  import { LocationBMapGL } from '@/components/Map';
+
+  defineOptions({ name: 'LocationModal' });
+  const emit = defineEmits(['success', 'register']);
+
+  const addr = ref<string>();
+  const lng = ref<string>();
+  const lat = ref<string>();
+  const newGeoLoc = ref<any>();
+
+  const [registerModal, { setModalProps, closeModal }] = useModalInner((data) => {
+    setModalProps({ confirmLoading: false });
+    addr.value = data.addr || undefined;
+    lng.value = data.lng || undefined;
+    lat.value = data.lat || undefined;
+  });
+
+  async function handleSubmit() {
+    try {
+      setModalProps({ confirmLoading: true });
+      emit('success', newGeoLoc.value);
+      closeModal();
+    } finally {
+      setModalProps({ confirmLoading: false });
+    }
+  }
+
+  async function HandlerClickMap(location: any) {
+    newGeoLoc.value = location;
+  }
+</script>
+<style scoped>
+  .address {
+    margin-bottom: 10px;
+  }
+
+  .mapMode {
+    width: 900px;
+    height: 600px;
+    overflow: hidden;
+  }
+</style>
