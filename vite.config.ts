e6c7b5f9282adbdd9429495d5918c1eafbffde7d
xingyu <xingyu4j@vip.qq.com>
2024-01-06 09:19:52 +0800

    feat(deps): update vite version to 5.x (#3508)

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,9 @@ export default defineApplicationConfig({
           rewrite: (path) => path.replace(new RegExp(`^/upload`), ''),
         },
       },
+      warmup: {
+        clientFiles: ['./index.html', './src/{views,components}/*'],
+      },
     },
   },
 });
