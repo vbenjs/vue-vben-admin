c7220c2ccfddf34b53f6f2d8c3dacee40c9d3fc3
xachary <179740385@qq.com>
2024-01-11 15:29:36 +0800

    feat(BasicTable): table enable accordion expand (#3533). resolve #3525

--- src/components/Table/src/types/table.ts
+++ src/components/Table/src/types/table.ts
@@ -148,6 +148,7 @@ export interface BasicTableProps<T = any> {
   // 点击行选中
   clickToRowSelect?: boolean;
   isTreeTable?: boolean;
+  accordion?: boolean; // isTreeTable 或 expandRowByClick 时支持
   // 自定义排序方法
   sortFn?: (sortInfo: SorterResult) => any;
   // 排序方法
