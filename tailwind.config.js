38c1a694e76df1281f4c83bc73a55b545a919fbb
Electrolux <3451613934@qq.com>
2024-02-20 10:35:14 +0800

    feat(config): 添加tailwind智能提示的config文件

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
