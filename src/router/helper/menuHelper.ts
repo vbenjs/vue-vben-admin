967ad233956eed9007442c6b3a15952557689066
sundong <abcd123456>
2024-03-01 10:57:02 +0800

    改为?.

--- src/router/helper/menuHelper.ts
+++ src/router/helper/menuHelper.ts
@@ -20,7 +20,7 @@ function joinParentPath(menus: Menu[], parentPath = '') {
     // 请注意，以 / 开头的嵌套路径将被视为根路径。
     // This allows you to leverage the component nesting without having to use a nested URL.
     // 这允许你利用组件嵌套，而无需使用嵌套 URL。
-    if (!(menu.path.startsWith('/') || isHttpUrl(menu.path))) {
+    if (!(menu.path?.startsWith('/') || isHttpUrl(menu.path))) {
       // path doesn't start with /, nor is it a url, join parent path
       // 路径不以 / 开头，也不是 url，加入父路径
       menu.path = `${parentPath}/${menu.path}`;
@@ -49,7 +49,7 @@ export function transformRouteToMenu(routeModList: AppRouteModule[], routerMappi
 
   // 对路由项进行修改
   cloneRouteModList.forEach((item) => {
-    if (routerMapping && item.meta.hideChildrenInMenu && typeof item.redirect === 'string') {
+    if (routerMapping && item.meta?.hideChildrenInMenu && typeof item.redirect === 'string') {
       item.path = item.redirect;
     }
 
