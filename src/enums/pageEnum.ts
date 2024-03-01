3749666a6247f68234e0df6be6f80e624d196400
shizhongming <zhongming4762@hotmail.com>
2024-03-01 09:26:13 +0800

    fix(架构): 修复登录后跳转进入死循环问题

--- src/enums/pageEnum.ts
+++ src/enums/pageEnum.ts
@@ -2,7 +2,7 @@ export enum PageEnum {
   // basic login path
   BASE_LOGIN = '/login',
   // basic home path
-  BASE_HOME = '/dashboard',
+  BASE_HOME = '/dashboard/analysis',
   // error page path
   ERROR_PAGE = '/exception',
   // error log page path
