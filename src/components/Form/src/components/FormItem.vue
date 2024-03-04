1191946bb15e72dd6113ae35798a5d6c1160d462
sundong <abcd123456>
2024-03-04 10:38:18 +0800

    sync

--- src/components/Form/src/components/FormItem.vue
+++ src/components/Form/src/components/FormItem.vue
@@ -67,6 +67,12 @@
         formProps: Ref<FormProps>;
       };
 
+      // 组件 CropperAvatar 的 size 属性类型为 number
+      // 此处补充一个兼容
+      if (schema.value.component === 'CropperAvatar' && typeof formProps.value.size === 'string') {
+        formProps.value.size = undefined;
+      }
+
       const itemLabelWidthProp = useItemLabelWidth(schema, formProps);
 
       const getValues = computed(() => {
