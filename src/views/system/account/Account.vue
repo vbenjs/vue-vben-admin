d70d98d984c45a3d4e7b246f4e7b736d29f6f700
sundong <abcd123456>
2024-03-04 16:17:09 +0800

    更新Account

--- src/views/system/account/Account.vue
+++ src/views/system/account/Account.vue
@@ -11,58 +11,6 @@
           新建账户
         </a-button>
       </template>
-
-      <template #bodyCell="{ record, column }">
-        <template v-if="column.dataIndex === 'action'">
-          <TableAction
-            stopButtonPropagation
-            :actions="[
-              {
-                icon: 'clarity:note-edit-line',
-                tooltip: '账号编辑',
-                auth: 'accountManager_edit',
-                onClick: handleEdit.bind(null, record.id),
-                ifShow: record.sysDefault !== 'Y',
-              },
-              {
-                icon: 'ant-design:file-search-outlined',
-                tooltip: '登录记录',
-                auth: 'accountManager_login-record',
-                onClick: handleLoginRecord.bind(null, record),
-              },
-              {
-                icon: 'bx:map',
-                tooltip: '地点权限',
-                auth: 'UserStore',
-                onClick: handleStoreAuth.bind(null, record),
-              },
-              {
-                icon: 'carbon:password',
-                color: 'warning',
-                tooltip: '重置密码',
-                auth: 'accountManager_reset-pwd',
-                popConfirm: {
-                  title: '是否确认要重置密码？',
-                  placement: 'left',
-                  confirm: handleResetPass.bind(null, record.id),
-                },
-                ifShow: record.sysDefault !== 'Y',
-              },
-              {
-                icon: 'ant-design:delete-outlined',
-                color: 'error',
-                auth: 'accountManager_del',
-                popConfirm: {
-                  title: '是否确认删除？',
-                  placement: 'left',
-                  confirm: handleDelete.bind(null, record.id),
-                },
-                ifShow: record.sysDefault !== 'Y',
-              },
-            ]"
-          />
-        </template>
-      </template>
     </BasicTable>
 
     <AccountDrawer @register="registerDrawer" @success="handleSuccess" />
@@ -70,8 +18,7 @@
     <StoreAuth @register="registerDrawer2" />
   </div>
 </template>
-<script lang="tsx" setup name="Account">
-  // import { ref } from 'vue';
+<script lang="tsx" setup>
   import { BasicTable, TableAction, useTable } from '@/components/Table';
 
   import { getFormConfig, getColumn } from './data';
@@ -80,9 +27,10 @@
   import { useMessage } from '@/hooks/web/useMessage';
   import { createPassword } from '@/utils/password';
   import { copyText } from '@/utils/copyTextToClipboard';
-
-  import Icon from '@/components/Icon/Icon.vue';
+  import { Icon } from '@/components/Icon';
   import { createAsyncComponent } from '@/utils/factory/createAsyncComponent';
+
+  defineOptions({ name: 'Account' });
 
   const AccountDrawer = createAsyncComponent(() => import('./drawer/AccountDrawer.vue'));
   const LoginRecord = createAsyncComponent(() => import('./drawer/LoginRecord.vue'));
@@ -110,64 +58,24 @@
         'accountManager_del',
         'UserStore',
       ],
+      customRender: ({ record }) => {
+        return createActions(record as Account);
+      },
     },
     useSearchForm: true,
     formConfig: getFormConfig(),
     showTableSetting: true,
     tableSetting: { fullScreen: true },
-    // rowSelection: {
-    //   type: 'checkbox',
-    //   onChange: onSelectChange,
-    // },
     loading: true,
     showIndexColumn: false,
   });
 
-  // function onSelectChange(selectedRowKeys: (string | number)[]) {
-  //   checkedKeys.value = selectedRowKeys;
-  // }
   const handleCreate = () => {
     openDrawer(true, {
       actionKey: 'create',
     });
   };
 
-  const handleEdit = async (id: number) => {
-    const account = await getAccountById(id);
-    openDrawer(true, {
-      record: account,
-      actionKey: 'edit',
-    });
-  };
-
-  const handleLoginRecord = async (record: Recordable) => {
-    openLoginRecord(true, {
-      record,
-    });
-  };
-
-  const handleResetPass = async (id: number) => {
-    const password = createPassword();
-    resetPassword(id, password).then(() => {
-      createSuccessModal({
-        title: '重置成功！',
-        content: `密码已经重置为:${password},请保存！`,
-        onOk: () => {
-          copyText(password);
-          return Promise.resolve();
-        },
-        okText: () => (
-          <div>
-            <Icon style="margin-right:4px" icon="ant-design:copy-outlined" />
-            复制密码
-          </div>
-        ),
-      });
-    });
-  };
-  const handleDelete = (id: number) => {
-    deleteAccount(id).then(() => reload());
-  };
   function handleSuccess({ action, values }) {
     if (action == 'edit') {
       updateTableDataRecord(values.id, values);
@@ -176,9 +84,85 @@
     }
   }
 
-  const handleStoreAuth = (record: Recordable) => {
-    openStoreAuth(true, {
-      record,
-    });
+  const createActions = (record: Account) => {
+    return (
+      <TableAction
+        stopButtonPropagation
+        actions={[
+          {
+            icon: 'clarity:note-edit-line',
+            tooltip: '账号编辑',
+            auth: 'accountManager_edit',
+            onClick: async () => {
+              const account = await getAccountById(record.id);
+              openDrawer(true, {
+                record: account,
+                actionKey: 'edit',
+              });
+            },
+            ifShow: record.sysDefault !== 'Y',
+          },
+          {
+            icon: 'ant-design:file-search-outlined',
+            tooltip: '登录记录',
+            auth: 'accountManager_login-record',
+            onClick: () =>
+              openLoginRecord(true, {
+                record,
+              }),
+          },
+          {
+            icon: 'bx:map',
+            tooltip: '地点权限',
+            auth: 'UserStore',
+            onClick: () =>
+              openStoreAuth(true, {
+                record,
+              }),
+          },
+          {
+            icon: 'carbon:password',
+            color: 'warning',
+            tooltip: '重置密码',
+            auth: 'accountManager_reset-pwd',
+            popConfirm: {
+              title: '是否确认要重置密码？',
+              placement: 'left',
+              confirm: async () => {
+                const password = createPassword();
+                resetPassword(record.id, password).then(() => {
+                  createSuccessModal({
+                    title: '重置成功！',
+                    content: `密码已经重置为:${password},请保存！`,
+                    onOk: () => {
+                      copyText(password);
+                      return Promise.resolve();
+                    },
+                    okText: () => (
+                      <div>
+                        <Icon style="margin-right:4px" icon="ant-design:copy-outlined" />
+                        复制密码
+                      </div>
+                    ),
+                  });
+                });
+              },
+            },
+            ifShow: record.sysDefault !== 'Y',
+          },
+          {
+            icon: 'ant-design:delete-outlined',
+            color: 'error',
+            auth: 'accountManager_del',
+            popConfirm: {
+              title: '是否确认删除？',
+              placement: 'left',
+              confirm: () => deleteAccount(record.id).then(() => reload()),
+            },
+            ifShow: record.sysDefault !== 'Y',
+          },
+        ]}
+      />
+    );
   };
 </script>
