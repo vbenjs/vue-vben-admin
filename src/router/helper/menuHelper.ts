0434e718ed2ffaa38d0c6b6aa67e7433de8737cc
Ze.Rui <jiangjunzhao@126.com>
2024-03-15 16:50:31 +0800

    Pref: 路由菜单取title字段 而不是name，树结构数据修改 (#3666)
    * perf: 修改路由菜单获取title字段
    
    * perf: 调整树结构获取的数据源
    
    * perf: 修改getI18nName 获取值方式
    
    ---------
    
    Co-authored-by: 赵泽瑞 <zhaozerui@jilinxiangyun.com>

--- src/router/helper/menuHelper.ts
+++ src/router/helper/menuHelper.ts
@@ -61,12 +61,12 @@ export function transformRouteToMenu(routeModList: AppRouteModule[], routerMappi
   // 提取树指定结构
   const list = treeMap(routeList, {
     conversion: (node: AppRouteRecordRaw) => {
-      const { meta: { title, hideMenu = false } = {} } = node;
+      const { meta: { hideMenu = false } = {}, name } = node;
 
       return {
         ...(node.meta || {}),
         meta: node.meta,
-        name: title,
+        name,
         hideMenu,
         path: node.path,
         ...(node.redirect ? { redirect: node.redirect } : {}),
