be1a1fe46186fa9a57a73797b713a4588f85836a
Electrolux <3451613934@qq.com>
2024-05-01 17:17:18 +0800

    fix: remove unuse fn

--- src/components/Form/src/helper.ts
+++ src/components/Form/src/helper.ts
@@ -2,7 +2,7 @@ import type { Rule as ValidationRule } from 'ant-design-vue/lib/form/interface';
 import type { ComponentType } from './types';
 import { useI18n } from '@/hooks/web/useI18n';
 import { dateUtil } from '@/utils/dateUtil';
-import { isNumber, isObject } from '@/utils/is';
+import { isObject } from '@/utils/is';
 
 const { t } = useI18n();
 
