ba5b8f8506bb9d052ce2705653f255f0401963e8
Electrolux <59329360+electroluxcode@users.noreply.github.com>
2024-05-02 03:22:53 +0800

    perf(util): remove handleInputNumberValue (#3806)
    * perf(util): remove handleInputNumberValue
    
    * fix: remove unuse fn

--- src/components/Form/src/hooks/useFormEvents.ts
+++ src/components/Form/src/hooks/useFormEvents.ts
@@ -6,7 +6,6 @@ import { isArray, isFunction, isObject, isString, isDef, isNil } from '@/utils/i
 import { deepMerge } from '@/utils';
 import {
   dateItemType,
-  handleInputNumberValue,
   defaultValueComponents,
   isIncludeSimpleComponents,
 } from '../helper';
@@ -79,8 +78,6 @@ export function useFormEvents({
       const schema = unref(getSchema).find((item) => item.field === key);
       let value = get(values, key);
       const hasKey = has(values, key);
-
-      value = handleInputNumberValue(schema?.component, value);
       const { componentProps } = schema || {};
       let _props = componentProps as any;
       if (typeof componentProps === 'function') {
