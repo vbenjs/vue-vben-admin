ea5c66b6eb49a98100f926e37b69d70ad0fb1d01
vince <vince292007@gmail.com>
2024-07-31 22:15:01 +0800

    feat: add upgrade prompt

---
+++ src/layouts/default/header/components/UpgradePrompt.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { h } from 'vue';
+  import { Modal } from 'ant-design-vue';
+
+  Modal.info({
+    title: '新版本发布',
+    content: h('div', {}, [h('p', 'Vben Admin v5.0.0 预览版本已发布')]),
+    onOk() {
+      handleClick();
+    },
+    okText: '前往体验新版',
+  });
+
+  function handleClick() {
+    window.open('https://www.vben.pro', '_blank');
+  }
+</script>
+<template>
+  <div>
+    <a-button type="primary" @click="handleClick">前往体验新版</a-button>
+  </div>
+</template>
