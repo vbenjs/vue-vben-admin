4823c184d4e626e48fa618bf32cb3012854e11d6
jinmao <jinmao88@qq.com>
2024-08-05 11:02:43 +0800

    feat: 增加NDataTable示例

---
+++ apps/web-naive/src/views/demos/table/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { NDataTable } from 'naive-ui';
+
+const columns = ref([
+  {
+    key: 'no',
+    title: 'No',
+  },
+  {
+    key: 'title',
+    title: 'Title',
+  },
+  {
+    key: 'length',
+    title: 'Length',
+  },
+]);
+const data = [
+  { length: '4:18', no: 3, title: 'Wonderwall' },
+  { length: '4:48', no: 4, title: "Don't Look Back in Anger" },
+  { length: '7:27', no: 12, title: 'Champagne Supernova' },
+];
+</script>
+
+<template>
+  <NDataTable :columns="columns" :data="data" />
+</template>
+
+<style scoped></style>
