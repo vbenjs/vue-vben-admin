1309cf384ec1e1ac0d50606c6a0d7a24ce0eaef6
invalid w <wangjuesix@gmail.com>
2024-08-05 08:34:50 +0800

    Revert "chore: 组件路由放置到演示路由下"
    This reverts commit 3c24632ad51f031cf95aafa49183d7c5538098bb.

---
+++ apps/web-antd/src/router/routes/modules/comp.ts
@@ -0,0 +1,21 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { BasicLayout } from '#/layouts';
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    component: BasicLayout,
+    meta: {
+      icon: 'ic:baseline-view-in-ar',
+      keepAlive: true,
+      order: 1000,
+      title: $t('page.comp.title'),
+    },
+    name: 'Comp',
+    path: '/comp',
+    children: [],
+  },
+];
+
+export default routes;
--- apps/web-antd/src/router/routes/modules/demos.ts
+++ apps/web-antd/src/router/routes/modules/demos.ts
@@ -167,16 +167,6 @@ const routes: RouteRecordRaw[] = [
           },
         ],
       },
-      // 组件
-      {
-        meta: {
-          icon: 'ion:layers-outline',
-          title: $t('page.demos.comp.title'),
-        },
-        name: 'CompDemos',
-        path: '/demos/comp',
-        children: [],
-      },
       // 面包屑导航
       {
         name: 'BreadcrumbDemos',
