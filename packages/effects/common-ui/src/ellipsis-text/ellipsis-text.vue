71d4c9e647a87aa58a36acc085193832cbcf27ea
invalid w <wangjuesix@gmail.com>
2024-08-08 09:02:45 +0800

    chore: update props

--- packages/effects/common-ui/src/ellipsis-text/ellipsis-text.vue
+++ packages/effects/common-ui/src/ellipsis-text/ellipsis-text.vue
@@ -1,19 +1,54 @@
 <script setup lang="ts">
-import { computed, nextTick, ref, watchEffect } from 'vue';
-import type { CSSProperties } from 'vue';
+import { computed, type CSSProperties, nextTick, ref, watchEffect } from 'vue';
 
-import { VbenTooltip } from '@vben-core/shadcn-ui/components/tooltip';
+import { VbenTooltip } from '@vben-core/shadcn-ui';
 
 interface Props {
-  expand?: boolean; // 是否启用点击文本展开全部
-  line?: number; // 最大行数
-  maxWidth?: number | string; // 文本最大宽度
-  placement: 'bottom' | 'left' | 'right' | 'top'; // 提示框位置
-  tooltip?: boolean; // 是否启用文本提示框
-  tooltipBackgroundColor?: string; // 提示框背景颜色，优先级高于 overlayStyle
-  tooltipColor?: string; // 提示文本字体颜色，优先级高于 overlayStyle
-  tooltipFontSize?: number; // 提示文本字体大小，单位px，优先级高于 overlayStyle
-  tooltipMaxWidth?: number; // 提示框内容最大宽度，单位px，默认不设置时，提示文本内容自动与展示文本宽度保持一致
+  /**
+   * 是否启用点击文本展开全部
+   * @default false
+   */
+  expand?: boolean;
+  /**
+   * 文本最大行数
+   * @default 1
+   */
+  line?: number;
+  /**
+   * 文本最大宽度
+   * @default '100%'
+   */
+  maxWidth?: number | string;
+  /**
+   * 提示框位置
+   * @default 'top'
+   */
+  placement: 'bottom' | 'left' | 'right' | 'top';
+  /**
+   * 是否启用文本提示框
+   * @default true
+   */
+  tooltip?: boolean;
+  /**
+   * 提示框背景颜色，优先级高于 overlayStyle
+   */
+  tooltipBackgroundColor?: string;
+  /**
+   * 提示文本字体颜色，优先级高于 overlayStyle
+   */
+  tooltipColor?: string;
+  /**
+   * 提示文本字体大小，单位px，优先级高于 overlayStyle
+   */
+  tooltipFontSize?: number;
+  /**
+   * 提示框内容最大宽度，单位px，默认不设置时，提示文本内容自动与展示文本宽度保持一致
+   */
+  tooltipMaxWidth?: number;
+  /**
+   * 提示框内容区域样式
+   * @default { textAlign: 'justify' }
+   */
   tooltipOverlayStyle?: CSSProperties; // 提示框内容区域样式
 }
 const props = withDefaults(defineProps<Props>(), {
@@ -22,13 +57,13 @@ const props = withDefaults(defineProps<Props>(), {
   maxWidth: '100%',
   placement: 'top',
   tooltip: true,
-  tooltipBackgroundColor: 'red',
+  tooltipBackgroundColor: '',
   tooltipColor: '',
   tooltipFontSize: 14,
   tooltipMaxWidth: undefined,
   tooltipOverlayStyle: () => ({ textAlign: 'justify' }),
 });
-const emit = defineEmits(['expandChange']);
+const emit = defineEmits<{ expandChange: [boolean] }>();
 
 const textMaxWidth = computed(() => {
   if (typeof props.maxWidth === 'number') {
@@ -68,10 +103,11 @@ function onExpand() {
 <template>
   <VbenTooltip
     :content-style="{
+      ...tooltipOverlayStyle,
       maxWidth: `${defaultTooltipMaxWidth}px`,
       fontSize: `${tooltipFontSize}px`,
       color: tooltipColor,
-      ...tooltipOverlayStyle,
+      backgroundColor: tooltipBackgroundColor,
     }"
     :disabled="!showTooltip"
     :overlay-style="tooltipOverlayStyle"
