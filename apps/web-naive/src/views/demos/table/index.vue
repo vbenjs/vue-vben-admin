3ff8f9e72daac3c155eb05863f2da19a9eb448bc
likui628 <90845831+likui628@users.noreply.github.com>
2024-08-10 16:48:32 +0800

    chore: add demos

--- apps/web-naive/src/views/demos/table/index.vue
+++ apps/web-naive/src/views/demos/table/index.vue
@@ -27,7 +27,13 @@ const data = [
 </script>
 
 <template>
-  <Page>
+  <Page title="NDataTable">
+    <template #headerContent>
+      表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。
+    </template>
+    <template #footerContent>
+      <div>Footer Content</div>
+    </template>
     <NDataTable :columns="columns" :data="data" />
   </Page>
 </template>
