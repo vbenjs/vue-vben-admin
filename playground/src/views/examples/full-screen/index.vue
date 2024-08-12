a0a0ba7658f6596c32d9389813faa911fbe5a17e
invalid w <wangjuesix@gmail.com>
2024-08-12 18:26:06 +0800

    chore: rm unuse class

--- playground/src/views/examples/full-screen/index.vue
+++ playground/src/views/examples/full-screen/index.vue
@@ -16,7 +16,7 @@ const { isFullscreen: isDomFullscreen, toggle: toggleDom } =
 
 <template>
   <Page title="全屏示例">
-    <Card class="h-32 w-full rounded-md bg-white" title="Window Full Screen">
+    <Card title="Window Full Screen">
       <Button
         :disabled="isFullscreen"
         class="mr-2"
@@ -34,7 +34,7 @@ const { isFullscreen: isDomFullscreen, toggle: toggleDom } =
       Current State: {{ isFullscreen }}
     </Card>
 
-    <Card class="mt-5 w-full rounded-md bg-white" title="Dom Full Screen">
+    <Card title="Dom Full Screen">
       <Button class="mr-2" type="primary" @click="toggleDom">
         Enter Dom Full Screen
       </Button>
@@ -42,7 +42,7 @@ const { isFullscreen: isDomFullscreen, toggle: toggleDom } =
 
     <div
       ref="domRef"
-      class="mx-auto mt-10 flex h-64 w-1/2 items-center justify-center rounded-md bg-white"
+      class="mx-auto mt-10 flex h-64 w-1/2 items-center justify-center rounded-md bg-yellow-400"
     >
       <Button class="mr-2" type="primary" @click="toggleDom">
         {{ isDomFullscreen ? 'Exit Dom Full Screen' : 'Enter Dom Full Screen' }}
