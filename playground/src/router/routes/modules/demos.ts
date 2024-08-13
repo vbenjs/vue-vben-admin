6e6e35ae4a0848549a20168c779be175e36be1b2
invalid w <wangjuesix@gmail.com>
2024-08-13 11:11:13 +0800

    chore(@vben/playground): add a clipboard example (#4133)
    * chore(@vben/playground): 增加剪切板示例
    
    * chore: rm unuse code

--- playground/src/router/routes/modules/demos.ts
+++ playground/src/router/routes/modules/demos.ts
@@ -165,6 +165,15 @@ const routes: RouteRecordRaw[] = [
               },
             ],
           },
+          {
+            name: 'ClipboardDemo',
+            path: '/demos/features/clipboard',
+            component: () =>
+              import('#/views/demos/features/clipboard/index.vue'),
+            meta: {
+              title: $t('page.demos.features.clipboard'),
+            },
+          },
         ],
       },
       // 面包屑导航
