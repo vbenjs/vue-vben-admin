d14f7c787301559961ee75408ce4c2b141d03cf8
Li Kui <90845831+likui628@users.noreply.github.com>
2024-08-15 08:49:21 +0800

    fix: fix 500 error in mock server when accessToken is invalid

--- apps/backend-mock/api/auth/codes.ts
+++ apps/backend-mock/api/auth/codes.ts
@@ -1,15 +1,17 @@
 export default eventHandler((event) => {
-  const token = getHeader(event, 'Authorization');
+  try {
+    const token = getHeader(event, 'Authorization');
+    if (!token) {
+      throw new Error('UnauthorizedException');
+    }
 
-  if (!token) {
+    const username = Buffer.from(token, 'base64').toString('utf8');
+    const codes =
+      MOCK_CODES.find((item) => item.username === username)?.codes ?? [];
+
+    return useResponseSuccess(codes);
+  } catch {
     setResponseStatus(event, 401);
     return useResponseError('UnauthorizedException', 'Unauthorized Exception');
   }
-
-  const username = Buffer.from(token, 'base64').toString('utf8');
-
-  const codes =
-    MOCK_CODES.find((item) => item.username === username)?.codes ?? [];
-
-  return useResponseSuccess(codes);
 });
