178df972e628c90e951a6f3b82642c7418ae418e
likui628 <90845831+likui628@users.noreply.github.com>
2024-08-18 15:37:44 +0800

    chore: copy codes

--- apps/web-ele/src/api/request.ts
+++ apps/web-ele/src/api/request.ts
@@ -1,67 +1,82 @@
 /**
  * 该文件可自行根据业务逻辑进行调整
  */
-import type { HttpResponse } from '@vben/request';
-
 import { useAppConfig } from '@vben/hooks';
 import { preferences } from '@vben/preferences';
-import { RequestClient } from '@vben/request';
+import { errorMessageResponseInterceptor, RequestClient } from '@vben/request';
 import { useAccessStore } from '@vben/stores';
 
 import { ElMessage } from 'element-plus';
 
-import { useAuthStore } from '#/store';
+import { refreshTokenApi } from '#/api/core';
 
 const { apiURL } = useAppConfig(import.meta.env, import.meta.env.PROD);
 
 function createRequestClient(baseURL: string) {
   const client = new RequestClient({
     baseURL,
-    // 为每个请求携带 Authorization
-    makeAuthorization: () => {
-      return {
-        // 默认
-        key: 'Authorization',
-        tokenHandler: () => {
-          const accessStore = useAccessStore();
-          return {
-            refreshToken: `${accessStore.refreshToken}`,
-            token: `${accessStore.accessToken}`,
-          };
-        },
-        unAuthorizedHandler: async () => {
-          const accessStore = useAccessStore();
-          const authStore = useAuthStore();
-          accessStore.setAccessToken(null);
+  });
 
-          if (preferences.app.loginExpiredMode === 'modal') {
-            accessStore.setLoginExpired(true);
-          } else {
-            // 退出登录
-            await authStore.logout();
-          }
-        },
-      };
-    },
-    makeErrorMessage: (msg) => ElMessage.error(msg),
+  client.addRequestInterceptor({
+    fulfilled: (config) => {
+      const accessStore = useAccessStore();
 
-    makeRequestHeaders: () => {
-      return {
-        // 为每个请求携带 Accept-Language
-        'Accept-Language': preferences.app.locale,
-      };
+      config.headers.Authorization = `Bearer ${accessStore.accessToken}`;
+      config.headers['Accept-Language'] = preferences.app.locale;
+
+      return config;
     },
   });
-  client.addResponseInterceptor<HttpResponse>((response) => {
-    const { data: responseData, status } = response;
 
-    const { code, data, message: msg } = responseData;
-    if (status >= 200 && status < 400 && code === 0) {
-      return data;
-    }
-    throw new Error(`Error ${status}: ${msg}`);
+  client.addResponseInterceptor({
+    fulfilled: (response) => {
+      const { data: responseData, status } = response;
+
+      const { code, data, message: msg } = responseData;
+      if (status >= 200 && status < 400 && code === 0) {
+        return data;
+      }
+      throw new Error(`Error ${status}: ${msg}`);
+    },
   });
+
+  client.addResponseInterceptor({
+    rejected: async (error) => {
+      // 非refreshToken的处理方式
+      // if (error.response.status === 401) {
+      //    const accessStore = useAccessStore();
+      //    const authStore = useAuthStore();
+      //    accessStore.setAccessToken(null);
+      //
+      //    if (preferences.app.loginExpiredMode === 'modal') {
+      //      accessStore.setLoginExpired(true);
+      //    } else {
+      //      await authStore.logout();
+      //    }
+      //  }
+
+      // refreshToken的处理方式
+      const prevRequest = error.config;
+      const accessStore = useAccessStore();
+      if (error.response.status === 401 && !prevRequest?.sent) {
+        prevRequest.sent = true;
+        const resp = await refreshTokenApi();
+
+        accessStore.setAccessToken(resp.data);
+        return client.request(prevRequest.url, { ...prevRequest });
+      }
+      throw error;
+    },
+  });
+
+  client.addResponseInterceptor({
+    rejected: () =>
+      errorMessageResponseInterceptor((msg: any) => ElMessage.error(msg)),
+  });
+
   return client;
 }
 
 export const requestClient = createRequestClient(apiURL);
+
+export const baseRequestClient = new RequestClient({ baseURL: apiURL });
--- apps/web-naive/src/store/auth.ts
+++ apps/web-naive/src/store/auth.ts
@@ -33,13 +33,12 @@ export const useAuthStore = defineStore('auth', () => {
     let userInfo: null | UserInfo = null;
     try {
       loginLoading.value = true;
-      const { accessToken, refreshToken } = await loginApi(params);
+      const { accessToken } = await loginApi(params);
 
       // 如果成功获取到 accessToken
       if (accessToken) {
         // 将 accessToken 存储到 accessStore 中
         accessStore.setAccessToken(accessToken);
-        accessStore.setRefreshToken(refreshToken);
 
         // 获取用户信息并存储到 accessStore 中
         const [fetchUserInfoResult, accessCodes] = await Promise.all([
