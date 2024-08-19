c4b4564118b7beb57fe6be7e5d06b7353a8fb248
vince <vince292007@gmail.com>
2024-08-19 23:21:34 +0800

    fix: mock server deployment error

--- apps/backend-mock/utils/jwt-utils.ts
+++ apps/backend-mock/utils/jwt-utils.ts
@@ -4,17 +4,21 @@ import jwt from 'jsonwebtoken';
 
 import { UserInfo } from './mock-data';
 
+// TODO: Replace with your own secret key
+const ACCESS_TOKEN_SECRET = 'access_token_secret';
+const REFRESH_TOKEN_SECRET = 'refresh_token_secret';
+
 export interface UserPayload extends UserInfo {
   iat: number;
   exp: number;
 }
 
 export function generateAccessToken(user: UserInfo) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+  return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
 }
 
 export function generateRefreshToken(user: UserInfo) {
-  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+  return jwt.sign(user, REFRESH_TOKEN_SECRET, {
     expiresIn: '30d',
   });
 }
@@ -29,10 +33,7 @@ export function verifyAccessToken(
 
   const token = authHeader.split(' ')[1];
   try {
-    const decoded = jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET,
-    ) as UserPayload;
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET) as UserPayload;
 
     const username = decoded.username;
     const user = MOCK_USERS.find((item) => item.username === username);
@@ -47,10 +48,7 @@ export function verifyRefreshToken(
   token: string,
 ): null | Omit<UserInfo, 'password'> {
   try {
-    const decoded = jwt.verify(
-      token,
-      process.env.REFRESH_TOKEN_SECRET,
-    ) as UserPayload;
+    const decoded = jwt.verify(token, REFRESH_TOKEN_SECRET) as UserPayload;
     const username = decoded.username;
     const user = MOCK_USERS.find((item) => item.username === username);
     const { password: _pwd, ...userinfo } = user;
