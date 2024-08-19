f4e74d8d688dc3f20d5a6e207ea5580c03433d5f
vince <vince292007@gmail.com>
2024-08-19 22:45:35 +0800

    fix: ci error

--- apps/web-antd/src/store/auth.ts
+++ apps/web-antd/src/store/auth.ts
@@ -75,7 +75,7 @@ export const useAuthStore = defineStore('auth', () => {
     };
   }
 
-  async function logout() {
+  async function logout(redirect: boolean = true) {
     await logoutApi();
     resetAllStores();
     accessStore.setLoginExpired(false);
@@ -83,9 +83,11 @@ export const useAuthStore = defineStore('auth', () => {
     // 回登陆页带上当前路由地址
     await router.replace({
       path: LOGIN_PATH,
-      query: {
-        redirect: encodeURIComponent(router.currentRoute.value.fullPath),
-      },
+      query: redirect
+        ? {
+            redirect: encodeURIComponent(router.currentRoute.value.fullPath),
+          }
+        : {},
     });
   }
 
