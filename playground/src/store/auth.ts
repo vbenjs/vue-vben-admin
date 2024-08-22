1a8935b8ba5e13ef89542835629051204f7d5749
handsomeFu <fzq980219@gmail.com>
2024-08-22 16:08:50 +0800

    docs(other): update function parameters jsdoc
    - 移除`vue-router.d.ts`中`activePath`属性的默认值注释。
    - 重构`generateRoutes`函数签名，增加`options`参数。
    - 调整`authLogin`函数文档，增加`onSuccess`回调函数的说明。

--- playground/src/store/auth.ts
+++ playground/src/store/auth.ts
@@ -24,6 +24,7 @@ export const useAuthStore = defineStore('auth', () => {
    * 异步处理登录操作
    * Asynchronously handle the login process
    * @param params 登录表单数据
+   * @param onSuccess 成功之后的回调函数
    */
   async function authLogin(
     params: LoginAndRegisterParams,
