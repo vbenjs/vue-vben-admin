30851e95c33ce32fdc6a83e29bbf5d905eac9771
likui628 <90845831+likui628@users.noreply.github.com>
2024-09-03 07:26:00 +0800

    feat: error retry

---
+++ playground/src/views/demos/features/vue-query/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Page } from '@vben/common-ui';
+
+import { Card } from 'ant-design-vue';
+
+import Retry from './retry.vue';
+</script>
+
+<template>
+  <Page title="Vue Query示例">
+    <Card title="错误重试">
+      <Retry />
+    </Card>
+  </Page>
+</template>
