371e0be93327b726f378e000873d2fee5adfbe39
dap <15891557205@163.com>
2024-09-15 10:20:11 +0800

    fix: hover border style same as antd style when validate error

--- packages/@core/ui-kit/form-ui/src/form-render/form-field.vue
+++ packages/@core/ui-kit/form-ui/src/form-render/form-field.vue
@@ -258,7 +258,8 @@ function createComponentProps(slotProps: Record<string, any>) {
             <component
               :is="fieldComponent"
               :class="{
-                'border-destructive focus:border-destructive': isInValid,
+                'border-destructive focus:border-destructive hover:border-destructive':
+                  isInValid,
               }"
               v-bind="createComponentProps(slotProps)"
               :disabled="shouldDisabled"
