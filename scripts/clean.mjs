47eebe03807c5c1fbf187f7f2498153245b5ff4f
vince <vince292007@gmail.com>
2024-09-26 11:46:23 +0800

    fix: lint error

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -2,18 +2,14 @@ import { promises as fs } from 'node:fs';
 import { join } from 'node:path';
 
 const rootDir = process.cwd();
-// 要删除的目录及文件名称
-const targets = ['node_modules', 'dist', '.turbo', 'dist.zip'];
 
 /**
  * 递归查找并删除目标目录
  * @param {string} currentDir - 当前遍历的目录路径
  */
-async function cleanTargetsRecursively(currentDir, deletaLockFile) {
+async function cleanTargetsRecursively(currentDir, targets) {
   const items = await fs.readdir(currentDir);
-  if (deletaLockFile) {
-    targets.push('pnpm-lock.yaml');
-  }
+
   for (const item of items) {
     try {
       const itemPath = join(currentDir, item);
@@ -24,22 +20,32 @@ async function cleanTargetsRecursively(currentDir, deletaLockFile) {
       }
       const stat = await fs.lstat(itemPath);
       if (stat.isDirectory()) {
-        await cleanTargetsRecursively(itemPath, deletaLockFile);
+        await cleanTargetsRecursively(itemPath, targets);
       }
     } catch {
-      // console.error(`Error handling directory ${currentDir}: ${error.message}`);
+      // console.error(
+      //   `Error handling item ${item} in ${currentDir}: ${error.message}`,
+      // );
     }
   }
 }
 
 (async function startCleanup() {
+  // 要删除的目录及文件名称
+  const targets = ['node_modules', 'dist', '.turbo', 'dist.zip'];
+
+  const deleteLockFile = process.argv.includes('--del-lock');
+  const cleanupTargets = [...targets];
+  if (deleteLockFile) {
+    cleanupTargets.push('pnpm-lock.yaml');
+  }
+
   console.log(
-    `Starting cleanup of targets: ${targets.join(', ')} from root: ${rootDir}`,
+    `Starting cleanup of targets: ${cleanupTargets.join(', ')} from root: ${rootDir}`,
   );
 
-  const deletaLockFile = process.argv.includes('--del-lock');
   try {
-    await cleanTargetsRecursively(rootDir, deletaLockFile);
+    await cleanTargetsRecursively(rootDir, cleanupTargets);
     console.log('Cleanup process completed.');
   } catch (error) {
     console.error(`Unexpected error during cleanup: ${error.message}`);
