3572ce1538a20e7176d2cb2f75f8962fd08ef0ab
Vben <ann.vben@gmail.com>
2024-09-29 22:15:43 +0800

    fix: when multiple pop-ups exist at the same time, clicking will close all (#4548)

--- packages/@core/ui-kit/shadcn-ui/src/components/ui/dialog/DialogOverlay.vue
+++ packages/@core/ui-kit/shadcn-ui/src/components/ui/dialog/DialogOverlay.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { inject } from 'vue';
+
 import { useScrollLock } from '@vben-core/composables';
 
 useScrollLock();
+const id = inject('DISMISSABLE_MODAL_ID');
 </script>
 <template>
   <div
+    :data-dismissable-modal="id"
     class="bg-overlay fixed inset-0 z-[1000]"
-    data-dismissable-modal="true"
   ></div>
 </template>
