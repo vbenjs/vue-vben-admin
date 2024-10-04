0285691518b29c3de6081fb92833882d86723023
vben <ann.vben@gmail.com>
2024-10-04 22:54:58 +0800

    chore: add adapter

--- packages/effects/plugins/src/vxe-table/index.ts
+++ packages/effects/plugins/src/vxe-table/index.ts
@@ -1,4 +1,4 @@
 export { setupVbenVxeTable } from './init';
 export * from './use-vxe-grid';
 export { default as VbenVxeGrid } from './use-vxe-grid.vue';
-export type * from 'vxe-table';
+export type { VxeGridListeners, VxeGridProps } from 'vxe-table';
