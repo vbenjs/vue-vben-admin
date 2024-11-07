8f86bfff8fc5fc98e1477ba95521ccb77b7ff272
GavinLucky <gavin_global@163.com>
2024-11-07 23:53:42 +0800

    feat: 恢复原始逻辑

--- playground/src/views/demos/features/crypto/inner/CryptoHooks.ts
+++ playground/src/views/demos/features/crypto/inner/CryptoHooks.ts
@@ -1,42 +1,59 @@
 import { CryptoJS } from '@vben/utils';
 
-type HashAlgorithm =
-  | 'MD5'
-  | 'RIPEMD160'
-  | 'SHA1'
-  | 'SHA224'
-  | 'SHA256'
-  | 'SHA384'
-  | 'SHA512'
-  | 'SHA3224'
-  | 'SHA3256'
-  | 'SHA3384'
-  | 'SHA3512';
+export function hashingFn(inputMessage: string, type: string) {
+  const algorithms = type;
 
-export function hashingFn(inputMessage: string, type: HashAlgorithm): string {
-  if (!inputMessage) {
-    throw new Error('Input message cannot be empty');
+  let hashingOutput;
+  switch (algorithms) {
+    case 'MD5': {
+      hashingOutput = CryptoJS.MD5(inputMessage);
+      break;
+    }
+    case 'RIPEMD160': {
+      hashingOutput = CryptoJS.RIPEMD160(inputMessage);
+      break;
+    }
+    case 'SHA1': {
+      hashingOutput = CryptoJS.SHA1(inputMessage);
+      break;
+    }
+    case 'SHA224': {
+      hashingOutput = CryptoJS.SHA224(inputMessage);
+      break;
+    }
+    case 'SHA256': {
+      hashingOutput = CryptoJS.SHA256(inputMessage);
+      break;
+    }
+    case 'SHA384': {
+      hashingOutput = CryptoJS.SHA384(inputMessage);
+      break;
+    }
+    case 'SHA512': {
+      hashingOutput = CryptoJS.SHA512(inputMessage);
+      break;
+    }
+    case 'SHA3224': {
+      hashingOutput = CryptoJS.SHA3(inputMessage, { outputLength: 224 });
+      break;
+    }
+    case 'SHA3256': {
+      hashingOutput = CryptoJS.SHA3(inputMessage, { outputLength: 256 });
+      break;
+    }
+    case 'SHA3384': {
+      hashingOutput = CryptoJS.SHA3(inputMessage, { outputLength: 384 });
+      break;
+    }
+    case 'SHA3512': {
+      hashingOutput = CryptoJS.SHA3(inputMessage, { outputLength: 512 });
+      break;
+    }
+    default: {
+      hashingOutput = '';
+    }
   }
-
-  const sha3Variants = {
-    SHA3224: 224,
-    SHA3256: 256,
-    SHA3384: 384,
-    SHA3512: 512,
-  };
-
-  if (type.startsWith('SHA3')) {
-    return CryptoJS.SHA3(inputMessage, {
-      outputLength: sha3Variants[type as keyof typeof sha3Variants],
-    }).toString();
-  }
-
-  const hashFunction = CryptoJS[type];
-  if (!hashFunction) {
-    throw new Error(`Unsupported hash algorithm: ${type}`);
-  }
-
-  return hashFunction(inputMessage).toString();
+  return hashingOutput.toString();
 }
 
 export function aesEncryptFn(inputMessage: string, secret: string) {
