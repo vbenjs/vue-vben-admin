50b5fa72b03368dc42dcd09caa27c85439bbee20
Arthur Darkstone <arthurlli@qq.com>
2024-11-11 01:52:39 +0800

    feat: resize component

---
+++ packages/effects/plugins/src/resize/resize.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  w?: string;
+  h?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  w: '300px',
+  h: '100%',
+});
+</script>
+
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
