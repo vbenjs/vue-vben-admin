e12b35c7cfaec9408f42a3abe8631e2a70252db9
GavinLucky <gavin_global@163.com>
2024-11-13 18:43:33 +0800

    feat:  form  表单-指定哦组建添加一个标准自定义插槽示例

---
+++ playground/src/views/examples/form/costom-sub/graph-validate-code.vue
@@ -0,0 +1,129 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+
+import { Input, Spin, Tooltip } from 'ant-design-vue';
+
+/** 图形验证码 */
+
+defineOptions({
+  name: 'GraphValidateCode',
+});
+interface IGraphValidateCodeProps {
+  /** 获取图片Api接口 */
+  propApi?: () => Promise<string>;
+  /** 校验失败状态 */
+  propValidateFailed: boolean;
+}
+// eslint-disable-next-line vue/define-macros-order
+const props = defineProps<IGraphValidateCodeProps>();
+const emit = defineEmits<{
+  (e: 'onEmitOnBlur'): void;
+  (e: 'onEmitOnChange'): void;
+  (e: 'onEmitOnFocus'): void;
+}>();
+
+const validateFailedComputed = computed(() => {
+  return !!props.propValidateFailed;
+});
+
+const imgLoadingStateRef = ref(false);
+const inputValue = defineModel('value');
+const imgDataRef = ref();
+const loadImgDataApiFn = () => {
+  return new Promise((resolve, reject) => {
+    if (props.propApi) {
+      imgLoadingStateRef.value = true;
+      props
+        .propApi()
+        .then((res) => {
+          // console.log('res', res);
+          imgDataRef.value = res;
+
+          resolve(res);
+        })
+        .catch((error) => {
+          console.error('e', error);
+          reject(error);
+        })
+        .finally(() => {
+          imgLoadingStateRef.value = false;
+        });
+    }
+  });
+};
+const updateImgDataFn = () => {
+  loadImgDataApiFn().then().catch();
+};
+
+const handleBlur = () => {
+  // console.log('handleBlur');
+  emit('onEmitOnBlur');
+  // 光标失焦
+};
+const handleFocus = () => {
+  // console.log('handleFocus');
+  emit('onEmitOnFocus');
+  // 光标聚焦
+};
+const handleChange = (e) => {
+  // console.log('handleChange', e);
+  emit('onEmitOnChange', e);
+  // 输入框内容改变
+};
+onMounted(() => {
+  setTimeout(() => {
+    loadImgDataApiFn();
+  }, 100);
+});
+defineExpose({
+  loadImgFn: () => {
+    return loadImgDataApiFn();
+  },
+});
+</script>
+
+<template>
+  <div
+    class="graph-validate-code-cls flex w-full flex-row items-center justify-between"
+  >
+    <div class="mr-[2px] w-1/2 flex-1">
+      <Input
+        v-model:value="inputValue"
+        :class="[
+          validateFailedComputed ? 'border-destructive' : 'border-light',
+        ]"
+        placeholder="请输入验证码"
+        @blur="handleBlur"
+        @change="handleChange"
+        @focus="handleFocus"
+      />
+    </div>
+
+    <div class="w-1/2 flex-1">
+      <Spin :spinning="imgLoadingStateRef" size="small">
+        <Tooltip placement="top">
+          <template #title>
+            <span>点击更新验证码</span>
+          </template>
+
+          <div
+            class="hover:border-primary border-light ml-[2px] box-border cursor-pointer rounded-[8px] border px-[5px] py-[1px]"
+            @click="updateImgDataFn"
+          >
+            <img
+              :src="imgDataRef"
+              :style="{
+                height: '28px',
+                width: 'auto',
+              }"
+              alt=""
+              class="mx-auto h-full object-contain"
+            />
+          </div>
+        </Tooltip>
+      </Spin>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- playground/src/views/examples/form/custom.vue
+++ playground/src/views/examples/form/custom.vue
@@ -6,6 +6,17 @@ import { Page } from '@vben/common-ui';
 import { Card, Input, message } from 'ant-design-vue';
 
 import { useVbenForm } from '#/adapter/form';
+import { getBase64Img } from '#/views/examples/form/costom-sub/img.data';
+
+import GraphValidateCode from './costom-sub/graph-validate-code.vue';
+
+const getImgApi = () => {
+  return new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(getBase64Img());
+    }, 500);
+  });
+};
 
 const [Form] = useVbenForm({
   // 所有表单项共用，可单独在表单内覆盖
@@ -44,15 +55,53 @@ const [Form] = useVbenForm({
       label: '自定义组件',
       rules: 'required',
     },
+  ],
+  // 中屏一行显示2个，小屏一行显示1个
+  wrapperClass: 'grid-cols-1 md:grid-cols-2 ',
+});
+const [FormCustom] = useVbenForm({
+  // 所有表单项共用，可单独在表单内覆盖
+  commonConfig: {
+    // 所有表单项
+    componentProps: {
+      class: 'w-full ',
+    },
+    labelClass: 'w-2/6',
+  },
+  // 提交函数
+  handleSubmit: onSubmit,
+  // 垂直布局，label和input在不同行，值为vertical
+  // 水平布局，label和input在同一行
+  layout: 'horizontal',
+  schema: [
+    {
+      // 组件需要在 #/adapter.ts内注册，并加上类型
+      component: 'Input',
+      fieldName: 'field',
+      label: '自定义后缀',
+      suffix: () => h('span', { class: 'text-red-600' }, '元'),
+    },
     {
       component: 'Input',
-      fieldName: 'field3',
+      fieldName: 'field1',
+      label: '是用前后缀slot',
+      renderComponentContent: () => ({
+        prefix: () => 'prefix',
+        suffix: () => 'suffix',
+      }),
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        api: getImgApi,
+      },
+      fieldName: 'code',
       label: '自定义组件(slot)',
       rules: 'required',
     },
   ],
   // 中屏一行显示2个，小屏一行显示1个
-  wrapperClass: 'grid-cols-1 md:grid-cols-2',
+  wrapperClass: 'grid-cols-1 md:grid-cols-2 ',
 });
 
 function onSubmit(values: Record<string, any>) {
@@ -64,12 +113,27 @@ function onSubmit(values: Record<string, any>) {
 
 <template>
   <Page description="表单组件自定义示例" title="表单组件">
-    <Card title="基础示例">
+    <Card class="mb-[20px]" title="基础示例">
       <Form>
         <template #field3="slotProps">
           <Input placeholder="请输入" v-bind="slotProps" />
         </template>
       </Form>
+    </Card>
+    <Card title="自定义插槽示例">
+      <FormCustom>
+        <template #code="props">
+          <GraphValidateCode
+            :prop-api="props.api"
+            :prop-validate-failed="props.isInValid"
+            :value="props.value"
+            @on-emit-on-blur="props.handleBlur()"
+            @on-emit-on-change="props.handleChange($event)"
+            @on-emit-on-focus="() => {}"
+            @update:value="(nV) => props.setValue(nV)"
+          />
+        </template>
+      </FormCustom>
     </Card>
   </Page>
 </template>
