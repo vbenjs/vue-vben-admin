12f216c0e7395caca21112064f0734c8ded424b3
leizhiyou <34797441+leizhiyou@users.noreply.github.com>
2024-12-01 21:48:54 +0800

    fix: form 表单不支持field.xxx.xx格式的defaultValue配置 (#4965)
    * fix: form 表单不支持field.xxx.xx格式的defaultValue配置
    
    * chore: 修复代码规范问题

--- playground/src/views/examples/form/basic.vue
+++ playground/src/views/examples/form/basic.vue
@@ -291,6 +291,12 @@ const [CustomLayoutForm] = useVbenForm({
       formItemClass: 'col-start-1',
       label: '字符串',
     },
+    {
+      component: 'Input',
+      defaultValue: 'field4.path',
+      fieldName: 'field4.path',
+      label: 'field4.path',
+    },
   ],
   // 一共三列
   wrapperClass: 'grid-cols-3',
