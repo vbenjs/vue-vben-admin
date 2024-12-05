86eac7857784ffa3fba3634421bd19fefff12a38
ihuangxiaomin <1219549841@qq.com>
2024-12-05 10:22:08 +0800

    fix: 处理select 选择长选项导致样式超出的问题

--- packages/@core/ui-kit/form-ui/src/form-render/form-field.vue
+++ packages/@core/ui-kit/form-ui/src/form-render/form-field.vue
@@ -298,7 +298,11 @@ function autofocus() {
       >
         {{ label }}
       </FormLabel>
-      <div :class="cn('relative flex w-full items-center', wrapperClass)">
+      <div
+        :class="
+          cn('relative flex w-full items-center overflow-hidden', wrapperClass)
+        "
+      >
         <FormControl :class="cn(controlClass)">
           <slot
             v-bind="{
