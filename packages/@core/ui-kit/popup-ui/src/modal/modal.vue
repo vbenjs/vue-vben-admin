ca6503612b31b553f6c920bce40efe58ddc27803
Netfan <netfan@foxmail.com>
2024-12-07 09:18:08 +0800

    fix: modal radius not follow preferences

--- packages/@core/ui-kit/popup-ui/src/modal/modal.vue
+++ packages/@core/ui-kit/popup-ui/src/modal/modal.vue
@@ -172,7 +172,7 @@ function handleFocusOutside(e: Event) {
       ref="contentRef"
       :class="
         cn(
-          'left-0 right-0 top-[10vh] mx-auto flex max-h-[80%] w-[520px] flex-col p-0 sm:rounded-2xl',
+          'left-0 right-0 top-[10vh] mx-auto flex max-h-[80%] w-[520px] flex-col p-0 sm:rounded-[var(--radius)]',
           modalClass,
           {
             'border-border border': bordered,
