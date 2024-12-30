5a36c2fb4ce5dfb41e868e3977af8f6aa590754e
Netfan <netfan@foxmail.com>
2024-12-30 20:26:15 +0800

    feat: modal support destroy on close

--- packages/@core/ui-kit/popup-ui/src/modal/modal.ts
+++ packages/@core/ui-kit/popup-ui/src/modal/modal.ts
@@ -144,6 +144,10 @@ export interface ModalApiOptions extends ModalState {
    */
   connectedComponent?: Component;
   /**
+   * 在关闭时销毁弹窗。仅在使用 connectedComponent 时有效
+   */
+  destroyOnClose?: boolean;
+  /**
    * 关闭前的回调，返回 false 可以阻止关闭
    * @returns
    */
