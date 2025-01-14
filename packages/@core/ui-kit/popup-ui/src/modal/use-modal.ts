e1c503e51e64485ca5fadbd9fa1e6d22329c4ced
Netfan <netfan@foxmail.com>
2025-01-14 17:11:18 +0800

    feat: support set default props for drawer and modal (#5390)
    * feat: support set default props for drawer and modal
    
    * docs: fix typo

--- packages/@core/ui-kit/popup-ui/src/modal/use-modal.ts
+++ packages/@core/ui-kit/popup-ui/src/modal/use-modal.ts
@@ -17,6 +17,12 @@ import VbenModal from './modal.vue';
 
 const USER_MODAL_INJECT_KEY = Symbol('VBEN_MODAL_INJECT');
 
+const DEFAULT_MODAL_PROPS: Partial<ModalProps> = {};
+
+export function setDefaultModalProps(props: Partial<ModalProps>) {
+  Object.assign(DEFAULT_MODAL_PROPS, props);
+}
+
 export function useVbenModal<TParentModalProps extends ModalProps = ModalProps>(
   options: ModalApiOptions = {},
 ) {
@@ -68,6 +74,7 @@ export function useVbenModal<TParentModalProps extends ModalProps = ModalProps>(
   const injectData = inject<any>(USER_MODAL_INJECT_KEY, {});
 
   const mergedOptions = {
+    ...DEFAULT_MODAL_PROPS,
     ...injectData.options,
     ...options,
   } as ModalApiOptions;
