e225159cce49eea76f5b5f4796cfc3686d6d684d
Netfan <netfan@foxmail.com>
2025-01-22 00:59:10 +0800

    fix: request download and upload not support `responseReturn` (#5456)
    * fix: request download and upload not support `responseReturn`
    
    * docs: update
    
    * fix: type of request client upload result

--- packages/effects/request/src/request-client/types.ts
+++ packages/effects/request/src/request-client/types.ts
@@ -7,9 +7,9 @@ import type {
 
 type ExtendOptions = {
   /** 响应数据的返回方式。
-   * raw: 原始的AxiosResponse，包括headers、status等。
-   * body: 返回响应数据的BODY部分。
-   * data: 解构响应的BODY数据，只返回其中的data节点数据。
+   * raw: 原始的AxiosResponse，包括headers、status等，不做是否成功请求的检查。
+   * body: 返回响应数据的BODY部分（只会根据status检查请求是否成功，忽略对code的判断，这种情况下应由调用方检查请求是否成功）。
+   * data: 解构响应的BODY数据，只返回其中的data节点数据（会检查status和code是否为成功状态）。
    */
   responseReturn?: 'body' | 'data' | 'raw';
 };
