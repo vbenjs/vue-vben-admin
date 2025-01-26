67d1f299b3b7f9c9760b5adcff074796597f80f4
Netfan <netfan@foxmail.com>
2025-01-26 22:33:16 +0800

    fix: renderComponentContent lose slot props data (#5466)
    * 修复FormItem传递插槽时丢失插槽props的问题

--- packages/@core/ui-kit/form-ui/src/form-render/form-field.vue
+++ packages/@core/ui-kit/form-ui/src/form-render/form-field.vue
@@ -329,10 +329,14 @@ function autofocus() {
               v-bind="createComponentProps(slotProps)"
               :disabled="shouldDisabled"
             >
-              <template v-for="name in renderContentKey" :key="name" #[name]>
+              <template
+                v-for="name in renderContentKey"
+                :key="name"
+                #[name]="renderSlotProps"
+              >
                 <VbenRenderContent
                   :content="customContentRender[name]"
-                  v-bind="slotProps"
+                  v-bind="{ ...renderSlotProps, $formContext: slotProps }"
                 />
               </template>
               <!-- <slot></slot> -->
