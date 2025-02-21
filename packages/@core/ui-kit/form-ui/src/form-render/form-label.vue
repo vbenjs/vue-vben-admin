d3593007ca54341eb74de133b9f62eb544258d9e
Netfan <netfan@foxmail.com>
2025-02-21 11:13:27 +0800

    fix: empty label mark with required rules

--- packages/@core/ui-kit/form-ui/src/form-render/form-label.vue
+++ packages/@core/ui-kit/form-ui/src/form-render/form-label.vue
@@ -21,7 +21,7 @@ const props = defineProps<Props>();
 
 <template>
   <FormLabel :class="cn('flex items-center', props.class)">
-    <span v-if="required && label" class="text-destructive mr-[2px]">*</span>
+    <span v-if="required" class="text-destructive mr-[2px]">*</span>
     <slot></slot>
     <VbenHelpTooltip v-if="help" trigger-class="size-3.5 ml-1">
       <VbenRenderContent :content="help" />
