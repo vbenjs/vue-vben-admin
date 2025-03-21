6ed04dd58dfc05d5f4bfd1e0b85bb7fe800fabfa
Netfan <netfan@foxmail.com>
2025-03-21 14:23:41 +0800

    fix: default value for nested fields

--- packages/@core/base/shared/src/utils/index.ts
+++ packages/@core/base/shared/src/utils/index.ts
@@ -17,3 +17,4 @@ export * from './window';
 export { default as cloneDeep } from 'lodash.clonedeep';
 export { default as get } from 'lodash.get';
 export { default as isEqual } from 'lodash.isequal';
+export { default as set } from 'lodash.set';
