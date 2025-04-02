c623604ea91f82117edffe1d52677e4aeca7fa2c
Netfan <netfan@foxmail.com>
2025-04-02 20:34:38 +0800

    docs: fix alert demo in docs

--- docs/src/demos/vben-alert/confirm/index.vue
+++ docs/src/demos/vben-alert/confirm/index.vue
@@ -20,8 +20,11 @@ function showIconConfirm() {
 
 function showAsyncConfirm() {
   confirm({
-    beforeClose() {
-      return new Promise((resolve) => setTimeout(resolve, 2000));
+    beforeClose({ isConfirm }) {
+      if (isConfirm) {
+        // 这里可以执行一些异步操作。如果最终返回了false，将阻止关闭弹窗
+        return new Promise((resolve) => setTimeout(resolve, 2000));
+      }
     },
     content: 'This is an alert message with async confirm',
     icon: 'success',
