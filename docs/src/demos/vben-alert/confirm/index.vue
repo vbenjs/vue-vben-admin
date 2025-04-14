f0e9e55af23486ef701ceb667c12ee9498db6863
Netfan <netfan@foxmail.com>
2025-04-14 11:48:21 +0800

    feat: alert support customize `footer` (#5940)
    * Alert组件支持自定义footer

--- docs/src/demos/vben-alert/confirm/index.vue
+++ docs/src/demos/vben-alert/confirm/index.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
+import { h, ref } from 'vue';
+
 import { alert, confirm, VbenButton } from '@vben/common-ui';
+
+import { Checkbox, message } from 'ant-design-vue';
 
 function showConfirm() {
   confirm('This is an alert message')
@@ -15,6 +19,34 @@ function showIconConfirm() {
   confirm({
     content: 'This is an alert message with icon',
     icon: 'success',
+  });
+}
+
+function showfooterConfirm() {
+  const checked = ref(false);
+  confirm({
+    cancelText: '不要虾扯蛋',
+    confirmText: '是的，我们都是NPC',
+    content:
+      '刚才发生的事情，为什么我似乎早就经历过一般？\n我甚至能在事情发生过程中潜意识里预知到接下来会发生什么。\n\n听起来挺玄乎的，你有过这种感觉吗？',
+    footer: () =>
+      h(
+        Checkbox,
+        {
+          checked: checked.value,
+          class: 'flex-1',
+          'onUpdate:checked': (v) => (checked.value = v),
+        },
+        '不再提示',
+      ),
+    icon: 'question',
+    title: '未解之谜',
+  }).then(() => {
+    if (checked.value) {
+      message.success('我不会再拿这个问题烦你了');
+    } else {
+      message.info('下次还要继续问你哟');
+    }
   });
 }
 
@@ -37,6 +69,7 @@ function showAsyncConfirm() {
   <div class="flex gap-4">
     <VbenButton @click="showConfirm">Confirm</VbenButton>
     <VbenButton @click="showIconConfirm">Confirm With Icon</VbenButton>
+    <VbenButton @click="showfooterConfirm">Confirm With Footer</VbenButton>
     <VbenButton @click="showAsyncConfirm">Async Confirm</VbenButton>
   </div>
 </template>
