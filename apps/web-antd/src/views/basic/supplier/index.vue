a74a978afd43fd3a3a435e3c2a741bdbf58f8a8e
y393920685 <393920685@qq.cm>
2025-05-01 07:31:28 +0800

    add supplier

--- apps/web-antd/src/views/basic/supplier/index.vue
+++ apps/web-antd/src/views/basic/supplier/index.vue
@@ -1,8 +1,162 @@
 <script lang="ts" setup>
-import { Page } from '@vben/common-ui';
+import type { VxeGridProps } from '#/adapter/vxe-table';
+import type { SupplierApi } from '#/api';
 
-defineOptions({ name: 'Supplier' });
+import { Button, InputSearch, Modal } from 'ant-design-vue';
+import { h, reactive } from 'vue';
+import { Page, useVbenModal } from '@vben/common-ui';
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import { getSupplierListApi, deleteSupplierApi } from '#/api';
+import ExtraModal from './modal.vue';
+
+defineOptions({ name: `Supplier` });
+
+const form = reactive({
+  keyword: '',
+});
+const gridOptions: VxeGridProps = {
+  columns: [
+    { field: 'id', title: '编码', width: 70, fixed: 'left' },
+    { field: 'name', title: '名称', width: 200, fixed: 'left' },
+    { field: 'pinyin', title: '拼音', width: 120 },
+    { field: 'phone', title: '手机号', width: 120 },
+    { field: 'contact', title: '联系人', width: 100 },
+    { field: 'company', title: '公司名称', width: 150 },
+    { field: 'taxNo', title: '税号', width: 200 },
+    { field: 'bankName', title: '开户银行', width: 150 },
+    { field: 'bankAccount', title: '银行账号', width: 150 },
+    { field: 'bankHolder', title: '开户人', width: 100 },
+    {
+      field: 'bankAddress',
+      title: '开户地址',
+      width: 200,
+      align: 'left',
+      headerAlign: 'center',
+    },
+    {
+      field: 'address',
+      title: '地址',
+      width: 200,
+      align: 'left',
+      headerAlign: 'center',
+    },
+    {
+      field: 'status',
+      title: '状态',
+      width: 70,
+      slots: {
+        default: ({ row }) => {
+          return row.status === 1
+            ? h('span', { class: 'bg-[#e7f5ee] text-[#18a058] p-2 ' }, '正常')
+            : h('span', { class: 'bg-[#fbeff1] text-[#d03050] p-2' }, '禁用');
+        },
+      },
+    },
+    {
+      field: 'isDefault',
+      title: '默认',
+      width: 70,
+      slots: {
+        default: ({ row }) => {
+          return row.isDefault === 1
+            ? h('span', { class: 'bg-[#e8f2fd] text-[#2080f0] p-2' }, '是')
+            : '';
+        },
+      },
+    },
+    {
+      field: 'initialBalance',
+      title: '期初余额',
+      width: 120,
+      align: 'right',
+      headerAlign: 'center',
+    },
+    { field: 'createdAt', title: '创建时间', width: 145 },
+    { field: 'updatedAt', title: '更新时间', width: 145 },
+    {
+      field: 'action',
+      title: '操作',
+      width: 120,
+      fixed: 'right',
+      slots: {
+        default: 'action',
+      },
+    },
+  ],
+  proxyConfig: {
+    ajax: {
+      query: async ({ page }) => {
+        return await getSupplierListApi({
+          pageNum: page.currentPage,
+          pageSize: page.pageSize,
+          ...form,
+        });
+      },
+    },
+  },
+};
+const [Grid, gridApi] = useVbenVxeGrid({ gridOptions });
+const [SupplierModal, modalApi] = useVbenModal({
+  connectedComponent: ExtraModal,
+  destroyOnClose: true,
+});
+const handleAdd = () => {
+  modalApi.setData({}).open();
+};
+const handleEdit = (row: SupplierApi.SupplierItem) => {
+  modalApi.setData(row).open();
+};
+const handleDelete = (row: SupplierApi.SupplierItem) => {
+  Modal.confirm({
+    icon: null,
+    centered: true,
+    maskClosable: false,
+    destroyOnClose: true,
+    title: '删除供货商',
+    content: h('div', null, [
+      h('span', null, '您确定要删除 '),
+      h('span', { class: 'text-red-500' }, `${row.name}`),
+      h('span', null, ' 吗 ?'),
+    ]),
+    async onOk() {
+      try {
+        await deleteSupplierApi(row.id);
+        gridApi.query();
+      } catch {}
+    },
+  });
+};
+const handleRefresh = (code: 'query' | 'reload') => {
+  if (code === 'query') {
+    gridApi.query();
+  } else {
+    gridApi.reload();
+  }
+};
 </script>
 <template>
-  <Page auto-content-height> Supplier </Page>
+  <Page auto-content-height>
+    <template #title>
+      <InputSearch
+        v-model:value="form.keyword"
+        placeholder="请输入编码、名称、拼音、联系人"
+        class="w-80"
+        enterButton="查询"
+        allowClear
+        @search="handleRefresh('reload')"
+      />
+    </template>
+    <template #extra>
+      <Button type="primary" @click="handleAdd">新增</Button>
+    </template>
+    <Grid>
+      <template #action="{ row }">
+        <Button type="link" size="small" @click="handleEdit(row)">修改</Button>
+        <Button type="link" danger size="small" @click="handleDelete(row)">
+          删除
+        </Button>
+      </template>
+    </Grid>
+    <SupplierModal @refresh="handleRefresh" />
+  </Page>
 </template>
---
+++ apps/web-antd/src/views/basic/supplier/modal.vue
@@ -0,0 +1,298 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import { useVbenModal } from '@vben/common-ui';
+
+import { pinyin } from 'pinyin-pro';
+
+import { useVbenForm, z } from '#/adapter/form';
+import { createSupplierApi, updateSupplierApi } from '#/api';
+
+const emit = defineEmits(['refresh']);
+const isUpdate = ref(false);
+const [Form, formApi] = useVbenForm({
+  wrapperClass: 'grid-cols-12',
+  commonConfig: {
+    componentProps: {
+      class: 'w-full',
+    },
+    labelWidth: 60,
+    formItemClass: 'col-span-4',
+  },
+  handleSubmit: onSubmit,
+
+  schema: [
+    {
+      component: 'Input',
+      fieldName: 'id',
+      label: '编码',
+      componentProps: {
+        disabled: true,
+      },
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入供货商名称',
+      },
+      fieldName: 'name',
+      label: '名称',
+      rules: z.string().regex(/^\S(?:[\s\S]{0,38}\S)?$/, {
+        message: '长度为1-40字符',
+      }),
+    },
+    {
+      component: 'Input',
+      disabled: true,
+      fieldName: 'pinyin',
+      label: '拼音码',
+      rules: z
+        .string()
+        .max(40, {
+          message: '请输入1-40个字符',
+        })
+        .optional(),
+      dependencies: {
+        triggerFields: ['name'],
+        trigger(values) {
+          values.pinyin = pinyin(values.name, {
+            pattern: 'first',
+            toneType: 'none',
+            separator: '',
+          })
+            .replaceAll(/[^a-z0-9]/gi, '')
+            .toLocaleUpperCase();
+        },
+      },
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入联系人',
+      },
+      fieldName: 'contact',
+      label: '联系人',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,38}\S)?)?$/, {
+          message: '长度为1-40字符',
+        })
+        .optional(),
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入手机',
+      },
+      fieldName: 'phone',
+      label: '手机',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,28}\S)?)?$/, {
+          message: '长度为1-30字符',
+        })
+        .optional(),
+    },
+
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入公司名称',
+      },
+      fieldName: 'company',
+      label: '公司名称',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,38}\S)?)?$/, {
+          message: '长度为1-40字符',
+        })
+        .optional(),
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入税号',
+      },
+      fieldName: 'taxNo',
+      label: '税号',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,38}\S)?)?$/, {
+          message: '长度为1-40字符',
+        })
+        .optional(),
+      formItemClass: 'col-span-6',
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入账户',
+      },
+      fieldName: 'bankAccount',
+      label: '银行账户',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,38}\S)?)?$/, {
+          message: '长度为1-40字符',
+        })
+        .optional(),
+      formItemClass: 'col-span-6',
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入银行名称',
+      },
+      fieldName: 'bankName',
+      label: '银行名称',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,38}\S)?)?$/, {
+          message: '长度为1-40字符',
+        })
+        .optional(),
+      formItemClass: 'col-span-6',
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入开户人',
+      },
+      fieldName: 'bankHolder',
+      label: '开户人',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,38}\S)?)?$/, {
+          message: '长度为1-40字符',
+        })
+        .optional(),
+      formItemClass: 'col-span-6',
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        allowClear: true,
+        placeholder: '请输入银行地址',
+      },
+      fieldName: 'bankAddress',
+      label: '银行地址',
+      formItemClass: 'col-span-12',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,78}\S)?)?$/, {
+          message: '长度为1-80字符',
+        })
+        .optional(),
+    },
+
+    {
+      component: 'Input',
+      componentProps: {
+        allowClear: true,
+        placeholder: '请输入1-80字地址',
+      },
+      fieldName: 'address',
+      label: '地址',
+      formItemClass: 'col-span-12',
+      rules: z
+        .string()
+        .regex(/^(?:\S(?:[\s\S]{0,78}\S)?)?$/, {
+          message: '长度为1-80字符',
+        })
+        .optional(),
+    },
+
+    {
+      component: 'InputNumber',
+      componentProps: {
+        placeholder: '0.00',
+        controls: false,
+        precision: 2,
+      },
+      fieldName: 'initialBalance',
+      label: '期初余额',
+      defaultValue: 0,
+      dependencies: {
+        triggerFields: ['id'],
+        if: ({ id }) => !id,
+      },
+    },
+    {
+      component: 'RadioGroup',
+      fieldName: 'status',
+      label: '状态',
+      defaultValue: 1,
+      componentProps: {
+        options: [
+          {
+            label: '启用',
+            value: 1,
+          },
+          {
+            label: '禁用',
+            value: 0,
+          },
+        ],
+      },
+    },
+    {
+      component: 'RadioGroup',
+      fieldName: 'isDefault',
+      label: '默认',
+      defaultValue: 0,
+      componentProps: {
+        options: [
+          {
+            label: '是',
+            value: 1,
+          },
+          {
+            label: '否',
+            value: 0,
+          },
+        ],
+      },
+    },
+  ],
+});
+const [Modal, modalApi] = useVbenModal({
+  onOpenChange(isOpen: boolean) {
+    if (!isOpen) {
+      return;
+    }
+    const { id, ...values } = modalApi.getData();
+    isUpdate.value = !!id;
+    if (id) {
+      formApi.setValues({
+        id,
+        ...values,
+      });
+    }
+  },
+  onConfirm: async () => {
+    await formApi.validateAndSubmitForm();
+  },
+});
+
+async function onSubmit(values: Record<string, any>) {
+  if (isUpdate.value) {
+    await updateSupplierApi(values);
+    emit('refresh', 'query');
+  } else {
+    await createSupplierApi(values);
+    emit('refresh', 'reload');
+  }
+  modalApi.close();
+}
+</script>
+<template>
+  <Modal
+    class="w-[650px]"
+    :title="isUpdate ? '修改供货商' : '新增供货商'"
+    centered
+    :fullscreen-button="false"
+    auto-content-height
+  >
+    <Form />
+  </Modal>
+</template>
