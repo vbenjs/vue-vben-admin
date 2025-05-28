969e28aac7a40ea42b6ac171b96a54428d190946
zwtvip <zwtvip@qq.com>
2025-05-28 23:23:24 +0800

    fix:  When defaultHomePage is inconsistent with user.homePath, the page refresh route jump will be abnormal

--- playground/src/router/guard.ts
+++ playground/src/router/guard.ts
@@ -110,7 +110,7 @@ function setupAccessGuard(router: Router) {
       redirectPath = from.query.redirect as string;
     } else if (to.path === preferences.app.defaultHomePath) {
       redirectPath = preferences.app.defaultHomePath;
-    } else if (to.path === userInfo.homePath) {
+    } else if (userInfo.homePath && to.path === userInfo.homePath) {
       redirectPath = userInfo.homePath;
     } else {
       redirectPath = to.fullPath;
