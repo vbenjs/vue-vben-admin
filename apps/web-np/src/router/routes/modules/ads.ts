0f3289b5f2a07613d369c18ab231817d5db22963
Ethan Wilson <ethanwilson1291@gmail.com>
2025-06-02 23:29:02 +0700

    feat: Enable ads Menu

--- apps/web-np/src/router/routes/modules/ads.ts
+++ apps/web-np/src/router/routes/modules/ads.ts
@@ -1,45 +1,45 @@
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  // {
-  //   meta: {
-  //     icon: 'mdi:ads',
-  //     order: 50,
-  //     title: 'Ads Management',
-  //   },
-  //   name: 'ads',
-  //   path: '/ads',
-  //   children: [
-  //     {
-  //       meta: {
-  //         title: 'Ad Connections',
-  //         icon: 'ant-design:link-outlined',
-  //       },
-  //       name: 'ads.accounts',
-  //       path: '/ads/accounts',
-  //       component: () => import('#/views/ads/account/index.vue'),
-  //     },
-  //     {
-  //       meta: {
-  //         title: 'Ad Cost Rules',
-  //         icon: 'ant-design:dollar-circle-twotone',
-  //       },
-  //       name: 'ads.costs',
-  //       path: '/ads/ad-costs',
-  //       component: () => import('#/views/ads/ad-costs/index.vue'),
-  //     },
-  //     {
-  //       meta: {
-  //         title: 'Ad Cost Insights',
-  //         order: 20,
-  //         icon: 'clarity:analytics-outline-badged',
-  //       },
-  //       name: 'ads.insights',
-  //       path: '/ads/ad-insights',
-  //       component: () => import('#/views/ads/ad-insights/index.vue'),
-  //     },
-  //   ],
-  // },
+  {
+    meta: {
+      icon: 'mdi:ads',
+      order: 50,
+      title: 'Ads Management',
+    },
+    name: 'ads',
+    path: '/ads',
+    children: [
+      {
+        meta: {
+          title: 'Ad Connections',
+          icon: 'ant-design:link-outlined',
+        },
+        name: 'ads.accounts',
+        path: '/ads/accounts',
+        component: () => import('#/views/ads/account/index.vue'),
+      },
+      {
+        meta: {
+          title: 'Ad Cost Rules',
+          icon: 'ant-design:dollar-circle-twotone',
+        },
+        name: 'ads.costs',
+        path: '/ads/ad-costs',
+        component: () => import('#/views/ads/ad-costs/index.vue'),
+      },
+      {
+        meta: {
+          title: 'Ad Cost Insights',
+          order: 20,
+          icon: 'clarity:analytics-outline-badged',
+        },
+        name: 'ads.insights',
+        path: '/ads/ad-insights',
+        component: () => import('#/views/ads/ad-insights/index.vue'),
+      },
+    ],
+  },
 ];
 
 export default routes;
