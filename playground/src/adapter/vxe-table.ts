d88b39556c701697a7907dc6f2d1cc2c7de93b1e
QJia <532612154@qq.com>
2025-06-03 16:48:25 +0800

    fix: [adapter] 表格配置类型报错
    配置toolbarConfig中的search时会有类型报错

--- playground/src/adapter/vxe-table.ts
+++ playground/src/adapter/vxe-table.ts
@@ -1,3 +1,4 @@
+import type { VxeTableGridOptions } from '@vben/plugins/vxe-table';
 import type { Recordable } from '@vben/types';
 
 import { h } from 'vue';
@@ -42,7 +43,7 @@ setupVbenVxeTable({
         round: true,
         showOverflow: true,
         size: 'small',
-      },
+      } as VxeTableGridOptions,
     });
 
     /**
