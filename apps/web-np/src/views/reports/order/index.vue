3658e65f56b1fb110e8ec2c19538082ef85d8713
Ethan Wilson <ethanwilson1291@gmail.com>
2025-06-12 10:39:57 +0700

    feat: Update UX for recalculation order fees

--- apps/web-np/src/views/reports/order/index.vue
+++ apps/web-np/src/views/reports/order/index.vue
@@ -1,15 +1,21 @@
 <script lang="ts" setup>
+import type { INotification } from '#/store';
+
+import { onMounted } from 'vue';
+
 import { Page, useVbenModal, VbenButton } from '@vben/common-ui';
 import { IconifyIcon } from '@vben/icons';
 
 import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import { useShopStore } from '#/store';
 
 import FormModalOrderDetail from './form-modal-order-detail.vue';
 import FormModalRecalculate from './form-modal-recalculate.vue';
 import { orderTableOptions } from './table-config';
 import { formOptions } from './table-filter';
 
-const [Grid] = useVbenVxeGrid({
+const shopStore = useShopStore();
+const [Grid, gridApi] = useVbenVxeGrid({
   gridOptions: orderTableOptions,
   formOptions,
 });
@@ -20,6 +26,24 @@ const [FormContentModal, formContentModalApi] = useVbenModal({
 
 const [FormOrdDetailModal, formDetailModalApi] = useVbenModal({
   connectedComponent: FormModalOrderDetail,
+});
+
+onMounted(() => {
+  shopStore.pusherChannel.bind(
+    shopStore.pusherEventName,
+    (payload: INotification) => {
+      switch (payload.type) {
+        case 'OrderCalculatedNotification': {
+          gridApi.reload();
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
+    },
+  );
 });
 
 const handleDetailOpen = (order: any) => {
