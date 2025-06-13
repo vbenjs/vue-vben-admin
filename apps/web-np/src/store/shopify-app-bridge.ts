ee55689a400d018939fea87b85dd58a3e72d7c17
Ethan Wilson <ethanwilson1291@gmail.com>
2025-06-13 09:59:22 +0700

    feat: Update layout

--- apps/web-np/src/store/shopify-app-bridge.ts
+++ apps/web-np/src/store/shopify-app-bridge.ts
@@ -9,16 +9,24 @@ import { defineStore } from 'pinia';
 export const useShopifyAppBridgeStore = defineStore('np-shopify-appbridge', {
   actions: {
     initAppBridge() {
-      if (!isShopifyEmbedded()) {
+      if (!isShopifyEmbedded() || this.appBridge) {
         return;
       }
 
-      this.appBridge = createApp({
-        apiKey: import.meta.env.VITE_GLOB_SHOPIFY_APP_KEY,
-        host: new URLSearchParams(location.search).get('host') as string,
-      });
+      try {
+        const app = createApp({
+          apiKey: import.meta.env.VITE_GLOB_SHOPIFY_APP_KEY,
+          host: new URLSearchParams(location.search).get('host') as string,
+        });
+
+        this.appBridge = app;
+      } catch {}
     },
     getSessionToken() {
+      if (!this.appBridge) {
+        return;
+      }
+
       getSessionToken(this.appBridge).then((_) => {
         // You can use the token for further API calls or verification
       });
