90fec81ffc692b3152d88d13824aaafee57bf2c7
qianyuanji <qianyuanji@proton.me>
2025-08-29 09:01:16 +0800

    fix(@vben/common-ui): resolve #6698

--- packages/effects/common-ui/src/components/page/page.vue
+++ packages/effects/common-ui/src/components/page/page.vue
@@ -86,7 +86,7 @@ onMounted(() => {
       </div>
     </div>
 
-    <div :class="cn('min-h-0 flex-1 p-4', contentClass)" :style="contentStyle">
+    <div :class="cn('h-full p-4', contentClass)" :style="contentStyle">
       <slot></slot>
     </div>
     <div
