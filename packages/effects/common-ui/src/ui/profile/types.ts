7dc68ed368c59cd1d533bcf202c2a8a37f0cb911
xingyu4j <xingyu4j@vip.qq.com>
2025-11-10 17:49:58 +0800

    feat: add profile comps

---
+++ packages/effects/common-ui/src/ui/profile/types.ts
@@ -0,0 +1,22 @@
+import type { BasicUserInfo } from '@vben/types';
+
+export interface Props {
+  title?: string;
+  userInfo: BasicUserInfo | null;
+  tabs: {
+    label: string;
+    value: string;
+  }[];
+}
+
+export interface FormSchemaItem {
+  description: string;
+  fieldName: string;
+  label: string;
+  name: string;
+  value: boolean;
+}
+
+export interface SettingProps {
+  formSchema: FormSchemaItem[];
+}
