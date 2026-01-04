231a5169ecb257abde8c02a074161b6bb76f3204
xingyu4j <xingyu4j@vip.qq.com>
2026-01-04 10:56:55 +0800

    fix: lint

--- playground/src/views/examples/context-menu/index.vue
+++ playground/src/views/examples/context-menu/index.vue
@@ -43,15 +43,14 @@ const contextMenus = () => {
     },
   ];
 };
-
 </script>
 
 <template>
   <Page title="Context Menu 上下文菜单">
     <Card title="基本使用">
       <div>一共四个菜单（刷新、关闭当前、关闭其他、关闭所有）</div>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <VbenContextMenu :menus="contextMenus" :modal="true" item-class="pr-6">
         <Button> 右键点击我打开上下文菜单(有隐藏项) </Button>
       </VbenContextMenu>
