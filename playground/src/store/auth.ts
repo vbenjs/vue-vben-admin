1cb53e943ea99acb655100dcfaf743b01de1a09a
eric <kuchaguangjie@gmail.com>
2026-01-09 23:13:06 +0800

    refactor: 将跳转放到最后

--- playground/src/store/auth.ts
+++ playground/src/store/auth.ts
@@ -89,21 +89,21 @@ export const useAuthStore = defineStore('auth', () => {
 
       resetAllStores();
       accessStore.setLoginExpired(false);
-
-      // 回登录页带上当前路由地址
-      await router.replace({
-        path: LOGIN_PATH,
-        query: redirect
-          ? {
-              redirect: encodeURIComponent(router.currentRoute.value.fullPath),
-            }
-          : {},
-      });
     } catch {
       // 不做任何处理
     } finally {
       isLoggingOut.value = false; // 重置 标识
     }
+
+    // 回登录页带上当前路由地址
+    await router.replace({
+      path: LOGIN_PATH,
+      query: redirect
+        ? {
+            redirect: encodeURIComponent(router.currentRoute.value.fullPath),
+          }
+        : {},
+    });
   }
 
   async function fetchUserInfo() {
