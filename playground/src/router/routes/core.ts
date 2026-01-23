88b4b5610330438c9ad998bc3f621e39f75b61f4
eric <kuchaguangjie@gmail.com>
2026-01-24 02:19:28 +0800

    fix: 个人中心在后端权限模式打不开

--- playground/src/router/routes/core.ts
+++ playground/src/router/routes/core.ts
@@ -36,7 +36,18 @@ const coreRoutes: RouteRecordRaw[] = [
     name: 'Root',
     path: '/',
     redirect: preferences.app.defaultHomePath,
-    children: [],
+    children: [
+      {
+        name: 'Profile',
+        path: '/profile',
+        component: () => import('#/views/_core/profile/index.vue'),
+        meta: {
+          title: '个人中心',
+          icon: 'lucide:user',
+          hideInMenu: true, // 隐藏在菜单栏，但路由依然存在
+        },
+      },
+    ],
   },
   {
     component: AuthPageLayout,
