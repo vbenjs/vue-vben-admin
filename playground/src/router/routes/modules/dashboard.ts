45b843f3441b3b45cae6192981f5422ff1310831
ming4762 <zhongming4762@hotmail.com>
2026-02-26 06:21:08 +0800

    fix: fix bug where `renderEcharts` gets stuck in a dead loop (#7561)
    * 触发条件：echart所在页面开启keepalive 在其他页面切换颜色模式

--- playground/src/router/routes/modules/dashboard.ts
+++ playground/src/router/routes/modules/dashboard.ts
@@ -20,6 +20,7 @@ const routes: RouteRecordRaw[] = [
           affixTab: true,
           icon: 'lucide:area-chart',
           title: $t('page.dashboard.analytics'),
+          keepAlive: true,
         },
       },
       {
