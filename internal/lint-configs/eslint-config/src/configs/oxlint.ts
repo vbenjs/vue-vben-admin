771277d5d968b7faf16eee788faa1cd628e6271a
xingyu4j <xingyu4j@vip.qq.com>
2026-03-13 20:28:56 +0800

    fix: align oxlint compat config typing

--- internal/lint-configs/eslint-config/src/configs/oxlint.ts
+++ internal/lint-configs/eslint-config/src/configs/oxlint.ts
@@ -5,5 +5,9 @@ import { mergeOxlintConfigs, oxlintConfig } from '@vben/oxlint-config';
 import oxlint from 'eslint-plugin-oxlint';
 
 export async function oxcCompat(): Promise<Linter.Config[]> {
-  return oxlint.buildFromOxlintConfig(mergeOxlintConfigs(oxlintConfig));
+  const { extends: _extends, ...config } = mergeOxlintConfigs(oxlintConfig);
+
+  return oxlint.buildFromOxlintConfig(
+    config as Parameters<typeof oxlint.buildFromOxlintConfig>[0],
+  );
 }
