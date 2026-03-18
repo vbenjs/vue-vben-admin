beef38e63fb5245a66bf16003e68239b6937c094
xingyu4j <xingyu4j@vip.qq.com>
2026-03-18 20:01:28 +0800

    fix: lint

--- internal/lint-configs/eslint-config/src/configs/node.ts
+++ internal/lint-configs/eslint-config/src/configs/node.ts
@@ -18,6 +18,7 @@ export async function node(): Promise<Linter.Config[]> {
           {
             allowModules: [
               'tsdown',
+              'unplugin-vue',
               '@vben/vite-config',
               'vitest',
               'vite',
