f443bfbc7bb0a6ba106c87a0ae8a6d4c61b2cd1b
dullathanol <327533967@qq.com>
2026-04-08 10:11:05 +0800

    fix: tailwindcss config

--- packages/@core/ui-kit/shadcn-ui/src/ui/button/button.ts
+++ packages/@core/ui-kit/shadcn-ui/src/ui/button/button.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed  disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
   {
     defaultVariants: {
       size: 'default',
