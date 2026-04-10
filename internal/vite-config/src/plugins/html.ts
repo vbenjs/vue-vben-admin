298930b0d7d34938180bafc0bbce148e3260474f
xingyu4j <xingyu4j@vip.qq.com>
2026-04-10 22:00:33 +0800

    chore: remove vite-plugin-html

---
+++ internal/vite-config/src/plugins/html.ts
@@ -0,0 +1,35 @@
+import type { PluginOption } from 'vite';
+import type { Options as HtmlMinifierOptions } from 'html-minifier-terser';
+
+import { minify } from 'html-minifier-terser';
+
+const HTML_MINIFY_OPTIONS = {
+  collapseWhitespace: true,
+  minifyCSS: true,
+  minifyJS: true,
+  removeComments: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  useShortDoctype: true,
+} as const;
+
+function viteHtmlPlugin(options: HtmlMinifierOptions = {}): PluginOption {
+  return {
+    name: 'vben-native-html',
+    transformIndexHtml: {
+      order: 'post',
+      async handler(html, ctx) {
+        if (!ctx.bundle) {
+          return html;
+        }
+        return await minify(html, {
+          ...HTML_MINIFY_OPTIONS,
+          ...options,
+        });
+      },
+    },
+  };
+}
+
+export { viteHtmlPlugin };
