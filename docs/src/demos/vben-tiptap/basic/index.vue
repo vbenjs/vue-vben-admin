eb9cdbab9b3a4d80ae320aeaeb5a5121567926c3
xingyu4j <xingyu4j@vip.qq.com>
2026-05-16 12:55:19 +0800

    fix: lint

--- docs/src/demos/vben-tiptap/basic/index.vue
+++ docs/src/demos/vben-tiptap/basic/index.vue
@@ -11,10 +11,9 @@ const content = ref('<p>开始编辑你的内容...</p>');
     <VbenTiptap v-model="content" />
     <div class="mt-4">
       <p class="text-sm text-gray-500">当前内容:</p>
-      <pre
-        class="mt-2 p-2 bg-gray-100 rounded text-xs overflow-auto max-h-40"
-        >{{ content }}</pre
-      >
+      <pre class="mt-2 p-2 bg-gray-100 rounded text-xs overflow-auto max-h-40">
+        {{ content }}
+      </pre>
     </div>
   </div>
 </template>
