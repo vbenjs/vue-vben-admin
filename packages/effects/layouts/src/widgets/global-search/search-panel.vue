a52b37c1f4c904a0692d541e0d348ff01d34418a
cdc <3589045787@qq.com>
2026-05-18 22:19:19 +0800

    fix: search-panel when ArrowUp and ArrowDown

--- packages/effects/layouts/src/widgets/global-search/search-panel.vue
+++ packages/effects/layouts/src/widgets/global-search/search-panel.vue
@@ -11,7 +11,12 @@ import { mapTree, traverseTreeValues, uniqueByField } from '@vben/utils';
 import { VbenIcon, VbenScrollbar } from '@vben-core/shadcn-ui';
 import { isHttpUrl } from '@vben-core/shared/utils';
 
-import { onKeyStroke, useLocalStorage, useThrottleFn } from '@vueuse/core';
+import {
+  onKeyStroke,
+  useEventListener,
+  useLocalStorage,
+  useThrottleFn,
+} from '@vueuse/core';
 
 defineOptions({
   name: 'SearchPanel',
@@ -34,6 +39,7 @@ const searchHistory = useLocalStorage<MenuRecordRaw[]>(
 const activeIndex = ref(-1);
 const searchItems = shallowRef<MenuRecordRaw[]>([]);
 const searchResults = ref<MenuRecordRaw[]>([]);
+const isNavigating = ref(false);
 
 const handleSearch = useThrottleFn(search, 200);
 
@@ -114,6 +120,7 @@ function handleUp() {
   if (searchResults.value.length === 0) {
     return;
   }
+  isNavigating.value = true;
   activeIndex.value--;
   if (activeIndex.value < 0) {
     activeIndex.value = searchResults.value.length - 1;
@@ -126,6 +133,7 @@ function handleDown() {
   if (searchResults.value.length === 0) {
     return;
   }
+  isNavigating.value = true;
   activeIndex.value++;
   if (activeIndex.value > searchResults.value.length - 1) {
     activeIndex.value = 0;
@@ -141,6 +149,7 @@ function handleClose() {
 
 // Activate when the mouse moves to a certain line
 function handleMouseenter(e: MouseEvent) {
+  if (isNavigating.value) return;
   const index = (e.target as HTMLElement)?.dataset.index;
   activeIndex.value = Number(index);
 }
@@ -218,6 +227,10 @@ onMounted(() => {
   onKeyStroke('ArrowDown', handleDown);
   // esc close
   onKeyStroke('Escape', handleClose);
+});
+
+useEventListener('mousemove', () => {
+  isNavigating.value = false;
 });
 </script>
 
