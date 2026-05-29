f26328f8204afa71441ae5cf827b195607821298
xingyu4j <xingyu4j@vip.qq.com>
2026-05-29 16:38:10 +0800

    fix: type error

--- packages/@core/ui-kit/shadcn-ui/src/components/table-action/types.ts
+++ packages/@core/ui-kit/shadcn-ui/src/components/table-action/types.ts
@@ -33,7 +33,7 @@ export interface ActionItem {
   /** 是否禁用 */
   disabled?: boolean;
   /** 图标组件 */
-  icon?: typeof VbenIcon;
+  icon?: typeof VbenIcon.icon;
   /** 是否显示：布尔或返回布尔的函数 */
   ifShow?: (() => boolean) | boolean;
   /** 唯一标识，点击回调可据此区分 */
