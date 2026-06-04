748b182e440863711dc9624924e92440f5ddc4d3
afe1 <2279948211@qq.com>
2026-06-05 05:13:08 +0800

    fix: workflow (#8013)
    * fix: catelog
    
    * fix: system
    
    * chore: sync upstream changes
    
    * feat:  component and workflow error

--- packages/@core/ui-kit/shadcn-ui/src/ui/dialog/DialogOverlay.vue
+++ packages/@core/ui-kit/shadcn-ui/src/ui/dialog/DialogOverlay.vue
@@ -1,5 +1,13 @@
-<script lang="ts">
+<script setup lang="ts">
+import type { DialogOverlayProps } from 'reka-ui';
+
 import { DialogOverlay } from 'reka-ui';
 
-export default DialogOverlay;
+const props = defineProps<DialogOverlayProps>();
 </script>
+
+<template>
+  <DialogOverlay data-slot="dialog-overlay" v-bind="props">
+    <slot></slot>
+  </DialogOverlay>
+</template>
